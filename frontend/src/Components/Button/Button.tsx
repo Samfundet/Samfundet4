805f62eaada806ec057b4dc0bcb39615fe009ee5
Emil Telstad <emil.telstad@gmail.com>
2022-09-11 22:40:16 +0200

    Configure eslint, prettier and stylelint.

--- frontend/src/Components/Button/Button.tsx
+++ frontend/src/Components/Button/Button.tsx
@@ -1,29 +1,30 @@
-import classnames from "classnames";
-import { ButtonType, Children } from "../../types";
-import styles from "./Button.module.scss"
+import React from 'react';
+import classnames from 'classnames';
+import { ButtonType, Children } from '../../types';
+import styles from './Button.module.scss';
 
-type ButtonTheme = 'samf' | 'secondary'
+type ButtonTheme = 'samf' | 'secondary';
 
 type ButtonProps = {
-    name?: string;
-    theme?: ButtonTheme;
-    type?: ButtonType;
-    className?: string;
-    disabled?: boolean;
-    children?: Children;
-    onClick?: () => void;
-}
+  name?: string;
+  theme?: ButtonTheme;
+  type?: ButtonType;
+  className?: string;
+  disabled?: boolean;
+  children?: Children;
+  onClick?: () => void;
+};
 
-const mapThemeToStyle: { [theme in ButtonTheme]: string} = {
-    'samf': styles.button__samf,
-    'secondary': styles.button__secondary,
-}
+const mapThemeToStyle: { [theme in ButtonTheme]: string } = {
+  samf: styles.button_samf,
+  secondary: styles.button_secondary,
+};
 
-export function Button({name, type, theme="samf", onClick, disabled, className, children}: ButtonProps) {
-    const classNames = classnames(mapThemeToStyle[theme], className)
-    return (
-        <button name={name} onClick={onClick} type={type} disabled={disabled} className={classNames}>
-            {children}
-        </button>
-    )
+export function Button({ name, type, theme = 'samf', onClick, disabled, className, children }: ButtonProps) {
+  const classNames = classnames(mapThemeToStyle[theme], className);
+  return (
+    <button name={name} onClick={onClick} type={type} disabled={disabled} className={classNames}>
+      {children}
+    </button>
+  );
 }
