70ad2445191e1dbee38f3bd4568f42bcc5aecf2b
Emil Telstad <emil.telstad@gmail.com>
2022-09-13 21:11:36 +0200

    Import constants from root.

--- backend/root/settings/base.py
+++ backend/root/settings/base.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 import environ
 
-from constants import Environment
+from root.constants import Environment
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
