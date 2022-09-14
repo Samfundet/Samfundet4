b23da23859feb58c7f01ce1814f50481dc8cc5b7
Emil Telstad <emil.telstad@gmail.com>
2022-09-14 21:37:31 +0200

    Add routes.

--- frontend/src/RouterRoutes.tsx
+++ frontend/src/RouterRoutes.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './Pages';
+import { ROUTES } from './routes';
 
 export function RouterRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path={ROUTES.frontend.home} element={<HomePage />} />
     </Routes>
   );
 }
