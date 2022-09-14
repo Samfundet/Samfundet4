329d7713c400e20cf86f9cf2ff11b05755ba469d
Emil Telstad <emil.telstad@gmail.com>
2022-09-14 18:55:14 +0200

    Add generate frontend routes command.

---
+++ backend/root/management/commands/generate_frontend_routes.py
@@ -0,0 +1,94 @@
+from __future__ import annotations
+
+import json
+from dataclasses import dataclass
+
+from dataclasses_json import dataclass_json
+
+from django.core import management
+from django.core.management.base import BaseCommand
+
+# pylint: disable=positional-arguments
+
+
+def to_camel_case(snake_str: str):
+    components = snake_str.split('_')
+    # We capitalize the first letter of each component except the first one
+    # with the 'title' method and join them together.
+    return components[0] + ''.join(x.title() for x in components[1:])
+
+
+@dataclass_json
+@dataclass
+class Url:
+    url: str
+    module: str
+    name: str
+    decorators: str
+
+
+def parse_url(url: str):
+    """Parse django url to frontend route format."""
+
+    # from: '/some/path/<int:org_id>/'
+    sub_paths = url.split('/')
+    # to: ['', 'some', 'path', '<int:org_id>', '']
+
+    for i, sub_path in enumerate(sub_paths, 0):
+        # Keep new equal to old if no parsing is needed.
+        new_sub_path = sub_path
+
+        try:
+            colon_index = sub_path.index(':')
+            # Assuming sub_path param has no prefix of form 'something<int:org_id>'.
+            # crocl_index = sub_path.index('<')
+            new_sub_path = to_camel_case(sub_path[colon_index:-1])  # ':orgId'
+
+        except ValueError:
+            # ':' was not found.
+            # sub_path = '<org_id>' or 'path'
+            try:
+                _chevron_left_index = sub_path.index('<')
+                # Found param, sub_path = '<org_id>'
+                new_sub_path = ':' + to_camel_case(sub_path[1:-1])  # ':orgId'
+            except ValueError:
+                # '<' was not found.
+                # sub_path = 'path'
+                # Do nothing with normal sub_path.
+                pass
+
+        # Replace old sub_path with new_sub_path.
+        sub_paths[i] = new_sub_path
+
+    # from: ['', 'some', 'path', ':orgId', '']
+    new_url = '/'.join(sub_paths)
+    # to: '/some/path/:orgId/'
+
+    return new_url
+
+
+class Command(BaseCommand):
+    """Generate frontend routes"""
+
+    def handle(self, *args, **options):
+        # Get all urls as json.
+        urls_json: str = management.call_command('show_urls', format_style='json')
+
+        # Load urls to dict.
+        urls_dict: dict = json.loads(urls_json)
+
+        # Parse urls into objects.
+        urls: list[Url] = [Url.from_dict(url) for url in urls_dict]
+
+        # Create space from output after call_command('show_urls').
+        print('\n' * 40)
+
+        # Parse all urls to frontend routes.
+        for url in urls:
+
+            # Parse url to frontend route.
+            url.name = url.name.replace(':', '__')  # 'feide:new_tjeneste' -> 'feide__new_tjeneste'
+            url.url = parse_url(url.url)
+
+            # Print in javascript mode for easy copy.
+            print(f"{url.name or 'unknown'}: '{url.url}',")
