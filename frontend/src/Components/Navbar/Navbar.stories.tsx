26d3b3248ba3f61397ff72a5da0d79558a5473ca
Mathias Aas <mathias.aas123@gmail.com>
2022-10-04 08:37:55 +0200

    Create mobile friendly navigation

--- frontend/src/Components/Navbar/Navbar.stories.tsx
+++ frontend/src/Components/Navbar/Navbar.stories.tsx
@@ -1,10 +1,13 @@
-import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ComponentMeta, ComponentStory, DecoratorFn } from '@storybook/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import { withRouter } from 'storybook-addon-react-router-v6';
 import { Navbar } from './Navbar';
 
 // Local component config.
 export default {
   title: 'Components/Navbar',
   component: Navbar,
+  decorators: [withRouter],
 } as ComponentMeta<typeof Navbar>;
 
 const Template: ComponentStory<typeof Navbar> = () => {
--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../assets/logo_white.png';
 import { NavLink as Link } from 'react-router-dom';
 import styles from './Navbar.module.scss';
-import { Button } from 'Components/Button';
 import { ROUTES } from '../../routes';
+import { Button } from '../Button';
+
+function changeLanguage() {
+  console.log('language changed :)');
+}
 
 export function Navbar() {
+  const [mobileNavigation, setMobileNavigation] = useState(0);
+
+  function hamburgerFunction() {
+    return (
+      <div
+        className={styles.navbar_hamburger}
+        onClick={() => (mobileNavigation ? setMobileNavigation(0) : setMobileNavigation(1))}
+      >
+        <div
+          className={styles.navbar_hamburger_line}
+          style={
+            mobileNavigation
+              ? {
+                  transform: 'translateY(12px) rotate(-45deg)',
+                  transition: 'transform 400ms ease',
+                }
+              : {}
+          }
+        />
+        <div
+          className={styles.navbar_hamburger_line}
+          style={mobileNavigation ? { opacity: '0%' } : { opacity: '100%' }}
+        />
+        <div
+          className={styles.navbar_hamburger_line}
+          style={
+            mobileNavigation
+              ? {
+                  transform: 'translateY(-11px) rotate(45deg)',
+                  transition: 'transform 400ms ease',
+                }
+              : {}
+          }
+        />
+      </div>
+    );
+  }
+
+  function showMobileNavigation() {
+    return (
+      <nav className={styles.navbar_popup_mobile}>
+        <Link to={ROUTES.frontend.health} className={styles.navbar_link_mobile}>
+          Arrangement
+        </Link>
+        <Link to={ROUTES.frontend.health} className={styles.navbar_link_mobile}>
+          Information
+        </Link>
+        <Link to={ROUTES.frontend.health} className={styles.navbar_link_mobile}>
+          Restaurant
+        </Link>
+        <Link to={ROUTES.frontend.health} className={styles.navbar_link_mobile}>
+          Opptak
+        </Link>
+        <br />
+        <a onClick={() => changeLanguage()} className={styles.navbar_language_mobile}>
+          English
+        </a>
+        <Button className={styles.navbar_member_button}>
+          <Link to={ROUTES.frontend.health} className={styles.navbar_member_link}>
+            Medlem
+          </Link>
+        </Button>
+        <Button theme="secondary" className={styles.navbar_internal_button}>
+          <Link to={ROUTES.frontend.health} className={styles.navbar_internal_link}>
+            Intern
+          </Link>
+        </Button>
+      </nav>
+    );
+  }
+
   return (
     <>
       <nav className={styles.navbar_nav}>
@@ -22,7 +97,7 @@ export function Navbar() {
           Restaurant
         </Link>
         <Link to={ROUTES.frontend.health} className={styles.navbar_link}>
-          Restaurant
+          Opptak
         </Link>
         <div className={styles.navbar_signup}>
           <Button className={styles.navbar_member_button}>
@@ -36,12 +111,9 @@ export function Navbar() {
             </Link>
           </Button>
         </div>
-        <div className={styles.navbar_hamburger}>
-          <div className={styles.navbar_hamburger_lines} />
-          <div className={styles.navbar_hamburger_lines} />
-          <div className={styles.navbar_hamburger_lines} />
-        </div>
+        {hamburgerFunction()}
       </nav>
+      {mobileNavigation ? showMobileNavigation() : <></>}
     </>
   );
 }
