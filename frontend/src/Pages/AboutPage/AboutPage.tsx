ac212ee95e200866760d38d95a25359f9457d5f3
eiriksho <hoemeirik@gmail.com>
2022-10-11 21:53:09 +0200

    Add venues

--- frontend/src/Pages/AboutPage/AboutPage.tsx
+++ frontend/src/Pages/AboutPage/AboutPage.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
-
 import styles from './AboutPage.module.scss';
-import button from '../../Components/Button/Button.module.scss';
+import bodegaen from '../../assets/bodegaen.jpg';
+import daglighallen from '../../assets/daglighallen.jpg';
+import edgar from '../../assets/edgar.jpg';
+import klubben from '../../assets/klubben.jpg';
+import knaus from '../../assets/knaus.jpg';
+import lyche from '../../assets/lyche.jpg';
 import runderode from '../../assets/runderode.jpg';
+import rundhallen from '../../assets/rundhallen.jpg';
+import selskapssiden from '../../assets/selskapssiden.jpg';
 import splash from '../../assets/splash.jpeg';
+import storsalen from '../../assets/storsalen.jpg';
+import strossa from '../../assets/strossa.jpg';
 import { Button } from 'Components';
 
 export function AboutPage() {
@@ -36,9 +43,56 @@ export function AboutPage() {
           foregår i henhold til norske og interne lover. Samfundet har en daglig leder, økonomiansvarlig, husøkonom,
           vaktmester og renholdspersonell som er ansatte.
         </p>
-        <button className={button.button_samf}>SAMFUNDET SOM ORGANISASJON</button>
-        <button className={button.button_secondary}>SAKSDOKUMENTER</button>
-        <button className={button.button_samf}>SAMFUNDETS HISTORIE</button>
+        <Button>SAMFUNDET SOM ORGANISASJON</Button>
+        <Button theme="secondary">SAKSDOKUMENTER</Button>
+        <Button theme="secondary">SAMFUNDETS HISTORIE</Button>
+      </div>
+      <div className={styles.venues}>
+        <h1>LOKALER</h1>
+        <a className={styles.venueBox} href="/about/storsalen">
+          <img src={storsalen} alt="Storsalen" className={styles.venueImg}></img>
+          <h3>Storsalen</h3>
+        </a>
+        <a href="/about/bodegaen">
+          <img src={bodegaen} alt="Bodegaen" className={styles.venueImg}></img>
+          <h3>Bodegaen</h3>
+        </a>
+        <a href="/about/klubben">
+          <img src={klubben} alt="Klubben" className={styles.venueImg}></img>
+          <h3>Klubben</h3>
+        </a>
+        <a href="/about/strossa">
+          <img src={strossa} alt="Strossa" className={styles.venueImg}></img>
+          <h3>Strossa</h3>
+        </a>
+        <a href="/about/selskapssiden">
+          <img src={selskapssiden} alt="Selskapssiden" className={styles.venueImg}></img>
+          <h3>Selskapssiden</h3>
+        </a>
+        <a href="/about/knaus">
+          <img src={knaus} alt="Knaus" className={styles.venueImg}></img>
+          <h3>Knaus</h3>
+        </a>
+        <a href="/about/edgar">
+          <img src={edgar} alt="Edgar" className={styles.venueImg}></img>
+          <h3>Edgar</h3>
+        </a>
+        <a href="https://samfundet.no/lyche">
+          <img src={lyche} alt="Lyche" className={styles.venueImg}></img>
+          <h3>Lyche</h3>
+        </a>
+        <a href="/about/daglighallen">
+          <img src={daglighallen} alt="Daglighallen" className={styles.venueImg}></img>
+          <h3>Daglighallen</h3>
+        </a>
+        <a href="/about/rundhallen">
+          <img src={rundhallen} alt="Rundhallen" className={styles.venueImg}></img>
+          <h3>Rundhallen</h3>
+        </a>
+      </div>
+      <div className={styles.mapButton}>
+        <Button theme="secondary">OVERSIKTSKART</Button>
+        <Button theme="secondary">NYBYGG</Button>
       </div>
       <div className={styles.moreInfo}>
         <div className={styles.volunteer}>
@@ -50,12 +104,12 @@ export function AboutPage() {
             og gjengen vurderer hver enkelt søker. Med sine omtrent 1700 frivillige utgjør det indre miljøet i
             Studentersamfundet en betydelig del av det organiserte fritidstilbudet til studenter i Trondheim.
           </p>
-          <button className={button.button_samf}>Gjenge på Samfundet</button>
-          <button className={button.button_samf}>Opptak</button>
+          <Button theme="secondary">GJENGENE PÅ SAMFUNDET</Button>
+          <Button theme="secondary">OPPTAK</Button>
         </div>
         <div className={styles.moreInfoRight}>
           <div className={styles.festival}>
-            <h2>UKA og ISFiT</h2>
+            <h2>UKA & ISFiT</h2>
             <p>
               Annenhvert år arrangeres Norges største kulturfestival UKA og verdens største internasjonale tematiske
               studentfestival under Studentersamfundets paraply.
@@ -65,14 +119,14 @@ export function AboutPage() {
           </div>
           <div className={styles.otherInfo}>
             <h2>Annen info</h2>
-            <Button>Aldersgrenser</Button>
-            <Button>Booking</Button>
-            <Button>Quiz</Button>
-            <Button>Leie og tjenester</Button>
-            <Button>Presse</Button>
-            <Button>Filmklubben</Button>
-            <Button>Personvern</Button>
-            <Button>Tilrettelegging</Button>
+            <Button theme="secondary">Aldersgrenser</Button>
+            <Button theme="secondary">Booking</Button>
+            <Button theme="secondary">Quiz</Button>
+            <Button theme="secondary">Leie og tjenester</Button>
+            <Button theme="secondary">Presse</Button>
+            <Button theme="secondary">Filmklubben</Button>
+            <Button theme="secondary">Personvern</Button>
+            <Button theme="secondary">Tilrettelegging</Button>
           </div>
         </div>
       </div>
