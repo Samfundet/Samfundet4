fb24a1690a5d6dc7903bb83dc3af9e2f8c9313fc
Mathias Aas <mathias.aas123@gmail.com>
2022-10-13 21:56:32 +0200

    Wrap navbar in router for Storybook

--- frontend/src/Components/Navbar/Navbar.stories.tsx
+++ frontend/src/Components/Navbar/Navbar.stories.tsx
@@ -1,17 +1,19 @@
-import { ComponentMeta, ComponentStory, DecoratorFn } from '@storybook/react';
-import { MemoryRouter, Route, Routes } from 'react-router-dom';
-import { withRouter } from 'storybook-addon-react-router-v6';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { BrowserRouter } from 'react-router-dom';
 import { Navbar } from './Navbar';
 
 // Local component config.
 export default {
   title: 'Components/Navbar',
   component: Navbar,
-  decorators: [withRouter],
 } as ComponentMeta<typeof Navbar>;
 
 const Template: ComponentStory<typeof Navbar> = () => {
-  return <Navbar />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+    </BrowserRouter>
+  );
 };
 
 export const Primary = Template.bind({});
