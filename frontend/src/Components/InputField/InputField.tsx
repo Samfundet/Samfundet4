0773cb900b9344780df3b16c5c2604f24bec25b0
Mathias Aas <mathias.aas123@gmail.com>
2022-10-13 21:35:59 +0200

    improve InputField component

--- frontend/src/Components/InputField/InputField.tsx
+++ frontend/src/Components/InputField/InputField.tsx
@@ -1,10 +1,22 @@
-import { Children } from '../../types';
+import classNames from 'classnames';
+import styles from './InputField.module.scss';
 
-type RadioButtonProps = {
+type InputTheme = 'samf' | 'secondary';
+
+type InputFieldProps = {
   placeholder?: string;
   className?: string;
+  theme?: InputTheme;
+  type?: string;
+  onChange?: (e: any) => void;
 };
 
-export function InputField({ placeholder, className }: RadioButtonProps) {
-  return <input placeholder={placeholder} className={className} />;
+const mapThemeToStyle: { [theme in InputTheme]: string } = {
+  samf: styles.input_samf,
+  secondary: styles.input_secondary,
+};
+
+export function InputField({ placeholder, className, type, onChange, theme = 'samf' }: InputFieldProps) {
+  const classnames = classNames(mapThemeToStyle[theme], className);
+  return <input placeholder={placeholder} className={classnames} type={type} onChange={onChange} />;
 }
