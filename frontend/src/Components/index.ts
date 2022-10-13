8be1828cdbfde9c2b59fa73b6cbf70b3238ab737
Snorre Skjellestad Kristiansen <snorre.ssk@gmail.com>
2022-10-13 21:40:16 +0200

    Add styling

--- frontend/src/Components/index.ts
+++ frontend/src/Components/index.ts
@@ -2,3 +2,4 @@ export { Input } from './Input';
 export { Button } from './Button';
 export { ProgressBar } from './ProgressBar';
 export { RadioButton } from './RadioButton';
+export { Input } from './Input';
