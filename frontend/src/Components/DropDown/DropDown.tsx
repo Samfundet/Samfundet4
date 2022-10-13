0ab3091b6ce3685e58fdeb3c2d7a1f3102078ad9
Snorre Saether <snosaet@hotmail.com>
2022-10-13 21:32:52 +0200

    Startet på dropdown

---
+++ frontend/src/Components/DropDown/DropDown.tsx
@@ -0,0 +1,28 @@
+import classnames from 'classnames';
+import styles from './Button.module.scss';
+
+type DropDownTheme = 'samf' | 'secondary';
+
+type DropDownProps = {
+  className?: string;
+  dropDownList?: Array<string>;
+  theme?: DropDownTheme;
+};
+const mapThemeToStyle: { [theme in DropDownTheme]: string } = {
+  samf: styles.dropdown_samf,
+  secondary: styles.dropdown_secondary,
+};
+export function DropDown({ dropDownList, theme = 'samf', className }: DropDownProps) {
+  const classNames = classnames(mapThemeToStyle[theme], className);
+  return (
+    <select>
+      {dropDownList?.map(function (element, index) {
+        return (
+          <option value={element} key={index} className={classNames}>
+            {element}
+          </option>
+        );
+      })}
+    </select>
+  );
+}
