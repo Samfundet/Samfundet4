dade078b165b69b8759af5ecf47e68320fc9dc3d
Snorre Saether <snosaet@hotmail.com>
2022-10-13 21:34:43 +0200

    lagt til storybook og css

---
+++ frontend/src/Components/DropDown/DropDown.stories.tsx
@@ -0,0 +1,18 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { DropDown } from './DropDown';
+
+// Local component config.
+export default {
+  title: 'Components/DropDown',
+  component: DropDown,
+  args: {
+    name: 'name',
+  },
+} as ComponentMeta<typeof DropDown>;
+
+const Template: ComponentStory<typeof DropDown> = function (args) {
+  return <DropDown {...args}></DropDown>;
+};
+
+export const Basic = Template.bind({});
+Basic.args = { dropDownList: ['a', 'b'] };
