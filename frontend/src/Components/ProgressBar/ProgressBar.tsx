373592b052c3d35e7a591388d7b179d2e0643102
Emil Telstad <emil.telstad@gmail.com>
2022-10-13 19:36:46 +0200

    Add ProgressBar.

---
+++ frontend/src/Components/ProgressBar/ProgressBar.tsx
@@ -0,0 +1,42 @@
+import classNames from 'classnames';
+import { Children } from 'types';
+import styles from './ProgressBar.module.scss';
+
+type ProgressBarProps = {
+  className?: string;
+  children?: Children;
+  value?: number;
+  max?: number;
+};
+
+export function ProgressBar({ className, children, value, max }: ProgressBarProps) {
+  // Breakpoints:
+  const bpLower = 0.33;
+  const bpUpper = 0.67;
+
+  const ratio = value && max ? value / max : undefined;
+
+  // Progress states:
+  const isLow = ratio && ratio < bpLower;
+  const isMedium = ratio && bpLower <= ratio && ratio <= bpUpper;
+  const isHigh = ratio && bpUpper < ratio;
+  const isLoading = !isLow && !isMedium && !isHigh;
+
+  return (
+    <label>
+      {children}
+      <progress
+        className={classNames(className, styles.red, styles.progress_bar, {
+          [styles.red]: isLow,
+          [styles.orange]: isMedium,
+          [styles.green]: isHigh,
+          [styles.blue]: isLoading,
+        })}
+        value={value}
+        max={max}
+      >
+        {value}
+      </progress>
+    </label>
+  );
+}
--- frontend/src/Components/index.ts
+++ frontend/src/Components/index.ts
@@ -1,2 +1,4 @@
 export { Button } from './Button';
+export { ProgressBar } from './ProgressBar';
 export { RadioButton } from './RadioButton';
+
