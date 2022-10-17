e619fde28efa505bf7a6900485f779bde9fcbcb4
simensee <simensee@gmail.com>
2022-10-17 10:31:51 +0200

    connected events page to backend

--- frontend/src/Pages/EventsPage/EventsPage.tsx
+++ frontend/src/Pages/EventsPage/EventsPage.tsx
@@ -1,25 +1,52 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './EventsPage.module.scss';
 
 export function EventsPage() {
   const [events, setEvents] = useState([]);
-
+  const [dates, setDates] = useState<Date[]>([]);
   useEffect(() => {
-    fetch('http://localhost:8000/samfundet/api/events')
+    fetch('http://localhost:8000/samfundet/api/events/')
       .then((response) => response.json())
       .then((data) => {
-        setEvents(data);
+        const events = data;
+        setEvents(events);
+        console.log(events);
+        events.map((event: any) => {
+          if (dates.indexOf(event.start_dt) < 0) {
+            setDates((prevEvents) => [...prevEvents, event.start_dt]);
+            console.log('Added date: ' + event.start_dt.toString());
+          }
+        });
       });
-  }, []);
+  }, [dates]);
   return (
     <div className={styles.container}>
-      <div className={styles.dates_container}>
-        <div className={styles.events_container}>
-          <p>jdsjdksj</p>
-        </div>
-        <div className={styles.events_container}></div>
-        <div className={styles.events_container}></div>
-      </div>
+      {dates.map((date: any) => {
+        return (
+          // eslint-disable-next-line react/jsx-key
+          <div className={styles.dates_container}>
+            <div className={styles.events_container}>
+              <h2> {date.toString()} </h2>
+              {events.map((event: any) => {
+                if (event.start_dt == date) {
+                  {
+                    console.log(event.title);
+                  }
+                  // eslint-disable-next-line react/jsx-key
+                  return (
+                    // eslint-disable-next-line react/jsx-key
+                    <Link to={'/events/' + event.id}>
+                      {' '}
+                      <p> {event.title}</p>{' '}
+                    </Link>
+                  );
+                }
+              })}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
