053b5e3110e6be18df09d8f569bf2c6cefd2cf03
sindrelothe <sindrl@online.no>
2022-10-20 20:59:32 +0200

    make ordered and unorderd list more general

--- frontend/src/Components/List/List.tsx
+++ frontend/src/Components/List/List.tsx
@@ -6,21 +6,12 @@ type ListProps = {
 };
 
 export function List({ items, type = 'unordered' }: ListProps) {
+  function MakeList(element: string, index: number) {
+    return <li key={index}>{element}</li>;
+  }
   if (type === 'ordered') {
-    return (
-      <ol>
-        {items.map(function (element, index) {
-          return <li key={index}>{element}</li>;
-        })}
-      </ol>
-    );
+    return <ol>{items.map((element, index) => MakeList(element, index))}</ol>;
   } else {
-    return (
-      <ul>
-        {items.map(function (element, index) {
-          return <li key={index}>{element}</li>;
-        })}
-      </ul>
-    );
+    return <ul>{items.map((element, index) => MakeList(element, index))}</ul>;
   }
 }
