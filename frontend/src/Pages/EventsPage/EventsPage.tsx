2ef77e05104afd01e5ebdbbf992d8890c4b6f2e7
simensee <simensee@gmail.com>
2022-10-20 22:01:07 +0200

    sort events by dates without dupes

--- frontend/src/Pages/EventsPage/EventsPage.tsx
+++ frontend/src/Pages/EventsPage/EventsPage.tsx
@@ -19,93 +19,33 @@ const monthNames = [
 
 export function EventsPage() {
   const [events, setEvents] = useState([]);
-  const [prevDate, setPrevDate] = useState('');
   const [dates, setDates] = useState<string[]>([]);
   useEffect(() => {
-    console.log('Running useeffect');
     fetch('http://localhost:8000/samfundet/api/events/')
       .then((response) => response.json())
       .then((data) => {
         const events = data;
         setEvents(events);
-        console.log(events);
-        setDates([]);
+        const dates_arr: string[] = [];
         events.map((event: any) => {
-          //const isPresent = false;
-          //   dates.map((date: any) => {
-          //     console.log(
-          //       'Second part of if: ' + date.toString().toString().split('-')[0] + date.toString().split('-')[1],
-          //     );
-          //     if (
-          //       event.start_dt.toString().split('-')[0] + event.start_dt.toString().split('-')[1] ==
-          //       date.toString().toString().split('-')[0] + date.toString().split('-')[1]
-          //     ) {
-          //       isPresent = true;
-          //     } else {
-          //       console.log('is not present!');
-          //     }
-          //   });
-          console.log('prev date: ' + prevDate);
-          console.log(
-            'Index of bool: ' +
-              dates.indexOf(
-                event.start_dt.toString().split('-')[1] + '-' + event.start_dt.toString().split('-')[2].split('T')[0],
-              ),
-          );
-          if (
-            dates.indexOf(
-              event.start_dt.toString().split('-')[1] + '-' + event.start_dt.toString().split('-')[2].split('T')[0],
-            ) == -1 &&
-            event.start_dt.toString().split('-')[1] + '-' + event.start_dt.toString().split('-')[2].split('T')[0] !=
-              prevDate
-          ) {
-            setDates((prevEvents) => [
-              ...prevEvents,
-              event.start_dt.toString().split('-')[1] + '-' + event.start_dt.toString().split('-')[2].split('T')[0],
-            ]);
-            setPrevDate(
-              event.start_dt.toString().split('-')[1] + '-' + event.start_dt.toString().split('-')[2].split('T')[0],
-            );
-            console.log(
-              'Added date: ' +
-                event.start_dt.toString().split('-')[1] +
-                '-' +
-                event.start_dt.toString().split('-')[2].split('T')[0],
-            );
+          const dateStr =
+            event.start_dt.toString().split('-')[1] + '-' + event.start_dt.toString().split('-')[2].split('T')[0];
+          if (dates_arr.indexOf(dateStr) == -1) {
+            dates_arr.push(dateStr);
           }
         });
-        console.log(dates);
+        setDates(dates_arr);
       });
-    console.log(dates);
-    const tempArray: string[] = [];
-    dates.forEach((date: string) => {
-      console.log('date in foreach: ' + date);
-      if (tempArray.indexOf(date) == -1) {
-        tempArray.push(date);
-        console.log('Pushed date: ' + date);
-      }
-    });
-    setDates(tempArray);
   }, []);
   return (
     <div className={styles.container}>
-      {dates.map((date: any) => {
+      {dates.map((date: string) => {
         return (
           // eslint-disable-next-line react/jsx-key
           <div className={styles.dates_container}>
             <div className={styles.events_container}>
-              <h2>
-                {date}
-                {monthNames[Number(date.toString().split('-')[1]) - 1]}{' '}
-              </h2>
+              <h2>{date.split('-')[1] + '. ' + monthNames[Number(date.split('-')[0]) - 1]}</h2>
               {events.map((event: any) => {
-                console.log(
-                  'lol' +
-                    event.start_dt.toString().split('-')[1] +
-                    '-' +
-                    event.start_dt.toString().split('-')[2].split('T')[0],
-                );
-                console.log('date: ' + date);
                 if (
                   event.start_dt.toString().split('-')[1] +
                     '-' +
