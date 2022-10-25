c822ce2b9d9aec44c130d33430bd60c161e9cdf8
sindrelothe <sindrl@online.no>
2022-10-25 18:53:40 +0200

    add option to have bulletless lists

--- frontend/src/Components/List/List.tsx
+++ frontend/src/Components/List/List.tsx
@@ -1,4 +1,6 @@
-type ListType = 'ordered' | 'unordered';
+import styles from './List.module.scss';
+
+type ListType = 'ordered' | 'unordered' | 'no_bullets';
 
 type ListProps = {
   items: Array<string>;
@@ -11,6 +13,8 @@ export function List({ items, type = 'unordered' }: ListProps) {
   }
   if (type === 'ordered') {
     return <ol>{items.map((element, index) => MakeList(element, index))}</ol>;
+  } else if (type === 'no_bullets') {
+    return <ul className={styles.list_no_bullets}>{items.map((element, index) => MakeList(element, index))}</ul>;
   } else {
     return <ul>{items.map((element, index) => MakeList(element, index))}</ul>;
   }
