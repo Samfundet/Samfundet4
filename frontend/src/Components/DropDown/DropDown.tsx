868306ec08ba256f437d635b8edff38d7dbad74d
Snorre Saether <snosaet@hotmail.com>
2022-10-27 22:51:37 +0200

    Mulig ferdig med styling

--- frontend/src/Components/DropDown/DropDown.tsx
+++ frontend/src/Components/DropDown/DropDown.tsx
@@ -1,30 +1,29 @@
 import classnames from 'classnames';
 import styles from './DropDown.module.scss';
 
-type DropDownTheme = 'samf' | 'secondary';
+// type DropDownTheme = 'samf' | 'secondary';
 
 type DropDownProps = {
   className?: string;
   dropDownList?: Array<string>;
-  theme?: DropDownTheme;
+  //   theme?: DropDownTheme;
 };
-const mapThemeToStyle: { [theme in DropDownTheme]: string } = {
-  samf: styles.dropdown_samf,
-  secondary: styles.dropdown_secondary,
-};
-export function DropDown({ dropDownList, theme = 'samf', className }: DropDownProps) {
-  const classNames = classnames(mapThemeToStyle[theme], className);
+// const mapThemeToStyle: { [theme in DropDownTheme]: string } = {
+//   samf: styles.dropdown_samf,
+//   secondary: styles.dropdown_secondary,
+// };
+export function DropDown({ dropDownList, className }: DropDownProps) {
+  //theme = 'samf'
+  const classNames = classnames(className); //mapThemeToStyle[theme],
   return (
-    <div class="custom-select" style={{"width:200px"}}>
-      <select>
-        {dropDownList?.map(function (element, index) {
-          return (
-            <option value={element} key={index} className={classNames}>
-              {element}
-            </option>
-          );
-        })}
-      </select>
-    </div>
+    <select className={styles.samf_select}>
+      {dropDownList?.map(function (element, index) {
+        return (
+          <option value={element} key={index} className={classNames}>
+            {element}
+          </option>
+        );
+      })}
+    </select>
   );
 }
