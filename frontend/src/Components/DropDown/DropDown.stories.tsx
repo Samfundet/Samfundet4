920cff629e1af9deb81f331e980c6990f7c4ac9d
Snorre Saether <snosaet@hotmail.com>
2022-11-03 20:10:53 +0100

    lagt til stort

--- frontend/src/Components/DropDown/DropDown.stories.tsx
+++ frontend/src/Components/DropDown/DropDown.stories.tsx
@@ -16,4 +16,33 @@ const Template: ComponentStory<typeof DropDown> = function (args) {
 };
 
 export const Basic = Template.bind({});
-Basic.args = { dropDownList: ['alternativ 1', 'alternativ 2'] };
+Basic.args = {
+  option: ['alternativ 1', 'alternativ 2'],
+};
+export const Many = Template.bind({});
+Many.args = {
+  option: [
+    'alternativ 1',
+    'alternativ 2',
+    'alternativ 3',
+    'alternativ 4',
+    'alternativ 5',
+    'alternativ 6',
+    'alternativ 7',
+    'alternativ 8',
+    'alternativ 9',
+    'alternativ 10',
+    'alternativ 11',
+    'alternativ 12',
+    'alternativ 13',
+    'alternativ 14',
+    'alternativ 15',
+    'alternativ 16',
+    'alternativ 17',
+    'alternativ 18',
+    'alternativ 19',
+    'alternativ 20',
+    'alternativ 21',
+    'alternativ 22',
+  ],
+};
--- frontend/src/Components/DropDown/DropDown.tsx
+++ frontend/src/Components/DropDown/DropDown.tsx
@@ -1,28 +1,19 @@
 import classnames from 'classnames';
 import styles from './DropDown.module.scss';
 
-// type DropDownTheme = 'samf' | 'secondary';
-
 type DropDownProps = {
   className?: string;
-  dropDownList?: Array<string>;
+  option?: string[];
   label?: string;
-
-  //   theme?: DropDownTheme;
 };
 
-// const mapThemeToStyle: { [theme in DropDownTheme]: string } = {
-//   samf: styles.dropdown_samf,
-//   secondary: styles.dropdown_secondary,
-// };
-export function DropDown({ dropDownList, className, label }: DropDownProps) {
-  //theme = 'samf'
-  const classNames = classnames(className); //mapThemeToStyle[theme],
+export function DropDown({ option, className, label }: DropDownProps) {
+  const classNames = classnames(className);
   return (
     <label className={styles.select_wrapper}>
       {label}
       <select className={styles.samf_select}>
-        {dropDownList?.map(function (element, index) {
+        {option?.map(function (element, index) {
           return (
             <option value={element} key={index} className={classNames}>
               {element}
