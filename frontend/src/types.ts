0397bdda5a95e1d077b24f9a809748b8d6cfcbff
simensee <simensee@gmail.com>
2022-11-03 12:52:30 +0100

    add types for event, months and days

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -39,3 +39,68 @@ export type Color = typeof COLORS;
 export type ColorKey = keyof Color;
 /** Easy type when adding setStates to Context. */
 export type SetState<T> = Dispatch<SetStateAction<T>>;
+/** Type for event */
+export type Event = {
+  id: number;
+  title_no: string;
+  title_en: string;
+  start_dt: Date;
+  end_dt: Date;
+  description_long_no: string;
+  description_long_en: string;
+  description_short_no: string;
+  description_short_en: string;
+  publish_dt: Date;
+  host: string;
+  location: string;
+};
+
+export const monthNamesNo = {
+  1: 'Januar',
+  2: 'Februar',
+  3: 'Mars',
+  4: 'April',
+  5: 'Mai',
+  6: 'Juni',
+  7: 'Juli',
+  8: 'August',
+  9: 'September',
+  10: 'Oktober',
+  11: 'November',
+  12: 'Desember',
+};
+
+export const monthNamesEn = {
+  1: 'January',
+  2: 'February',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'Juni',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December',
+};
+
+export const weekDayNamesNo = {
+  1: 'Mandag',
+  2: 'Tirsdag',
+  3: 'Onsdag',
+  4: 'Torsdag',
+  5: 'Fredag',
+  6: 'Lørdag',
+  7: 'Søndag',
+};
+
+export const weekDayNamesEn = {
+  1: 'Monday',
+  2: 'Tuesday',
+  3: 'Wednesday',
+  4: 'Thursday',
+  5: 'Friday',
+  6: 'Saturday',
+  7: 'Sunday',
+};
