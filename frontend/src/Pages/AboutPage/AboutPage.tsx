e1ef62bd240fcd36421ef7f3a829fb3a052d6a3f
eiriksho <hoemeirik@gmail.com>
2022-11-03 16:21:52 +0100

    Clean up code and import images directly

--- frontend/src/Pages/AboutPage/AboutPage.tsx
+++ frontend/src/Pages/AboutPage/AboutPage.tsx
@@ -1,16 +1,4 @@
 import styles from './AboutPage.module.scss';
-import bodegaen from '../../assets/bodegaen.jpg';
-import daglighallen from '../../assets/daglighallen.jpg';
-import edgar from '../../assets/edgar.jpg';
-import klubben from '../../assets/klubben.jpg';
-import knaus from '../../assets/knaus.jpg';
-import lyche from '../../assets/lyche.jpg';
-import runderode from '../../assets/runderode.jpg';
-import rundhallen from '../../assets/rundhallen.jpg';
-import selskapssiden from '../../assets/selskapssiden.jpg';
-import splash from '../../assets/splash.jpeg';
-import storsalen from '../../assets/storsalen.jpg';
-import strossa from '../../assets/strossa.jpg';
 import { Button } from 'Components';
 import { ROUTES } from 'routes';
 
@@ -18,7 +6,7 @@ export function AboutPage() {
   return (
     <div className={styles.container}>
       <div className={styles.about}>
-        <img src={runderode} alt="Runderode" className={styles.runderode} />
+        <img src={require('../../assets/runderode.jpg')} alt="Runderode" className={styles.runderode} />
         <div className={styles.aboutText}>
           <h2>Om Samfundet</h2>
           <p>
@@ -28,10 +16,10 @@ export function AboutPage() {
             kafé og en restaurant. Mest sagnomsust er Samfundsmøtene, viet til debatt om politikk og aktuelle spørsmål,
             eller til underholdning og moro. Samfundet har også tre av Trondheims beste konsertscener.
           </p>
-          <Button>MEDLEMSKAP</Button>
-          <Button>FAQ</Button>
-          <Button>KONTAKTINFO</Button>
-          <Button>BILLETTER</Button>
+          <Button className={styles.button}>MEDLEMSKAP</Button>
+          <Button className={styles.button}>FAQ</Button>
+          <Button className={styles.button}>KONTAKTINFO</Button>
+          <Button className={styles.button}>BILLETTER</Button>
         </div>
       </div>
 
@@ -44,69 +32,83 @@ export function AboutPage() {
           foregår i henhold til norske og interne lover. Samfundet har en daglig leder, økonomiansvarlig, husøkonom,
           vaktmester og renholdspersonell som er ansatte.
         </p>
-        <Button>SAMFUNDET SOM ORGANISASJON</Button>
-        <Button theme="secondary">SAKSDOKUMENTER</Button>
-        <Button theme="secondary">SAMFUNDETS HISTORIE</Button>
+        <Button className={styles.button}>SAMFUNDET SOM ORGANISASJON</Button>
+        <Button className={styles.button} theme="secondary">
+          SAKSDOKUMENTER
+        </Button>
+        <Button className={styles.button} theme="secondary">
+          SAMFUNDETS HISTORIE
+        </Button>
       </div>
       <div className={styles.venues}>
         <h1>LOKALER</h1>
         <a href={ROUTES.frontend.storsalen}>
-          <img src={storsalen} alt="Storsalen" className={styles.venueImg}></img>
+          <img src={require('../../assets/storsalen.jpg')} alt="Storsalen" className={styles.venueImg}></img>
           <h3>Storsalen</h3>
         </a>
         <a href={ROUTES.frontend.bodegaen}>
-          <img src={bodegaen} alt="Bodegaen" className={styles.venueImg}></img>
+          <img src={require('../../assets/bodegaen.jpg')} alt="Bodegaen" className={styles.venueImg}></img>
           <h3>Bodegaen</h3>
         </a>
         <a href={ROUTES.frontend.klubben}>
-          <img src={klubben} alt="Klubben" className={styles.venueImg}></img>
+          <img src={require('../../assets/klubben.jpg')} alt="Klubben" className={styles.venueImg}></img>
           <h3>Klubben</h3>
         </a>
         <a href={ROUTES.frontend.strossa}>
-          <img src={strossa} alt="Strossa" className={styles.venueImg}></img>
+          <img src={require('../../assets/strossa.jpg')} alt="Strossa" className={styles.venueImg}></img>
           <h3>Strossa</h3>
         </a>
         <a href={ROUTES.frontend.selskapssiden}>
-          <img src={selskapssiden} alt="Selskapssiden" className={styles.venueImg}></img>
+          <img src={require('../../assets/selskapssiden.jpg')} alt="Selskapssiden" className={styles.venueImg}></img>
           <h3>Selskapssiden</h3>
         </a>
         <a href={ROUTES.frontend.knaus}>
-          <img src={knaus} alt="Knaus" className={styles.venueImg}></img>
+          <img src={require('../../assets/knaus.jpg')} alt="Knaus" className={styles.venueImg}></img>
           <h3>Knaus</h3>
         </a>
         <a href={ROUTES.frontend.edgar}>
-          <img src={edgar} alt="Edgar" className={styles.venueImg}></img>
+          <img src={require('../../assets/edgar.jpg')} alt="Edgar" className={styles.venueImg}></img>
           <h3>Edgar</h3>
         </a>
         <a href={ROUTES.frontend.lyche}>
-          <img src={lyche} alt="Lyche" className={styles.venueImg}></img>
+          <img src={require('../../assets/lyche.jpg')} alt="Lyche" className={styles.venueImg}></img>
           <h3>Lyche</h3>
         </a>
         <a href={ROUTES.frontend.daglighallen}>
-          <img src={daglighallen} alt="Daglighallen" className={styles.venueImg}></img>
+          <img src={require('../../assets/daglighallen.jpg')} alt="Daglighallen" className={styles.venueImg}></img>
           <h3>Daglighallen</h3>
         </a>
         <a href={ROUTES.frontend.rundhallen}>
-          <img src={rundhallen} alt="Rundhallen" className={styles.venueImg}></img>
+          <img src={require('../../assets/rundhallen.jpg')} alt="Rundhallen" className={styles.venueImg}></img>
           <h3>Rundhallen</h3>
         </a>
       </div>
-      <div className={styles.mapButton}>
-        <Button theme="secondary">OVERSIKTSKART</Button>
-        <Button theme="secondary">NYBYGG</Button>
+      <div>
+        <Button className={styles.button} theme="secondary">
+          OVERSIKTSKART
+        </Button>
+        <Button className={styles.button} theme="secondary">
+          NYBYGG
+        </Button>
       </div>
       <div className={styles.moreInfo}>
         <div className={styles.volunteer}>
-          <img src={splash} alt="Splash" className={styles.splash} />
-          <h2>Frivilligheten</h2>
-          <p>
-            Det meste av arbeid på Studentersamfundet i Trondhjem gjøres gjennom dugnad av studenter. Arbeidet er
-            organisert i enheter som kalles gjenger. Potensielle nye medlemmer må søke den aktuelle gjengen om opptak,
-            og gjengen vurderer hver enkelt søker. Med sine omtrent 1700 frivillige utgjør det indre miljøet i
-            Studentersamfundet en betydelig del av det organiserte fritidstilbudet til studenter i Trondheim.
-          </p>
-          <Button theme="secondary">GJENGENE PÅ SAMFUNDET</Button>
-          <Button theme="secondary">OPPTAK</Button>
+          <img src={require('../../assets/splash.jpeg')} alt="Splash" className={styles.splash} />
+          <div className={styles.volunteerText}>
+            <h2>Frivilligheten</h2>
+            <p>
+              Det meste av arbeid på Studentersamfundet i Trondhjem gjøres gjennom dugnad av studenter. Arbeidet er
+              organisert i enheter som kalles gjenger. Potensielle nye medlemmer må søke den aktuelle gjengen om opptak,
+              og gjengen vurderer hver enkelt søker. Med sine omtrent 1700 frivillige utgjør det indre miljøet i
+              Studentersamfundet en betydelig del av det organiserte fritidstilbudet til studenter i Trondheim.
+            </p>
+            <Button className={styles.button} theme="secondary">
+              GJENGENE PÅ SAMFUNDET
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              OPPTAK
+            </Button>
+          </div>
         </div>
         <div className={styles.moreInfoRight}>
           <div className={styles.festival}>
@@ -115,19 +117,35 @@ export function AboutPage() {
               Annenhvert år arrangeres Norges største kulturfestival UKA og verdens største internasjonale tematiske
               studentfestival under Studentersamfundets paraply.
             </p>
-            <Button>UKA</Button>
-            <Button>ISFiT</Button>
+            <Button className={styles.button}>UKA</Button>
+            <Button className={styles.button}>ISFiT</Button>
           </div>
           <div className={styles.otherInfo}>
             <h2>Annen info</h2>
-            <Button theme="secondary">Aldersgrenser</Button>
-            <Button theme="secondary">Booking</Button>
-            <Button theme="secondary">Quiz</Button>
-            <Button theme="secondary">Leie og tjenester</Button>
-            <Button theme="secondary">Presse</Button>
-            <Button theme="secondary">Filmklubben</Button>
-            <Button theme="secondary">Personvern</Button>
-            <Button theme="secondary">Tilrettelegging</Button>
+            <Button className={styles.button} theme="secondary">
+              Aldersgrenser
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              Booking
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              Quiz
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              Leie og tjenester
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              Presse
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              Filmklubben
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              Personvern
+            </Button>
+            <Button className={styles.button} theme="secondary">
+              Tilrettelegging
+            </Button>
           </div>
         </div>
       </div>
