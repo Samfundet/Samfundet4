c9ec411f807cb687c94d39701ed95d262289ca4e
sindrelothe <sindrl@online.no>
2022-11-08 21:17:45 +0100

    add height and width options for image

--- frontend/src/Components/Image/Image.tsx
+++ frontend/src/Components/Image/Image.tsx
@@ -1,9 +1,11 @@
 type ImageProps = {
   src: string;
+  height?: number;
+  width?: number;
   alt?: string;
   className?: string;
 };
 
-export function Image({ src, alt, className }: ImageProps) {
-  return <img src={src} alt={alt} className={className} />;
+export function Image({ src, height, width, alt, className }: ImageProps) {
+  return <img src={src} height={height} width={width} alt={alt} className={className} />;
 }
