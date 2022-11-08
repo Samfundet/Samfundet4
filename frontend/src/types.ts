bee43ed1b17407bcd7992b8a83b2076787306003
Snorre Saether <snosaet@hotmail.com>
2022-11-08 20:44:53 +0100

    Lagt til types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -9,3 +9,34 @@ export type Children = ReactNode;
 
 /** Easy type when adding setStates to Context. */
 export type SetState<T> = Dispatch<SetStateAction<T>>;
+
+/**Type for label alignment */
+export type AlignLabel = 'top' | 'right' | 'bottom' | 'left';
+
+/**Type for input placeholders  */
+export type Placeholder =
+  | 'Navn'
+  | 'E-post'
+  | 'Nummer'
+  | 'Alder'
+  | 'Passord'
+  | 'Adresse'
+  | 'DD/MM/YYY'
+  | 'Medlemsnummer'
+  | '19:00'
+  | 'Lokale'
+  | 'Arrangement';
+
+/***Type for input default value  */
+export type DefaultValue =
+  | 'Navn'
+  | 'E-post'
+  | 'Nummer'
+  | 'Alder'
+  | 'Passord'
+  | 'Adresse'
+  | 'DD/MM/YYY'
+  | 'Medlemsnummer'
+  | '19:00'
+  | 'Lokale'
+  | 'Arrangement';
