bfe8cbdbfe9c77bcf6a63fbad60c41ed86837c12
Snorre Skjellestad Kristiansen <snorre.ssk@gmail.com>
2022-11-08 21:48:23 +0100

    update style for button and change theme logic

--- frontend/src/Components/Button/Button.tsx
+++ frontend/src/Components/Button/Button.tsx
@@ -2,7 +2,7 @@ import classnames from 'classnames';
 import { ButtonType, Children } from '~/types';
 import styles from './Button.module.scss';
 
-type ButtonTheme = 'samf' | 'secondary';
+type ButtonTheme = 'basic' | 'samf' | 'secondary';
 
 type ButtonProps = {
   name?: string;
@@ -15,12 +15,13 @@ type ButtonProps = {
 };
 
 const mapThemeToStyle: { [theme in ButtonTheme]: string } = {
+  basic: styles.button_basic,
   samf: styles.button_samf,
   secondary: styles.button_secondary,
 };
 
-export function Button({ name, type, theme = 'samf', onClick, disabled, className, children }: ButtonProps) {
-  const classNames = classnames(mapThemeToStyle[theme], className);
+export function Button({ name, type, theme = 'basic', onClick, disabled, className, children }: ButtonProps) {
+  const classNames = classnames(styles.button, mapThemeToStyle[theme], className);
   return (
     <button name={name} onClick={onClick} type={type} disabled={disabled} className={classNames}>
       {children}
