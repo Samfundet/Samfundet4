db3039e26574e6b2fcf8fe20dd80ebe557503f39
Emil Telstad <emil.telstad@gmail.com>
2022-11-14 00:03:54 +0100

    Add rest_framework urls.

--- backend/root/urls.py
+++ backend/root/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest_framework/', include('rest_framework.urls')),
     path('', include('samfundet.urls')),  # Put last.
 ]
 
