2267e285b244662496d7b5206fbe83da46638eeb
Emil Telstad <emil.telstad@gmail.com>
2022-11-14 21:22:03 +0100

    Add permission constants.

---
+++ backend/samfundet/permissions.py
@@ -0,0 +1,38 @@
+### auth ###
+AUTH_VIEW_CONTENTTYPES = 'auth.view_contenttypes'
+AUTH_ADD_CONTENTTYPES = 'auth.add_contenttypes'
+AUTH_CHANGE_CONTENTTYPES = 'auth.change_contenttypes'
+AUTH_DELETE_CONTENTTYPES = 'auth.delete_contenttypes'
+
+### guardian ###
+GUARDIAN_VIEW_GROUPOBJECTPERMISSION = 'guardian.view_groupobjectpermission'
+GUARDIAN_ADD_GROUPOBJECTPERMISSION = 'guardian.add_groupobjectpermission'
+GUARDIAN_CHANGE_GROUPOBJECTPERMISSION = 'guardian.change_groupobjectpermission'
+GUARDIAN_DELETE_GROUPOBJECTPERMISSION = 'guardian.delete_groupobjectpermission'
+
+GUARDIAN_VIEW_USEROBJECTPERMISSION = 'guardian.view_userobjectpermission'
+GUARDIAN_ADD_USEROBJECTPERMISSION = 'guardian.add_userobjectpermission'
+GUARDIAN_CHANGE_USEROBJECTPERMISSION = 'guardian.change_userobjectpermission'
+GUARDIAN_DELETE_USEROBJECTPERMISSION = 'guardian.delete_userobjectpermission'
+
+### sessions ###
+SESSIONS_VIEW_SESSION = 'sessions.view_session'
+SESSIONS_ADD_SESSION = 'sessions.add_session'
+SESSIONS_CHANGE_SESSION = 'sessions.change_session'
+SESSIONS_DELETE_SESSION = 'sessions.delete_session'
+
+### samfundet ###
+SAMFUNDET_VIEW_EVENT = 'samfundet.view_event'
+SAMFUNDET_ADD_EVENT = 'samfundet.add_event'
+SAMFUNDET_CHANGE_EVENT = 'samfundet.change_event'
+SAMFUNDET_DELETE_EVENT = 'samfundet.delete_event'
+
+SAMFUNDET_VIEW_EVENTGROUP = 'samfundet.view_eventgroup'
+SAMFUNDET_ADD_EVENTGROUP = 'samfundet.add_eventgroup'
+SAMFUNDET_CHANGE_EVENTGROUP = 'samfundet.change_eventgroup'
+SAMFUNDET_DELETE_EVENTGROUP = 'samfundet.delete_eventgroup'
+
+SAMFUNDET_VIEW_VENUE = 'samfundet.view_venue'
+SAMFUNDET_ADD_VENUE = 'samfundet.add_venue'
+SAMFUNDET_CHANGE_VENUE = 'samfundet.change_venue'
+SAMFUNDET_DELETE_VENUE = 'samfundet.delete_venue'
