40eca942cec7e0c9c9c95f52c40cba1e27de6fc2
Snorre Skjellestad Kristiansen <snorre.ssk@gmail.com>
2022-11-14 21:05:55 +0100

    Add textAlignment to Alert

--- frontend/src/Components/Alert/Alert.tsx
+++ frontend/src/Components/Alert/Alert.tsx
@@ -9,17 +9,19 @@ type AlertProps = {
   title?: string;
   className?: 'string';
   closable?: boolean;
+  align?: 'center' | 'left' | 'right';
 };
 
-export function Alert({ message, type = 'info', title, className, closable = false }: AlertProps) {
+export function Alert({ message, type = 'info', title, className, closable = false, align = 'left' }: AlertProps) {
   const [closed, setClosed] = useState(false);
-  const classNames = classnames(styles[type], className, styles.alert);
+  const classNames = classnames(styles[type], className, styles.alert, align === 'right' && styles.rightWrapper);
   return (
     <>
       {!closed && (
         <div className={classNames}>
-          <div>
-            <p className={styles.title}>{title}</p>
+          {closable && align === 'center' && <div className={styles.offset} />}
+          <div className={styles[align]}>
+            {title && <p className={styles.title}>{title}</p>}
             <p>{message}</p>
           </div>
           {closable && (
