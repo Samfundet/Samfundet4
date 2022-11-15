8fdb2f6cf8c9b9c9b8cb5ce3b2826597eb63fc12
Snorre Skjellestad Kristiansen <snorre.ssk@gmail.com>
2022-11-15 18:44:04 +0100

    Add skeleton

--- frontend/src/Pages/HomePage/HomePage.tsx
+++ frontend/src/Pages/HomePage/HomePage.tsx
@@ -1,25 +1,24 @@
-import { getAllPermissions, getCsrfToken, getUser, login, logout } from '~/api';
-import logo from '~/assets/logo_black.png';
-import splash from '~/assets/splash.jpeg';
-import { Button } from '~/Components';
 import styles from './HomePage.module.scss';
 
 export function HomePage() {
   return (
-    <div className={styles.container}>
-      <img src={splash} alt="Splash" className={styles.splash} />
-      <div className={styles.content}>
-        <img src={logo} alt="Logo" className={styles.logo} />
-        <h1>Velkommen til Samfundet og MG::Web!</h1>
-        <p>
-          Gratulerer! Du har nå fått tutorial-prosjektet opp å kjøre, og alt ser ut til å fungere som det skal! Det
-          første som er smart å gjøre er å utforske koden og bli litt kjent med hvordan ting er satt opp.
-        </p>
-        <Button onClick={() => getCsrfToken()}>csrf</Button>
-        <Button onClick={() => login('emilte', 'Django123')}>login</Button>
-        <Button onClick={() => getUser()}>user</Button>
-        <Button onClick={() => getAllPermissions()}>perms</Button>
-        <Button onClick={() => logout()}>logout</Button>
+    <div className={styles.containter}>
+      <div>
+        <h1>BannerLock</h1>
+      </div>
+      <div>
+        <h3>Alert</h3>
+      </div>
+      <div>
+        <div>
+          <h2>Hva skjer</h2>
+        </div>
+        <div>
+          <h2>Åpningstider</h2>
+        </div>
+      </div>
+      <div>
+        <h1>Arrangmenter</h1>
       </div>
     </div>
   );
