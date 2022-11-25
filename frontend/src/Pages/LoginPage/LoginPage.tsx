74d188e542c55297c6bea88a766326304484a87c
Mathias Aas <mathias.aas123@gmail.com>
2022-11-25 15:33:03 +0100

    Add login connection to backend

--- frontend/src/Pages/LoginPage/LoginPage.tsx
+++ frontend/src/Pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Link } from 'react-router-dom';
-import { Button, InputField } from '~/Components';
+import { Link, useNavigate } from 'react-router-dom';
+import { login } from '~/api';
+import { Alert, Button, InputField } from '~/Components';
 import { KEY } from '~/i18n/constants';
 import { ROUTES } from '~/routes';
 import styles from './LoginPage.module.scss';
@@ -10,22 +11,41 @@ export function LoginPage() {
   const { t } = useTranslation();
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
+  const [loginFailed, setLoginFailed] = useState(false);
+  const navigate = useNavigate();
   function handleLogin() {
-    //TODO: add function logic
+    login(name, password)
+      .then((token) => {
+        token === 202 ? navigate(ROUTES.frontend.home) : setLoginFailed(true);
+      })
+      .catch(() => {
+        setLoginFailed(true);
+      });
   }
 
   return (
     <div className={styles.container}>
+      {loginFailed && (
+        <Alert
+          message="Login failed"
+          type="error"
+          align="center"
+          closable={true}
+          onClose={() => {
+            setLoginFailed(false);
+          }}
+        ></Alert>
+      )}
       <div className={styles.content_container}>
         <h1 className={styles.header_text}>{t(KEY.login_internal_login)}</h1>
         <InputField
           className={styles.input_field}
-          placeholder={t(KEY.login_email_placeholder)}
+          placeholder={t(KEY.login_email_placeholder) || undefined}
           onChange={(e) => setName(e ? e.currentTarget.value : '')}
         />
         <InputField
           className={styles.input_field}
-          placeholder={t(KEY.common_password)}
+          placeholder={t(KEY.common_password) || undefined}
           type="password"
           onChange={(e) => setPassword(e?.currentTarget.value || '')}
         />
