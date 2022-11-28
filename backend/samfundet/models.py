b14b620bf7186361ce91e5e37728df58744ad9d1
Emil Telstad <emil.telstad@gmail.com>
2022-11-28 21:21:06 +0100

    Add models for menues.
    Co-authored-by: Sigve Røkenes <evgiz@users.noreply.github.com>

--- backend/samfundet/models.py
+++ backend/samfundet/models.py
@@ -174,10 +174,10 @@ class InformationPage(models.Model):
 
 class Table(models.Model):
     name_no = models.CharField(max_length=64, unique=True, blank=True, null=True, verbose_name='Navn (norsk)')
-    description_no = models.CharField(max_length=64, blank=True, null=True, verbose_name='Tittel (norsk)')
+    description_no = models.CharField(max_length=64, blank=True, null=True, verbose_name='Beskrivelse (norsk)')
 
     name_en = models.CharField(max_length=64, blank=True, null=True, verbose_name='Navn (engelsk)')
-    description_en = models.CharField(max_length=64, blank=True, null=True, verbose_name='Tittel (engelsk)')
+    description_en = models.CharField(max_length=64, blank=True, null=True, verbose_name='Beskrivelse (engelsk)')
 
     seating = models.PositiveSmallIntegerField(blank=True, null=True)
 
@@ -187,6 +187,61 @@ class Table(models.Model):
     class Meta:
         verbose_name = 'Table'
         verbose_name_plural = 'Tables'
+
+    def __str__(self) -> str:
+        return f'{self.name_no}'
+
+
+class FoodPreference(models.Model):
+    name_no = models.CharField(max_length=64, unique=True, blank=True, null=True, verbose_name='Navn (norsk)')
+    name_en = models.CharField(max_length=64, blank=True, null=True, verbose_name='Navn (engelsk)')
+
+    def __str__(self) -> str:
+        return f'{self.name_no}'
+
+
+class FoodCategory(models.Model):
+    name_no = models.CharField(max_length=64, unique=True, blank=True, null=True, verbose_name='Navn (norsk)')
+    name_en = models.CharField(max_length=64, blank=True, null=True, verbose_name='Navn (engelsk)')
+    order = models.PositiveSmallIntegerField(blank=True, null=True, unique=True)
+
+    def __str__(self) -> str:
+        return f'{self.name_no}'
+
+
+class MenuItem(models.Model):
+    name_no = models.CharField(max_length=64, unique=True, blank=True, null=True, verbose_name='Navn (norsk)')
+    description_no = models.TextField(blank=True, null=True, verbose_name='Beskrivelse (norsk)')
+
+    name_en = models.CharField(max_length=64, blank=True, null=True, verbose_name='Navn (engelsk)')
+    description_en = models.TextField(blank=True, null=True, verbose_name='Beskrivelse (engelsk)')
+
+    price = models.PositiveSmallIntegerField(blank=True, null=True)
+    price_member = models.PositiveSmallIntegerField(blank=True, null=True)
+
+    order = models.PositiveSmallIntegerField(blank=True, null=True, unique=True)
+    food_preferences = models.ManyToManyField(FoodPreference, blank=True)
+
+    class Meta:
+        verbose_name = 'MenuItem'
+        verbose_name_plural = 'MenuItems'
+
+    def __str__(self) -> str:
+        return f'{self.name_no}'
+
+
+class Menu(models.Model):
+    name_no = models.CharField(max_length=64, unique=True, blank=True, null=True, verbose_name='Navn (norsk)')
+    description_no = models.TextField(blank=True, null=True, verbose_name='Beskrivelse (norsk)')
+
+    name_en = models.CharField(max_length=64, blank=True, null=True, verbose_name='Navn (engelsk)')
+    description_en = models.TextField(blank=True, null=True, verbose_name='Beskrivelse (engelsk)')
+
+    menu_items = models.ManyToManyField(MenuItem, blank=True)
+
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menus'
 
     def __str__(self) -> str:
         return f'{self.name_no}'
