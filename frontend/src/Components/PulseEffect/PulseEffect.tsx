57e8a73243483320930e951524c9f319084401ee
Emil Telstad <emil.telstad@gmail.com>
2022-12-13 02:05:18 +0100

    Add component PulseEffect.

---
+++ frontend/src/Components/PulseEffect/PulseEffect.tsx
@@ -0,0 +1,26 @@
+import classnames from 'classnames';
+import { THEME } from '~/constants';
+import { useGlobalContext } from '~/GlobalContextProvider';
+import { Children } from '~/types';
+import styles from './PulseEffect.module.scss';
+
+type PulseEffectProps = {
+  children: Children;
+  className?: string;
+};
+
+export function PulseEffect({ children, className }: PulseEffectProps) {
+  const { theme } = useGlobalContext();
+  const isDarkTheme = theme === THEME.DARK;
+  const isLightTheme = theme === THEME.LIGHT;
+  return (
+    <span
+      className={classnames(className, {
+        [styles.ripple_theme_dark]: isDarkTheme,
+        [styles.ripple_theme_light]: isLightTheme,
+      })}
+    >
+      <span className={classnames(styles.pulse)}>{children}</span>
+    </span>
+  );
+}
