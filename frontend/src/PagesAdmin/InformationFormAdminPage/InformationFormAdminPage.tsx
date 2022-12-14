43eaca27dc0321c90ad2d8b33955880896780bde
magsyg <magsyg2@gmail.com>
2022-12-14 14:17:48 +0100

    add put to informationpage form

--- frontend/src/PagesAdmin/InformationFormAdminPage/InformationFormAdminPage.tsx
+++ frontend/src/PagesAdmin/InformationFormAdminPage/InformationFormAdminPage.tsx
@@ -7,9 +7,10 @@ import { useAuthContext } from '~/AuthContext';
 import { useTranslation } from 'react-i18next';
 import { KEY, LANGUAGES } from '~/i18n/constants';
 import { ROUTES } from '~/routes';
+import { useParams } from 'react-router-dom';
 import styles from './InformationFormAdminPage.module.scss';
 import { InformationPageDto } from '~/dto';
-import {  postInformationPage } from '~/api';
+import {  getInformationPage, postInformationPage, putInformationPage } from '~/api';
 import { STATUS } from '~/http_status_codes';
 import { reverse } from '~/named-urls';
 
@@ -25,11 +26,35 @@ export function InformationFormAdminPage() {
   const [titleEn, setTitleEn] = useState<string>('');
   const [textEn, setTextEn] = useState<string>('');
 
+  // If form has a slugfield, check if it exists, and then load that item.
+  const { slugField } = useParams();
+
   // Stuff to do on first render.
   //TODO add permissions on render
   useEffect(() => {
     setShowSpinner(false);
   }, []);
+
+  useEffect(() => {
+    // TODO add fix on no slugfield on editpage
+    if (slugField) {
+      getInformationPage(slugField)
+        .then((data) => {
+          setName(data.slug_field);
+          setTitleNo(data.title_no);
+          setTitleEn(data.title_en);
+          setTextEn(data.text_en);
+          setTextNo(data.text_no);
+        }).catch((data) => {
+          console.log(data);
+          // TODO add error pop up message?
+          if (data.request.status === STATUS.HTTP_404_NOT_FOUND) {
+            navigate(ROUTES.frontend.admin_information);
+          }
+          
+        });
+    }
+  }, [slugField]);
 
   if (showSpinner) {
     return (
@@ -38,29 +63,33 @@ export function InformationFormAdminPage() {
       </div>
     );
   }
-  // TODO ADD TRANSLATIONS pr element
 
   function post(event: SyntheticEvent) {
     event.preventDefault();
     const data = {
-      slug_field: name,
       title_no: titleNo,
-      title_en: titleNo,
+      title_en: titleEn,
       text_no: textNo,
-      text_en: textNo,
+      text_en: textEn,
     };
-    postInformationPage(data)
-      .then((status) => {
-        navigate(ROUTES.frontend.information_page_list + data.slug_field);
-        if (status === STATUS.HTTP_201_CREATED) {
-          navigate(ROUTES.frontend.information_page_list);
-        } else {
-          console.log(status);
-        }
-      })
-      .catch((e) => {
-        console.log(e);
+    if (slugField) {
+      putInformationPage(slugField, data)
+        .then((status) => {
+            navigate(ROUTES.frontend.information_page_list + slugField);
+        })
+        .catch((e) => {
+          console.log(e);
       });
+    } else {
+      data.slug_field = name;
+      postInformationPage(data)
+        .then((status) => {
+          navigate(ROUTES.frontend.information_page_list + data.slug_field);
+        })
+        .catch((e) => {
+          console.log(e);
+      });
+    }
   }
 
   return (
@@ -72,12 +101,14 @@ export function InformationFormAdminPage() {
       >
         <p>{t(KEY.back)}</p>
       </Button>
-      <h1>{t(KEY.admin_information_new_page)}</h1>
+      <h1>{slugField ? t(KEY.admin_information_edit_page) : t(KEY.admin_information_new_page)}</h1>
       <form onSubmit={post}>
-        <div className={styles.inputGroup}>
+        {!slugField && (
           <InputField value={name} onChange={(e) => setName(e ? e.currentTarget.value : '')}>
             <p className={styles.labelText}>{t(KEY.name)}</p>
           </InputField>
+        )}
+        <div className={styles.inputGroup}>
           <h2>{t(KEY.norwegian)}</h2>
           <InputField value={titleNo} onChange={(e) => setTitleNo(e ? e.currentTarget.value : '')}>
             <p className={styles.labelText}>
@@ -90,9 +121,24 @@ export function InformationFormAdminPage() {
             </p>
           </TextAreaField>
         </div>
+        <div className={styles.inputGroup}>
+          <h2>{t(KEY.english)}</h2>
+          <InputField value={titleEn} onChange={(e) => setTitleEn(e ? e.currentTarget.value : '')}>
+            <p className={styles.labelText}>
+              {t(KEY.english)} {t(KEY.title)}
+            </p>
+          </InputField>
+          <TextAreaField value={textEn} onChange={(e) => setTextEn(e ? e.currentTarget.value : '')}>
+            <p className={styles.labelText}>
+              {t(KEY.english)} {t(KEY.content)}
+            </p>
+          </TextAreaField>
+        </div>
         <div className={styles.submitContainer}>
           <Button theme={'success'} type='submit'>
-            <p className={styles.submit}>{t(KEY.admin_information_create_page)}</p>
+            <p className={styles.submit}>
+              {slugField ? t(KEY.admin_information_update_page) : t(KEY.admin_information_create_page)}
+            </p>
           </Button>
         </div>
       </form>
