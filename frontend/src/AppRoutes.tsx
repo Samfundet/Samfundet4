e8ebbaf69f5bf5123892cde4b4a64548714bc03e
Mathias Aas <mathias.aas123@gmail.com>
2023-01-12 11:32:28 +0100

    Add Saksdokument box to admin page

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import { useGoatCounter } from '~/hooks';
 import {
   AboutPage,
+  AdminPage,
   ApiTestingPage,
   ComponentPage,
+  EventPage,
+  EventsPage,
   GroupsPage,
   HealthPage,
   HomePage,
@@ -12,18 +14,16 @@ import {
   InformationPage,
   LoginPage,
   LychePage,
-  AdminPage,
-  EventPage,
-  EventsPage,
 } from '~/Pages';
 import {
-  EventsAdminPage,
   EventFormAdminPage,
+  EventsAdminPage,
   GangsAdminPage,
   GangsFormAdminPage,
   InformationAdminPage,
   InformationFormAdminPage,
 } from '~/PagesAdmin';
+import { useGoatCounter } from '~/hooks';
 import { ROUTES } from './routes';
 
 export function AppRoutes() {
