ef7d50760c219d7a7cc3751e90156e7dfadc3334
robines <espi.robin@gmail.com>
2023-02-22 16:37:53 +0100

    yapf fix

--- backend/samfundet/admin.py
+++ backend/samfundet/admin.py
@@ -53,7 +53,8 @@ admin.site.unregister(Group)
 class UserAdmin(CustomGuardedUserAdmin):
     sortable_by = ['id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'date_joined', 'updated_at']
     list_display = [
-        'id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'group_memberships', 'last_login', 'date_joined', 'updated_at'
+        'id', 'username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'group_memberships', 'last_login', 'date_joined',
+        'updated_at'
     ]
     list_display_links = ['id', 'username']
     list_select_related = True
@@ -217,7 +218,9 @@ class VenueAdmin(CustomGuardedModelAdmin):
     # ordering = []
     sortable_by = ['id', 'name', 'floor', 'last_renovated', 'handicapped_approved', 'responsible_crew', 'opening', 'closing', 'created_at', 'updated_at']
     list_filter = ['handicapped_approved']
-    list_display = ['id', '__str__', 'name', 'floor', 'last_renovated', 'handicapped_approved', 'responsible_crew', 'opening', 'closing', 'created_at', 'updated_at']
+    list_display = [
+        'id', '__str__', 'name', 'floor', 'last_renovated', 'handicapped_approved', 'responsible_crew', 'opening', 'closing', 'created_at', 'updated_at'
+    ]
     search_fields = ['id', 'name', 'responsible_crew']
     # filter_horizontal = []
     list_display_links = ['id', '__str__']
