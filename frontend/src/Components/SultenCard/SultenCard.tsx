8b5c43bc07f7cb0447f55a740b4b5efa344a4cf8
Mathias Aas <mathias.aas123@gmail.com>
2023-03-08 16:37:42 +0100

    Create card for sulten landing page content

---
+++ frontend/src/Components/SultenCard/SultenCard.tsx
@@ -0,0 +1,36 @@
+import { useMobile } from '~/hooks';
+import { SultenButton } from '../SultenButton';
+import styles from './SultenCard.module.scss';
+
+type SultenCardProps = {
+  header: string;
+  image: string;
+  imageAlt: string;
+  text: string;
+  buttonText?: string;
+  imageAlignment: 'left' | 'right';
+};
+
+export function SultenCard({ header, image, imageAlt, text, buttonText, imageAlignment = 'left' }: SultenCardProps) {
+  const imageLeft = imageAlignment === 'left';
+  const isMobile = useMobile();
+
+  const leftAlignedImage = (imageLeft || isMobile) && (
+    <img src={image} alt={imageAlt} className={styles.card_image}></img>
+  );
+  const rightAlignedImage = !imageLeft && !isMobile && (
+    <img src={image} alt={imageAlt} className={styles.card_image}></img>
+  );
+
+  return (
+    <div className={styles.container}>
+      {leftAlignedImage}
+      <div className={styles.text_container}>
+        <h2 className={styles.card_header}>{header}</h2>
+        <p className={styles.card_text}>{text}</p>
+        {buttonText && <SultenButton className={styles.card_button}>{buttonText}</SultenButton>}
+      </div>
+      {rightAlignedImage}
+    </div>
+  );
+}
