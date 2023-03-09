29012f11829b2e44d09bfdf9d2c0e07f8c366b65
Sigve Røkenes <sigve@Sigves-MacBook-Pro-2.local>
2023-03-09 21:09:52 +0100

    wip

--- frontend/src/Components/SplashHeaderBox/SplashHeaderBox.tsx
+++ frontend/src/Components/SplashHeaderBox/SplashHeaderBox.tsx
@@ -1,49 +1,43 @@
 import { Icon } from '@iconify/react';
 import classnames from 'classnames';
-import { useAuthContext } from '~/AuthContext';
-import { useGlobalContext } from '~/GlobalContextProvider';
 import { useScrollY } from '~/hooks';
 import styles from './SplashHeaderBox.module.scss';
 
 type SplashHeaderBoxProps = {
+  className?: string;
 };
 
-export function SplashHeaderBox({  }: SplashHeaderBoxProps) {
-  const { switchTheme, theme } = useGlobalContext();
-  const { user } = useAuthContext();
-
+export function SplashHeaderBox({ className }: SplashHeaderBoxProps) {
   const scrollY = useScrollY();
 
   const containerScrollSpeed = -0.1;
   const containerTranslation = scrollY * containerScrollSpeed;
-  const containerStyle = {
-    transform: "translateY(" + containerTranslation + "px)"
-  }
-  
+  const containerTransform = 'translateY(' + containerTranslation + 'px)';
+
   return (
-    <div className={styles.container} style={containerStyle}>
-        <div className={classnames(styles.box, styles.box_left)}>
-          <h2 className={styles.title_left}>
-            <Icon icon="bx:party"/>
-            Hva skjer?
-          </h2>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-        </div>
-        <div className={classnames(styles.box, styles.box_right)}>
-          <h2 className={styles.title_right}>
-            <Icon icon="material-symbols:calendar-month-rounded"/>
-            Åpningstider
-          </h2>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-          <p>10:13 - Some weird event</p>
-        </div>
+    <div className={classnames(styles.container, className)} style={{ transform: containerTransform }}>
+      <div className={classnames(styles.box, styles.box_left)}>
+        <h2 className={styles.title_left}>
+          <Icon icon="bx:party" />
+          Hva skjer?
+        </h2>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+      </div>
+      <div className={classnames(styles.box, styles.box_right)}>
+        <h2 className={styles.title_right}>
+          <Icon icon="material-symbols:calendar-month-rounded"></Icon>
+          Åpningstider
+        </h2>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+        <p>10:13 - Some weird event</p>
+      </div>
     </div>
   );
 }
