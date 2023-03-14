ca4daa7b8e612d8c0751e11de07aec32f32b99c8
Emil Telstad <emil.telstad@gmail.com>
2023-03-14 18:58:37 +0100

    Fix permission classes.

--- backend/root/custom_classes/permission_classes.py
+++ backend/root/custom_classes/permission_classes.py
@@ -1,8 +1,19 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from rest_framework.permissions import DjangoObjectPermissions
+
+if TYPE_CHECKING:
+    from rest_framework.views import APIView
+    from rest_framework.request import Request
+    from django.db.models import Model, QuerySet
+    from samfundet.models import User
 
 
 class CustomDjangoObjectPermissions(DjangoObjectPermissions):
     """Add django permissions to read methods."""
+
     perms_map = {
         'GET': ['%(app_label)s.view_%(model_name)s'],
         'OPTIONS': [],
@@ -12,3 +23,18 @@ class CustomDjangoObjectPermissions(DjangoObjectPermissions):
         'PATCH': ['%(app_label)s.change_%(model_name)s'],
         'DELETE': ['%(app_label)s.delete_%(model_name)s'],
     }
+
+    def has_permission(self, request: Request, view: APIView) -> bool:
+        queryset: QuerySet = self._queryset(view)
+        model_cls: Model = queryset.model
+        user: User = request.user
+
+        if not user.is_authenticated:
+            return False
+
+        if request.method == 'POST':
+            post_perms: list[str] = self.get_required_permissions(method='POST', model_cls=model_cls)
+            has_perm = user.has_perms(perm_list=post_perms)
+            return has_perm
+
+        return True
