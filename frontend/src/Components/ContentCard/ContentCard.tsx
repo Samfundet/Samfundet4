50a3f1baba939665640d81b3ef9c36da11c55c0f
Sigve Røkenes <evgiz@users.noreply.github.com>
2023-03-16 22:11:43 +0100

    Home page complete API integration, fix event model (#427)
    * Event improvements and home page api integrations
    
    * Safe when no events, fix migrations
    
    * Update event ts dto
    
    * Fix dup docstring
    
    ---------
    
    Co-authored-by: Sigve Røkenes <sigve.rokenes@gmail.com>

--- frontend/src/Components/ContentCard/ContentCard.tsx
+++ frontend/src/Components/ContentCard/ContentCard.tsx
@@ -2,14 +2,20 @@ import { Icon } from '@iconify/react';
 import classnames from 'classnames';
 import { useId } from 'react';
 import { useScreenCenterOffset } from '~/hooks';
+import { backgroundImageFromUrl } from '~/utils';
 import { Button } from '../Button';
 import styles from './ContentCard.module.scss';
 
 type ContentCardProps = {
   className?: string;
+  title: string;
+  description: string;
+  buttonText: string;
+  url?: string;
+  imageUrl?: string;
 };
 
-export function ContentCard({ className }: ContentCardProps) {
+export function ContentCard({ className, title, description, buttonText, url, imageUrl }: ContentCardProps) {
   const id = useId();
   const scrollY = useScreenCenterOffset(id);
 
@@ -18,20 +24,21 @@ export function ContentCard({ className }: ContentCardProps) {
   const containerTransform = `translateY(${scrollContainer}px)`;
   const infoTransform = `translateY('${scrollInfo}px)`;
 
+  function followLink(url: string) {
+    window.location.href = url;
+  }
+
   return (
     <div className={classnames(styles.container, className)} style={{ transform: containerTransform }} id={id}>
       <div className={styles.card}>
-        <div className={styles.card_image} />
+        <div className={styles.card_image} style={backgroundImageFromUrl(imageUrl)} />
         <div className={styles.card_info} style={{ transform: infoTransform }}>
-          <div className={styles.info_header}>Et arrangement</div>
-          <div className={styles.info_description}>
-            Lorem ipsum dolor sit amet, lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet Indeed, it is lorem ipsum
-            dolor sit amet, lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet
-          </div>
+          <div className={styles.info_header}>{title}</div>
+          <div className={styles.info_description}>{description}</div>
           <div className={styles.info_bottom_row}>
-            <Button rounded={true} theme="black">
+            <Button rounded={true} theme="black" onClick={() => followLink(url ?? '#')}>
               <div className={styles.button_content}>
-                Kjøp billett
+                <span>{buttonText}</span>
                 <Icon icon="mdi:arrow-right" width={18} />
               </div>
             </Button>
