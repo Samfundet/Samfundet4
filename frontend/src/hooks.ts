df8b89b66dd8a33d6fde33d1a404af6dc15265ad
Sigve Røkenes <sigve.rokenes@gmail.com>
2023-03-22 21:40:45 +0100

    Hook docstrings

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -56,7 +56,10 @@ export function useMobile(): boolean {
   return width < mobileBpUpper;
 }
 
-// Scroll detection
+/**
+ * Scroll detection hook
+ * @returns the current y scroll
+ */
 export function useScrollY(): number {
   const [scrollY, setScrollY] = useState(window.scrollY);
   useEffect(() => {
@@ -74,7 +77,11 @@ export function useScrollY(): number {
   return scrollY;
 }
 
-// Element offset from screen center (id of html element)
+/**
+ * Element offset from screen center (id of html element)
+ * @param id id of the html element
+ * @returns pixel offset from centre of screen
+ */
 export function useScreenCenterOffset(id: string): number {
   const element = document.getElementById(id);
   const rect = element?.getBoundingClientRect();
@@ -101,7 +108,11 @@ export function useScreenCenterOffset(id: string): number {
   return positionY;
 }
 
-// Utility hook to get the previous value of react state
+/**
+ * Utility hook to get the previous value of react state
+ * @param value tcurrent state
+ * @returns previous state after next render
+ */
 export function usePrevious<T>(value: T): T | undefined {
   const ref = useRef<T>();
   useEffect(() => {
