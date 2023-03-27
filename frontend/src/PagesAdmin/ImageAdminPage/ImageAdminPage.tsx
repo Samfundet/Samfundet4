2e5cdc0aab9830f5fbd699c185b55826f8b8a31c
Sigve Røkenes <sigve.rokenes@gmail.com>
2023-03-27 22:51:17 +0200

    Cleanup image admin

--- frontend/src/PagesAdmin/ImageAdminPage/ImageAdminPage.tsx
+++ frontend/src/PagesAdmin/ImageAdminPage/ImageAdminPage.tsx
@@ -48,13 +48,12 @@ export function ImageAdminPage() {
           View in backend
         </Link>
       </div>
-      <Button theme="success" onClick={() => navigate(ROUTES.frontend.admin_images_create)}>
-        {t(KEY.admin_images_create)}
-      </Button>
-      <div className={styles.line} />
-      <ImageQuery allImages={allImages} setImages={setImages} />
-      <div className={styles.line} />
-      <h2 className={styles.subHeader}>{t(KEY.common_results)}</h2>
+      <div className={styles.action_row}>
+        <Button theme="success" rounded={true} onClick={() => navigate(ROUTES.frontend.admin_images_create)}>
+          {t(KEY.admin_images_create)}
+        </Button>
+        <ImageQuery allImages={allImages} setImages={setImages} />
+      </div>
       <div className={styles.imageContainer}>
         {images.map(function (element, key) {
           return <AdminImage key={key} image={element} className={styles.imageBox} />;
