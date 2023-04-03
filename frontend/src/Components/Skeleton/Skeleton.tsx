0891787bbf8e5e926af283d046095c5161797d9a
Emil Telstad <emil.telstad@gmail.com>
2023-04-03 03:33:12 +0200

    Set default colors on Skeleton.

--- frontend/src/Components/Skeleton/Skeleton.tsx
+++ frontend/src/Components/Skeleton/Skeleton.tsx
@@ -1,7 +1,18 @@
 import OriginalSkeleton, { SkeletonProps } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css';
+import { useIsDarkTheme } from '~/hooks';
+import { COLORS } from '~/types';
 
 /** Simple wrapper around package to import required css file. */
 export function Skeleton(props: SkeletonProps) {
-  return <OriginalSkeleton {...props} />;
+  const isDarkTheme = useIsDarkTheme();
+  const baseColor = isDarkTheme ? COLORS.theme_dark_input_bg : '#ddd';
+  const highlightColor = isDarkTheme ? '#333' : '#eee';
+  return (
+    <OriginalSkeleton
+      {...props}
+      baseColor={props.baseColor || baseColor}
+      highlightColor={props.highlightColor || highlightColor}
+    />
+  );
 }
