ac24e95f87d025a0243382661f1bf8883efeec43
Emil Telstad <emil.telstad@gmail.com>
2023-04-08 05:23:41 +0200

    Add hook for mouse position.

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -181,3 +181,21 @@ export function useIsLightTheme(): boolean {
   const { theme } = useGlobalContext();
   return theme === THEME.LIGHT;
 }
+
+export function useMousePosition(): { x: number; y: number } {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    function handleMouseMove(e: MouseEvent) {
+      setPosition({ x: e.clientX, y: e.clientY });
+    }
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  return position;
+}
