e420a0b330cbf8e087295d54993ed3a293be45b2
Sigve Røkenes <sigve.rokenes@gmail.com>
2023-04-10 12:21:02 +0200

    Move db router and fix model name to use meta

--- backend/root/db_router.py
+++ backend/root/db_router.py
@@ -40,6 +40,6 @@ class SamfundetDatabaseRouter:
         model_name: str | None = None,
         **hints: dict[str, Any],
     ) -> bool:
-        if model_name in [m.__name__ for m in BILLIG_MODELS]:
+        if model_name in [m._meta.model_name for m in BILLIG_MODELS]:
             return False
         return True
