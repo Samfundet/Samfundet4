c12cb1258015d07b529583f95dc7c86123e4c589
robines <espi.robin@gmail.com>
2023-04-13 18:54:09 +0200

    Add description in postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,3 +1,7 @@
+/**
+ * This enables PostCSS with the autoprefixer plugin. This plugin automatically adds CSS vendor prefixes
+ * Read more here: https://github.com/postcss/autoprefixer
+ */
 module.exports = {
   plugins: {
     autoprefixer: {},
