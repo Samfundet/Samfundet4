e2321b90b71a499d4a5b67a63fce0372fe4aa69b
Mathias Aas <mathias.aas123@gmail.com>
2023-04-14 16:56:26 +0200

    Move email sending variables to correct docker env

--- backend/root/settings/base.py
+++ backend/root/settings/base.py
@@ -310,7 +310,7 @@ APPEND_SLASH = True
 # ======================== #
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
