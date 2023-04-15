48041f560fa38263adbffa7062d6380ac87de2b6
Sigve Røkenes <sigve.rokenes@gmail.com>
2023-04-15 17:28:19 +0200

    Revert testing to master

--- frontend/src/Pages/HealthPage/HealthPage.tsx
+++ frontend/src/Pages/HealthPage/HealthPage.tsx
@@ -1,7 +1,4 @@
-import { DynamicBuildingMap } from '~/Components/DynamicBuildingMap';
-
 /** Page used by cypress to check healthy rendering of frontend. */
 export function HealthPage() {
-  return <DynamicBuildingMap />;
   return <span data-cy="health">I&apos;m alive!!</span>;
 }
