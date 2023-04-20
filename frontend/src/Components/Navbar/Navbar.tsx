6d6a7c4f0311943cf47910e4362b9712f88dce33
Johanne GG <johanne.gjedrem@icloud.com>
2023-04-20 19:40:43 +0200

    trying to merge conflic

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,153 +1,189 @@
-import classNames from 'classnames';
+import { Icon } from '@iconify/react';
+import { default as classNames } from 'classnames';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { NavLink as Link, useNavigate } from 'react-router-dom';
-import { logout } from '~/api';
-import { englishFlag, logoWhite, norwegianFlag, profileIcon } from '~/assets';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuthContext } from '~/AuthContext';
-import { Button, ThemeSwitch } from '~/Components';
-import { useDesktop } from '~/hooks';
+import { Button, Link, ThemeSwitch } from '~/Components';
+import { NavbarItem } from '~/Components/Navbar/components';
+import { HamburgerMenu } from '~/Components/Navbar/components/HamburgerMenu';
+import { useGlobalContext } from '~/GlobalContextProvider';
+import { logout } from '~/api';
+import { englishFlag, logoBlack, logoWhite, norwegianFlag } from '~/assets';
+import { useDesktop, useIsDarkTheme, useScrollY } from '~/hooks';
 import { STATUS } from '~/http_status_codes';
 import { KEY, LANGUAGES } from '~/i18n/constants';
 import { ROUTES } from '~/routes';
 import styles from './Navbar.module.scss';
-import { useGlobalContext } from '../../GlobalContextProvider';
+
+const scrollDistanceForOpaque = 30;
 
 export function Navbar() {
-  const { mobileNavigation, setMobileNavigation } = useGlobalContext();
+  const isDarkTheme = useIsDarkTheme();
+  const { isMobileNavigation, setIsMobileNavigation } = useGlobalContext();
   const { t, i18n } = useTranslation();
   const { user, setUser } = useAuthContext();
   const navigate = useNavigate();
   const isDesktop = useDesktop();
 
-  function languageImage() {
-    if (i18n.language == LANGUAGES.NB) {
-      return (
-        <img
-          src={englishFlag}
-          className={isDesktop ? styles.navbar_language_flag : styles.popup_change_language}
-          onClick={() => i18n.changeLanguage(LANGUAGES.EN)}
-        />
-      );
-    }
-    return (
-      <img
-        src={norwegianFlag}
-        className={isDesktop ? styles.navbar_language_flag : styles.popup_change_language}
-        onClick={() => i18n.changeLanguage(LANGUAGES.NB)}
-      />
-    );
-  }
+  // Each NavbarItem can have a dropdown menu.
+  // We want only one of them to be extended at any time, therefore this parent component
+  // is given the responsibility of managing this.
+  // Store the label of the currently selected dropdown.
+  const [expandedDropdown, setExpandedDropdown] = useState('');
 
-  // Return profile button for navbar if logged in
+  // Language
+  const currentLanguage = i18n.language;
+  const isNorwegian = currentLanguage === LANGUAGES.NB;
+  const otherLanguage = isNorwegian ? LANGUAGES.EN : LANGUAGES.NB;
+  const otherFlag = isNorwegian ? englishFlag : norwegianFlag;
+
+  // Scroll detection.
+  const scrollY = useScrollY();
+  const isScrolledNavbar = scrollY > scrollDistanceForOpaque;
+
+  // Navbar style.
+  const isRootPath = useLocation().pathname === ROUTES.frontend.home;
+  const isTransparentNavbar = isRootPath && !isScrolledNavbar && !isMobileNavigation;
+  const navbarLogo = isDarkTheme || isTransparentNavbar ? logoWhite : logoBlack;
+
+  useEffect(() => {
+    // Close expanded dropdown menu whenever mobile navbar is closed, or we switch from mobile to desktop, like when
+    // switching from portrait to landscape on iPad.
+    if (!isMobileNavigation || isDesktop) {
+      setExpandedDropdown('');
+    }
+  }, [isMobileNavigation, isDesktop]);
+
+  const languageButton = (
+    <img src={otherFlag} className={styles.language_flag} onClick={() => i18n.changeLanguage(otherLanguage)} />
+  );
+
+  // Return profile button for navbar if logged in.
   const profileButton = (
-    <div className={isDesktop ? styles.navbar_profile_button : styles.popup_profile}>
-      <img src={profileIcon} className={styles.profile_icon}></img>
-      <Link to={ROUTES.frontend.admin} className={styles.profile_text}>
+    <div className={styles.navbar_profile_button}>
+      <Icon icon="material-symbols:person"></Icon>
+      <Link url={ROUTES.frontend.admin} className={styles.profile_text}>
         {user?.username}
       </Link>
     </div>
   );
 
-  // return hamburger menu icon
-  const hamburgerMenu = (
-    <div
-      id={styles.navbar_hamburger}
-      onClick={() => setMobileNavigation(!mobileNavigation)}
-      className={mobileNavigation ? styles.open : styles.closed}
-    >
-      <div className={classNames(styles.navbar_hamburger_line, styles.top)} />
-      <div className={classNames(styles.navbar_hamburger_line, styles.middle)} />
-      <div className={classNames(styles.navbar_hamburger_line, styles.bottom)} />
-    </div>
+  const infoLinks = (
+    <>
+      <Link
+        url={ROUTES.frontend.about}
+        className={styles.navbar_dropdown_link}
+        onAfterClick={() => setExpandedDropdown('')}
+      >
+        {t(KEY.common_about_samfundet)}
+      </Link>
+      <a href="#" className={styles.navbar_dropdown_link} onClick={() => setExpandedDropdown('')}>
+        {t(KEY.common_membership)}
+      </a>
+      <a href="#" className={styles.navbar_dropdown_link} onClick={() => setExpandedDropdown('')}>
+        {t(KEY.common_opening_hours)}
+      </a>
+      <Link
+        url={ROUTES.frontend.venues}
+        className={styles.navbar_dropdown_link}
+        onAfterClick={() => setExpandedDropdown('')}
+      >
+        {t(KEY.navbar_map)}
+      </Link>
+      <a
+        href={ROUTES.other.foto_samfundet_no}
+        className={styles.navbar_dropdown_link}
+        onClick={() => setExpandedDropdown('')}
+      >
+        {t(KEY.navbar_photos)}
+      </a>
+      <a href="#" className={styles.navbar_dropdown_link} onClick={() => setExpandedDropdown('')}>
+        {t(KEY.navbar_nybygg)}
+      </a>
+    </>
   );
 
   const navbarHeaders = (
     <>
-      <Link
-        to={ROUTES.frontend.events}
-        className={isDesktop ? styles.navbar_link : styles.popup_link_mobile}
-        onClick={() => setMobileNavigation(false)}
-      >
-        {t(KEY.common_event)}
-      </Link>
-      <Link
-        to={ROUTES.frontend.about}
-        className={isDesktop ? styles.navbar_link : styles.popup_link_mobile}
-        onClick={() => setMobileNavigation(false)}
-      >
-        {t(KEY.common_information)}
-      </Link>
-      <Link
-        to={ROUTES.frontend.sulten}
-        className={isDesktop ? styles.navbar_link : styles.popup_link_mobile}
-        onClick={() => setMobileNavigation(false)}
-      >
-        {t(KEY.common_restaurant)}
-      </Link>
-      <Link
-        to={ROUTES.frontend.health}
-        className={isDesktop ? styles.navbar_link : styles.popup_link_mobile}
-        onClick={() => setMobileNavigation(false)}
-      >
-        {t(KEY.common_volunteer)}
-      </Link>
+      <NavbarItem
+        setExpandedDropdown={setExpandedDropdown}
+        expandedDropdown={expandedDropdown}
+        route={ROUTES.frontend.events}
+        label={t(KEY.common_event)}
+      />
+      <NavbarItem
+        setExpandedDropdown={setExpandedDropdown}
+        expandedDropdown={expandedDropdown}
+        route={'#'}
+        label={t(KEY.common_information)}
+        dropdownLinks={infoLinks}
+      />
+      <NavbarItem
+        setExpandedDropdown={setExpandedDropdown}
+        expandedDropdown={expandedDropdown}
+        route={ROUTES.frontend.sulten}
+        label={t(KEY.common_restaurant)}
+      />
+      <NavbarItem
+        setExpandedDropdown={setExpandedDropdown}
+        expandedDropdown={expandedDropdown}
+        route={ROUTES.frontend.health}
+        label={t(KEY.common_volunteer)}
+      />
     </>
   );
 
-  const loginButtons = (
-    <>
-      <Button
-        theme="samf"
-        className={isDesktop ? styles.navbar_member_button : styles.popup_member_button}
-        onClick={() => {
-          navigate(ROUTES.frontend.login);
-          setMobileNavigation(false);
-        }}
-      >
-        {t(KEY.common_member)}
-      </Button>
-      {/* Show login button */}
-      {!user && (
-        <Button
-          theme="secondary"
-          className={isDesktop ? undefined : styles.popup_internal_button}
-          onClick={() => {
-            navigate(ROUTES.frontend.login);
-            setMobileNavigation(false);
-          }}
-        >
-          {t(KEY.common_internal)}
-        </Button>
-      )}
-      {/* Show logout button */}
-      {user && (
-        <Button
-          theme="secondary"
-          className={isDesktop ? undefined : styles.popup_internal_button}
-          onClick={() => {
-            logout()
-              .then((response) => {
-                response.status === STATUS.HTTP_200_OK && setUser(undefined);
-              })
-              .catch(console.error);
+  const isLightLoginButton = isDarkTheme || (isTransparentNavbar && !isMobileNavigation);
 
-            setMobileNavigation(false);
-          }}
-        >
-          {t(KEY.common_logout)}
-        </Button>
-      )}
-    </>
+  const loginButton = !user && (
+    <Button
+      theme={isLightLoginButton ? 'white' : 'black'}
+      rounded={true}
+      className={isDesktop ? styles.login_button : styles.popup_internal_button}
+      onClick={() => {
+        navigate(ROUTES.frontend.login);
+        setIsMobileNavigation(false);
+      }}
+    >
+      {t(KEY.common_login)}
+    </Button>
+  );
+
+  const logoutButton = user && (
+    <Button
+      theme={isLightLoginButton ? 'white' : 'black'}
+      rounded={true}
+      className={isDesktop ? undefined : styles.popup_internal_button}
+      onClick={() => {
+        logout()
+          .then((response) => {
+            response.status === STATUS.HTTP_200_OK && setUser(undefined);
+          })
+          .catch(console.error);
+
+        setIsMobileNavigation(false);
+      }}
+    >
+      {t(KEY.common_logout)}
+    </Button>
   );
 
   // Show mobile popup for navigation
-  const showMobileNavigation = (
+  const mobileNavigation = (
     <>
       <nav id={styles.mobile_popup_container}>
         {navbarHeaders}
-        <br />
-        {languageImage()}
-        {loginButtons}
+
+        <div className={styles.mobile_widgets}>
+          {languageButton}
+          <div className={styles.mobile_user}>
+            {loginButton}
+            {logoutButton}
+          </div>
+          <ThemeSwitch />
+        </div>
+        <br></br>
         {user && profileButton}
       </nav>
     </>
@@ -155,21 +191,24 @@ export function Navbar() {
 
   return (
     <>
-      <div className={styles.navbar_padding} />
-      <nav id={styles.navbar_container}>
-        <Link to="/">
-          <img src={logoWhite} id={styles.navbar_logo} />
-        </Link>
-        {isDesktop && navbarHeaders}
-        <div className={styles.navbar_signup}>
-          <ThemeSwitch />
-          {user && profileButton}
-          {languageImage()}
-          {loginButtons}
+      <nav id={styles.navbar_container} className={classNames(isTransparentNavbar && styles.transparent_navbar)}>
+        <div className={styles.navbar_inner}>
+          <Link url={ROUTES.frontend.home} className={styles.navbar_logo}>
+            <img src={navbarLogo} id={styles.navbar_logo_img} />
+          </Link>
+          {isDesktop && navbarHeaders}
+          <div className={styles.navbar_widgets}>
+            {user && profileButton}
+
+            <ThemeSwitch />
+            {languageButton}
+            {loginButton}
+            {logoutButton}
+          </div>
+          <HamburgerMenu transparentBackground={isTransparentNavbar} />
         </div>
-        {hamburgerMenu}
       </nav>
-      {mobileNavigation && showMobileNavigation}
+      {isMobileNavigation && mobileNavigation}
     </>
   );
 }
