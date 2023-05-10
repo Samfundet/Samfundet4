e8fb6bc8f7ac374c2b5da40bf8422f2b792f0232
Mathias Aas <mathias.aas123@gmail.com>
2023-05-10 17:07:46 +0200

    Add todos, fix linting

--- backend/samfundet/views.py
+++ backend/samfundet/views.py
@@ -320,6 +320,7 @@ class AssignGroupView(APIView):
      Assigns a user to a group.
     """
 
+    # TODO Add functionality so that users can only assign themselves to groups they are a part of / are parent to.
     permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
@@ -327,15 +328,18 @@ class AssignGroupView(APIView):
         group_name = request.data.get('group_name')
 
         if not username or not group_name:
-            return Response({"error": "Username and group_name fields are required."}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'error': 'Username and group_name fields are required.'}, status=status.HTTP_400_BAD_REQUEST)
 
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            return Response({"error": "User not found."}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'error': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
 
-        group, created = Group.objects.get_or_create(name=group_name)
+        try: 
+            group = Group.objects.get(name=group_name)
+        except Group.DoesNotExist:
+            return Response({'error': 'Group not found.'}, status=status.HTTP_404_NOT_FOUND)
         user.groups.add(group)
         user.save()
 
-        return Response({"message": f"User '{username}' added to group '{group_name}'."}, status=status.HTTP_200_OK)
+        return Response({'message': f"User '{username}' added to group '{group_name}'."}, status=status.HTTP_200_OK)
