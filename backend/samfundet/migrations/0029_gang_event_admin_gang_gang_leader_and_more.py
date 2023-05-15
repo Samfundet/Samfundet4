9b24aa27f38fa6f14bd33bf5d712a9e553b57f09
Mathias Aas <mathias.aas123@gmail.com>
2023-05-15 23:19:34 +0200

    fix migrations and incorrect import

--- backend/samfundet/migrations/0029_gang_event_admin_gang_gang_leader_and_more.py
+++ backend/samfundet/migrations/0029_gang_event_admin_gang_gang_leader_and_more.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('samfundet', '0027_infobox'),
+        ('samfundet', '0028_notification'),
     ]
 
     operations = [
