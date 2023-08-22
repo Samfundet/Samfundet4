6ae4ad1528a918bc53303e3dd5e92a232eb74199
Emil Telstad <emil.telstad@gmail.com>
2023-08-22 15:18:29 +0200

    Add cmdk.

--- frontend/src/Components/Link/Link.tsx
+++ frontend/src/Components/Link/Link.tsx
@@ -1,10 +1,11 @@
 import classNames from 'classnames';
 import { CSSProperties } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useGlobalContext } from '~/GlobalContextProvider';
 import { BACKEND_DOMAIN } from '~/constants';
+import { useCustomNavigate } from '~/hooks';
 import { Children } from '~/types';
 import styles from './Link.module.scss';
+
+export type LinkTarget = 'frontend' | 'backend' | 'external' | 'email';
 
 export type LinkProps = {
   className?: string;
@@ -13,7 +14,7 @@ export type LinkProps = {
   url: string;
   title?: string;
   plain?: boolean;
-  target?: 'frontend' | 'backend' | 'external' | 'email';
+  target?: LinkTarget;
   onAfterClick?: () => void;
   children?: Children;
 };
@@ -29,37 +30,13 @@ export function Link({
   target = 'frontend',
   onAfterClick,
 }: LinkProps) {
-  const navigate = useNavigate();
-  const { setIsMobileNavigation } = useGlobalContext();
+  const navigate = useCustomNavigate();
   const finalUrl = target === 'backend' ? BACKEND_DOMAIN + url : url;
 
   function handleClick(event: React.MouseEvent) {
-    // Stop default <a> tag onClick handling. We want custom behaviour depending on the target.
-    event.preventDefault();
+    navigate({ linkTarget: target, url: url, event: event });
 
-    // Even though nested <a> tags are illegal, they might occur.
-    // To prevent multiple link clicks on overlaying elements, stop propagation upwards.
-    event.stopPropagation();
-
-    // Close mobile menu if originates from there
-    setIsMobileNavigation(false);
-
-    /** Detected desire to open the link in a new tab.
-     * True if ctrl or cmd click.
-     */
-    const isCmdClick = event.ctrlKey || event.metaKey;
-
-    // React navigation.
-    if (target === 'frontend' && !isCmdClick) {
-      navigate(url);
-    }
-    // Normal change of href to trigger reload.
-    else if (target === 'backend' && !isCmdClick) window.location.href = finalUrl;
-    else if (target === 'email') window.location.href = url;
-    // Open in new tab.
-    else window.open(finalUrl, '_blank');
-
-    // External callback can add additional functionality on click
+    // External callback can add additional functionality on click.
     onAfterClick?.();
   }
   return (
