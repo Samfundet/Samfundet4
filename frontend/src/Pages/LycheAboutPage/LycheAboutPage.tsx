be0128f471651b742cda668614cd4b421a1c5fe8
Snorre Sæther <snosaet@hotmail.com>
2023-09-05 22:34:16 +0200

    fixed formating

--- frontend/src/Pages/LycheAboutPage/LycheAboutPage.tsx
+++ frontend/src/Pages/LycheAboutPage/LycheAboutPage.tsx
@@ -1,14 +1,8 @@
 import { useTranslation } from 'react-i18next';
-
 import { SultenPage } from '~/Components/SultenPage';
-import { useTextItem } from '~/hooks'; // endre??
+import { useTextItem } from '~/hooks';
 import { KEY } from '~/i18n/constants';
-// import { toast } from 'react-toastify';
-// import { ROUTES } from '~/routes';
 import styles from './LycheAboutPage.module.scss';
-// import { useState, useEffect } from 'react';
-// import { getVenues } from '~/api';
-// import { VenueDto } from '~/dto';
 import { TextItem } from '~/constants';
 import { SultenCard } from '~/Components';
 import { burger, soup, dessert } from '~/assets';
@@ -16,31 +10,31 @@ import { burger, soup, dessert } from '~/assets';
 export function LycheAboutPage() {
   const { t } = useTranslation();
 
-  const aboutCard1 = (
+  const aboutCardWhatIsLyche = (
     <SultenCard
       image={soup}
       imageAlt={'Chef'}
-      header={t(KEY.sulten_what_is_lyche)} //hva er lyche
+      header={t(KEY.sulten_what_is_lyche)}
       text={useTextItem(TextItem.sulten_what_is_lyche_text)}
       imageAlignment="right"
       smallCard={true}
     />
   );
-  const aboutCard2 = (
+  const aboutCardLycheGoal = (
     <SultenCard
       image={burger}
       imageAlt={'Chef'}
-      header={t(KEY.sulten_lyche_goal)} //hva er lyche
+      header={t(KEY.sulten_lyche_goal)}
       text={useTextItem(TextItem.sulten_lyche_goal_text)}
       imageAlignment="left"
       smallCard={true}
     />
   );
-  const aboutCard3 = (
+  const aboutCardLycheAboutMenu = (
     <SultenCard
       image={dessert}
       imageAlt={'Chef'}
-      header={t(KEY.sulten_lyche_about_menu)} //hva er lyche
+      header={t(KEY.sulten_lyche_about_menu)}
       text={useTextItem(TextItem.sulten_lyche_about_menu_text)}
       imageAlignment="right"
       smallCard={true}
@@ -51,9 +45,9 @@ export function LycheAboutPage() {
     <>
       <SultenPage>
         <div className={styles.container}>
-          {aboutCard1}
-          {aboutCard2}
-          {aboutCard3}
+          {aboutCardWhatIsLyche}
+          {aboutCardLycheGoal}
+          {aboutCardLycheAboutMenu}
         </div>
       </SultenPage>
     </>
