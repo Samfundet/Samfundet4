5a26a63442c03162e5dd91680096d49bb07abd30
sindrelothe <sindrl@online.no>
2023-09-07 21:54:33 +0200

    reintroduce necessary import

--- backend/samfundet/models/general.py
+++ backend/samfundet/models/general.py
@@ -22,7 +22,7 @@ from root.utils import permissions
 from .utils.fields import LowerCaseField
 
 if TYPE_CHECKING:
-    from typing import Any
+    from typing import Any, Optional
     from django.db.models import Model
 
 
