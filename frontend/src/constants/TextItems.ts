550e8a3b6550f4540ff497af5127e04b7d5a9fc8
Snorre Sæther <snosaet@hotmail.com>
2023-09-21 19:29:30 +0200

    tweeks textItem. fixes dynamic style issue v2

--- frontend/src/constants/TextItems.ts
+++ frontend/src/constants/TextItems.ts
@@ -14,7 +14,7 @@ export const TextItem = {
   sulten_about_us_text: 'sulten_about_us_text',
   sulten_contact_text: 'sulten_contact_text',
   sulten_about_page_text: 'sulten_about_page_text',
-  sulten_what_is_lyche_text: 'sulten_what_is_lyche',
+  sulten_what_is_lyche_text: 'sulten_what_is_lyche_text',
   sulten_lyche_goal_text: 'sulten_lyche_goal_text',
-  sulten_lyche_about_menu_text: 'sulten_lyche_about_menu',
+  sulten_lyche_about_menu_text: 'sulten_lyche_about_menu_text',
 } as const;
