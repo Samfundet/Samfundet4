3f146868447bf64c319b8bb5fa57cc7fe6b3d6a7
Snorre Sæther <snosaet@hotmail.com>
2023-10-10 20:51:42 +0200

    creates lyche contact page

--- frontend/src/Pages/LycheContactPage/LycheContactPage.tsx
+++ frontend/src/Pages/LycheContactPage/LycheContactPage.tsx
@@ -1,26 +1,37 @@
-import { KEY } from '~/i18n/constants';
-import { ROUTES } from '~/routes';
-import { SultenPage } from '~/Components/SultenPage';
-import styles from './LycheContactPage.module.scss';
-import { Image } from '~/Components';
-import { kitteh } from '~/assets';
 import { Icon } from '@iconify/react';
-
+import { Link } from '~/Components/Link/Link';
+import { SultenPage } from '~/Components/SultenPage';
+import { TextItem } from '~/constants/TextItems';
+import { useTextItem } from '~/hooks';
+import styles from './LycheContactPage.module.scss';
 export function LycheContactPage() {
   const contactCard = (
     <div className={styles.contactContainer}>
-      <p className={styles.contactAddress}> HALLO !!!!</p>
-      <p className={styles.contactEmail}> HVOR ER JEG !!!!</p>
-      <div className={styles.contactSocials}>
-        <Icon icon="ph:instagram-logo-light" className={styles.icon} />
+      <div className={styles.textContainer}>
+        <p className={styles.lycheContactText}>{useTextItem(TextItem.sulten_contact_page_text)}</p>
+        <p className={styles.contactAddress}>booking-lyche@uka.no</p>
+        <p className={styles.contactAddress}>Elgesetergate 1</p>
+        <div className={styles.lycheSocialsContainer}>
+          <Link target="external" url="https://www.facebook.com/samfundet.lyche">
+            <Icon className={styles.icon} color="white" icon="bi:facebook" width={40} />
+          </Link>
+          <Link target="external" url="https://www.instagram.com/lychekjokkenogbar/">
+            <Icon className={styles.icon} color="white" icon="bi:instagram" width={40} />
+          </Link>
+          <Link target="external" url="https://maps.app.goo.gl/z98jeRNVqQUPRWsv9">
+            <Icon className={styles.icon} color="white" icon="carbon:map" width={40} />
+          </Link>
+        </div>
+      </div>
+
+      <div className={styles.lycheMapContainer}>
         <iframe
-          className={styles.lycheMap}
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d446.
-          272564853638!2d10.395138412303172!3d63.42227609031181!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.
-          1!3m3!1m2!1s0x466d31915ab2a009%3A0xd6ae74039e2b8871!2sLyche%20Kj%C3%B8kken%20%26%20Bar
-          !5e0!3m2!1sno!2sno!4v1695923302609!5m2!1sno!2sno"
-          // width="600"
-          // height="450"
+          className={styles.lycheStreet}
+          // eslint-disable-next-line max-len
+          src="https://www.google.com/maps/embed?pb=!4v1696369072803!6m8!1m7!1s2XEhTJwM_No8aTTrqI72RQ!2m2!1d63
+          .42220330892942!2d10.3953201100997!3f352.98823313957973!4f2.236618333891684!5f0.7820865974627469"
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
     </div>
@@ -28,12 +39,7 @@ export function LycheContactPage() {
   return (
     <>
       <SultenPage>
-        <div className={styles.outerContainer}>
-          <div className={styles.innerContainer}>
-            {/* <Image src={kitteh}></Image> */}
-            {contactCard}
-          </div>
-        </div>
+        <div className={styles.container}>{contactCard}</div>
       </SultenPage>
     </>
   );
