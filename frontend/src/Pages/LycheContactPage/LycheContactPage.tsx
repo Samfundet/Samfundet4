0521188161082c105c1e2a4c7e73baeb3e9c53fe
Snorre Sæther <snosaet@hotmail.com>
2023-10-19 19:28:11 +0200

    uses external routes and code was formated

--- frontend/src/Pages/LycheContactPage/LycheContactPage.tsx
+++ frontend/src/Pages/LycheContactPage/LycheContactPage.tsx
@@ -3,7 +3,9 @@ import { Link } from '~/Components/Link/Link';
 import { SultenPage } from '~/Components/SultenPage';
 import { TextItem } from '~/constants/TextItems';
 import { useTextItem } from '~/hooks';
+import { ROUTES } from '~/routes';
 import styles from './LycheContactPage.module.scss';
+
 export function LycheContactPage() {
   const contactCard = (
     <div className={styles.contactContainer}>
@@ -12,24 +14,21 @@ export function LycheContactPage() {
         <p className={styles.contactAddress}>booking-lyche@uka.no</p>
         <p className={styles.contactAddress}>Elgesetergate 1</p>
         <div className={styles.lycheSocialsContainer}>
-          <Link target="external" url="https://www.facebook.com/samfundet.lyche">
+          <Link target="external" url={ROUTES.other.lyche_facebook}>
             <Icon className={styles.icon} color="white" icon="bi:facebook" width={40} />
           </Link>
-          <Link target="external" url="https://www.instagram.com/lychekjokkenogbar/">
+          <Link target="external" url={ROUTES.other.sulten_instagram}>
             <Icon className={styles.icon} color="white" icon="bi:instagram" width={40} />
           </Link>
-          <Link target="external" url="https://maps.app.goo.gl/z98jeRNVqQUPRWsv9">
+          <Link target="external" url={ROUTES.other.maps_lyche}>
             <Icon className={styles.icon} color="white" icon="carbon:map" width={40} />
           </Link>
         </div>
       </div>
-
       <div className={styles.lycheMapContainer}>
         <iframe
           className={styles.lycheStreet}
-          // eslint-disable-next-line max-len
-          src="https://www.google.com/maps/embed?pb=!4v1696369072803!6m8!1m7!1s2XEhTJwM_No8aTTrqI72RQ!2m2!1d63
-          .42220330892942!2d10.3953201100997!3f352.98823313957973!4f2.236618333891684!5f0.7820865974627469"
+          src={ROUTES.other.streetview_lyche}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
