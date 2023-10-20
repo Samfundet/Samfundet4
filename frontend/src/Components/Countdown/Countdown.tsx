10259323c8a1373b565a934e3a2d6df61fae11fe
Mathias Aas <mathias.aas123@gmail.com>
2023-10-20 22:51:52 +0200

    move countdown function to utility

--- frontend/src/Components/Countdown/Countdown.tsx
+++ frontend/src/Components/Countdown/Countdown.tsx
@@ -1,40 +1,14 @@
 import { useEffect, useState } from 'react';
 
 import classNames from 'classnames';
+import { SECOND_MILLIS } from '~/constants';
 import { Children } from '~/types';
 import styles from './Countdown.module.scss';
+import { calculateTimeLeft, hasReachedTargetDate } from './utils';
 
 type CountdownProps = {
   targetDate: Date;
   children?: Children;
-};
-
-const SECOND_MILLIS = 1000;
-const MINUTE_MILLIS = 60 * SECOND_MILLIS;
-const HOUR_MILLIS = 60 * MINUTE_MILLIS;
-const DAY_MILLIS = 24 * HOUR_MILLIS;
-
-/**
- * Calculates the time left until targetDate.
- */
-const calculateTimeLeft = (targetDate: Date): [string, boolean] => {
-  const now = new Date();
-  const difference = targetDate.getTime() - now.getTime();
-
-  if (difference > 0) {
-    const days = Math.floor(difference / DAY_MILLIS);
-    const hours = Math.floor((difference % DAY_MILLIS) / HOUR_MILLIS);
-    const minutes = Math.floor((difference % HOUR_MILLIS) / MINUTE_MILLIS);
-    const seconds = Math.floor((difference % MINUTE_MILLIS) / SECOND_MILLIS);
-
-    if (days > 0) return [`${days}d ${hours}h`, false];
-    if (difference < 60 * SECOND_MILLIS) return [`${seconds}s`, false];
-    if (difference < 10 * MINUTE_MILLIS) return [`${minutes}m ${seconds}s`, false];
-    if (difference < 60 * MINUTE_MILLIS) return [`${minutes}m`, false];
-    return [`${hours}h ${minutes}m`, false];
-  }
-
-  return ['0m 0s', true];
 };
 
 export function Countdown({ targetDate, children }: CountdownProps) {
@@ -43,9 +17,9 @@ export function Countdown({ targetDate, children }: CountdownProps) {
 
   useEffect(() => {
     const updateTimer = setInterval(() => {
-      const [newTimeLeft, newIsTimeOut] = calculateTimeLeft(targetDate);
+      const newTimeLeft = calculateTimeLeft(targetDate);
       setTimeLeft(newTimeLeft);
-      if (newIsTimeOut) setIsTimeOut(true);
+      if (hasReachedTargetDate(targetDate)) setIsTimeOut(true);
     }, SECOND_MILLIS);
 
     return () => clearInterval(updateTimer);
