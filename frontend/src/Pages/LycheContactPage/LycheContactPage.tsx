77d65fb6be116952558206f9f1e80dce131f7cf5
Snorre Sæther <snosaet@hotmail.com>
2023-10-24 20:39:30 +0200

    removes outer container

--- frontend/src/Pages/LycheContactPage/LycheContactPage.tsx
+++ frontend/src/Pages/LycheContactPage/LycheContactPage.tsx
@@ -7,38 +7,35 @@ import { ROUTES } from '~/routes';
 import styles from './LycheContactPage.module.scss';
 
 export function LycheContactPage() {
-  const contactCard = (
-    <div className={styles.contactContainer}>
-      <div className={styles.textContainer}>
-        <p className={styles.lycheContactText}>{useTextItem(TextItem.sulten_contact_page_text)}</p>
-        <p className={styles.contactAddress}>booking-lyche@uka.no</p>
-        <p className={styles.contactAddress}>Elgesetergate 1</p>
-        <div className={styles.lycheSocialsContainer}>
-          <Link target="external" url={ROUTES.other.lyche_facebook}>
-            <Icon className={styles.icon} color="white" icon="bi:facebook" width={40} />
-          </Link>
-          <Link target="external" url={ROUTES.other.sulten_instagram}>
-            <Icon className={styles.icon} color="white" icon="bi:instagram" width={40} />
-          </Link>
-          <Link target="external" url={ROUTES.other.maps_lyche}>
-            <Icon className={styles.icon} color="white" icon="carbon:map" width={40} />
-          </Link>
-        </div>
-      </div>
-      <div className={styles.lycheMapContainer}>
-        <iframe
-          className={styles.lycheStreet}
-          src={ROUTES.other.streetview_lyche}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
-    </div>
-  );
   return (
     <>
       <SultenPage>
-        <div className={styles.container}>{contactCard}</div>
+        <div className={styles.contactContainer}>
+          <div className={styles.textContainer}>
+            <p className={styles.lycheContactText}>{useTextItem(TextItem.sulten_contact_page_text)}</p>
+            <p className={styles.contactAddress}>booking-lyche@uka.no</p>
+            <p className={styles.contactAddress}>Elgesetergate 1</p>
+            <div className={styles.lycheSocialsContainer}>
+              <Link target="external" url={ROUTES.other.lyche_facebook}>
+                <Icon className={styles.icon} color="white" icon="bi:facebook" width={40} />
+              </Link>
+              <Link target="external" url={ROUTES.other.sulten_instagram}>
+                <Icon className={styles.icon} color="white" icon="bi:instagram" width={40} />
+              </Link>
+              <Link target="external" url={ROUTES.other.maps_lyche}>
+                <Icon className={styles.icon} color="white" icon="carbon:map" width={40} />
+              </Link>
+            </div>
+          </div>
+          <div className={styles.lycheMapContainer}>
+            <iframe
+              className={styles.lycheStreet}
+              src={ROUTES.other.streetview_lyche}
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+        </div>
       </SultenPage>
     </>
   );
