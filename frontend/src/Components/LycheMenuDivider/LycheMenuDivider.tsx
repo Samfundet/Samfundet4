d3d9bbd7ef7e79aefa695331637902b2772e917c
Marion <Marionly@stud.ntnu.no>
2023-10-26 19:01:52 +0200

    requested changes

--- frontend/src/Components/LycheMenuDivider/LycheMenuDivider.tsx
+++ frontend/src/Components/LycheMenuDivider/LycheMenuDivider.tsx
@@ -1,14 +1,14 @@
 import styles from './LycheMenuDivider.module.scss';
 
-interface MenuCategory {
+type LycheMenuDividerProps = {
   title: string;
-}
+};
 
-export function LycheMenuDivider({ title }: MenuCategory) {
+export function LycheMenuDivider({ title }: LycheMenuDividerProps) {
   return (
     <div className={styles.lyche_menu_divider}>
       <div className={styles.lyche_menu_divider_left}></div>
-      <div className={styles.lyche_menu_divider_title}>{title}</div>
+      <div className={styles.lyche_menu_divider_title}> {title}</div>
       <div className={styles.lyche_menu_divider_right}></div>
     </div>
   );
