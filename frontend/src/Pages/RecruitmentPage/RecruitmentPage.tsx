53b2d084e0537cc6a767f7d510974f26cb311a04
Marion <Marionly@stud.ntnu.no>
2023-11-09 22:08:48 +0100

    show text when there is no active recruitment

--- frontend/src/Pages/RecruitmentPage/RecruitmentPage.tsx
+++ frontend/src/Pages/RecruitmentPage/RecruitmentPage.tsx
@@ -33,6 +33,38 @@ export function RecruitmentPage() {
           <GangTypeContainer gangTypes={gangTypes} recruitmentPositions={recruitmentPositions} />
         )}
       </div>
+      <div className={styles.heileshiten}>
+        <div>
+          <h1 className={styles.header}>Det er for tiden ingen opptak på Samfundet</h1>
+        </div>
+
+        <div className={styles.info}>
+          <p>
+            <br />
+            Vi har opptak på starten av hvert semester og ønsker at du søker til oss som frivillig!
+            <br />
+            Studentersamfundet i Trondhjem er Norges største studentersamfund og vi har et tilbud andre byer bare kan
+            drømme om.
+            <br />
+            Nesten uansett hvilken studiebakgrunn eller interesser du har, så finnes det en frivillig gjeng som søker
+            nettopp deg!
+            <br />
+            <br />
+            Over 1000 studenter bidrar allerede frivillig! Du kan være med å jobbe med blant annet lyd, lys, teater,
+            snekring, IT, artistbooking, korsang, musikk og mye annet. Du kan lære mye av å jobbe på Samfundet, og du
+            blir garantert kjent med mange andre studenter.
+            <br />
+            For mer informasjon om samfundets gjenger{' '}
+            <strong>
+              <a className={styles.link} href="https://samfundet.no/kontaktinfo">
+                Klikk her.
+              </a>
+            </strong>
+            <br />
+            Hvis du allerede har søkt kan du logge inn som søker for å prioritere og følge med på dine søknader.
+          </p>
+        </div>
+      </div>
     </Page>
   );
 }
