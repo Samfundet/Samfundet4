f026fd449e539218979b6d5a1267a7895a13c5e9
magsyg <magsyg@github.com>
2023-12-17 20:08:01 +0100

    add migrations

--- backend/root/custom_classes/admin_classes.py
+++ backend/root/custom_classes/admin_classes.py
@@ -45,7 +45,8 @@ def get_obj_link(obj: Any) -> str | None:
     """
     if obj:
         href = get_admin_url(obj=obj)
-        return mark_safe(f'<a href="{href}">{obj}</a>')  # nosec mark_safe
+
+        return mark_safe(f'<a href="{href}">{obj}</a>')  # nosec: B105
     return None
 
 
