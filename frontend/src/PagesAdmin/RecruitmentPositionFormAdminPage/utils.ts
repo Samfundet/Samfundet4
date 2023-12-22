7ea810760fccfa25fef867057f5e143aa802aba8
Mathias Aas <mathias.aas123@gmail.com>
2023-12-22 15:13:08 +0100

    use id instead of user values

--- frontend/src/PagesAdmin/RecruitmentPositionFormAdminPage/utils.ts
+++ frontend/src/PagesAdmin/RecruitmentPositionFormAdminPage/utils.ts
@@ -1,9 +1,9 @@
 import { DropDownOption } from '~/Components/Dropdown/Dropdown';
 import { UserDto } from '~/dto';
 
-export function mapUserToMultiselectOption(user: UserDto): DropDownOption<UserDto> {
+export function mapUserToMultiselectOption(user: UserDto): DropDownOption<Partial<UserDto>> {
   return {
     label: `${user?.username} ${user?.first_name} ${user?.last_name}`,
-    value: user,
+    value: { id: user?.id },
   };
 }
