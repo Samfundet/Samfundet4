1a4118b044e6230a3b6dd59a0ba10307d1af5c3a
magsyg <magsyg@github.com>
2023-12-26 13:43:38 +0100

    formating fixes

--- frontend/src/PagesAdmin/SultenMenuAdminPage/SultenMenuAdminPage.tsx
+++ frontend/src/PagesAdmin/SultenMenuAdminPage/SultenMenuAdminPage.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { Button } from '~/Components';
 import { CrudButtons } from '~/Components/CrudButtons/CrudButtons';
@@ -14,8 +13,6 @@ import { AdminPageLayout } from '../AdminPageLayout/AdminPageLayout';
 import styles from './SultenMenuAdminPage.module.scss';
 
 export function SultenMenuAdminPage() {
-  const navigate = useNavigate();
-
   const [menuItems, setMenuItems] = useState<MenuItemDto[]>([]);
   const [menus, setMenus] = useState<MenuDto[]>([]);
   const [showSpinner, setShowSpinner] = useState<boolean>(true);
