ea61925ad2ecbecbfceb4386893992fc1f3c4211
magsyg <magsyg@github.com>
2024-01-06 21:55:41 +0100

    yapf sucks

--- backend/samfundet/models/tests/test_recruitment.py
+++ backend/samfundet/models/tests/test_recruitment.py
@@ -13,8 +13,6 @@ datetime_fields_expecting_error = [
     'reprioritization_deadline_for_groups',
 ]
 
-# flake8-in-file-ignores: noqa: E124
-
 
 @pytest.fixture
 def fixture_org():
@@ -58,31 +56,30 @@ class TestRecruitmentClean:
         future_more = timezone.now() + timezone.timedelta(days=FUTURE_DAYS + 2)
         with pytest.raises(ValidationError) as error:
             _create_recruitment_with_dt(overrides={'visible_from': future_more})
-        assert 'Shown application deadline should be after visible' in dict(error.value)['actual_application_deadline']
-        assert 'Visible from should be before shown application deadline' in dict(error.value)['visible_from']
+        e = dict(error.value)
+        assert 'Shown application deadline should be after visible' in e['actual_application_deadline']
+        assert 'Visible from should be before shown application deadline' in e['visible_from']
 
     def test_application_deadline_before_reprioritization_deadline(self, fixture_org):
         future_more = timezone.now() + timezone.timedelta(days=FUTURE_DAYS + 2)
         with pytest.raises(ValidationError) as error:
             _create_recruitment_with_dt(overrides={'actual_application_deadline': future_more})
-
-        assert 'Actual application deadline should be before reprioritization deadline for applicants' in dict(error.value)['actual_application_deadline']
-        assert 'Reprioritization application deadline should be after actual deadline for applicants' in dict(error.value
-                                                                                                             )['reprioritization_deadline_for_applicant']
+        e = dict(error.value)
+        assert 'Actual application deadline should be before reprioritization deadline for applicants' in e['actual_application_deadline']
+        assert 'Reprioritization application deadline should be after actual deadline for applicants' in e['reprioritization_deadline_for_applicant']
 
     def test_reprioritization_deadline_for_applicant_before_reprioritization_deadline_for_groups(self, fixture_org):
         future_more = timezone.now() + timezone.timedelta(days=FUTURE_DAYS + 2)
         with pytest.raises(ValidationError) as error:
             _create_recruitment_with_dt(overrides={'reprioritization_deadline_for_applicant': future_more})
-        assert 'Reprioritization deadline for groups should be after reprioritization deadline for groups' in dict(error.value
-                                                                                                                  )['reprioritization_deadline_for_groups']
-        assert 'Reprioritization deadline for applicants should be before reprioritization deadline for groups' in dict(
-            error.value
-        )['reprioritization_deadline_for_applicant']
+        e = dict(error.value)
+        assert 'Reprioritization deadline for groups should be after reprioritization deadline for groups' in e['reprioritization_deadline_for_groups']
+        assert 'Reprioritization deadline for applicants should be before reprioritization deadline for groups' in e['reprioritization_deadline_for_applicant']
 
     def test_actual_deadline_before_shown_deadline(self, fixture_org):
         future_more = timezone.now() + timezone.timedelta(days=FUTURE_DAYS + 2)
         with pytest.raises(ValidationError) as error:
             _create_recruitment_with_dt(overrides={'shown_application_deadline': future_more})
-        assert 'Actual application deadline should be after the shown application deadline' in dict(error.value)['actual_application_deadline']
-        assert 'Shown application deadline should be before the actual application deadline' in dict(error.value)['shown_application_deadline']
+        e = dict(error.value)
+        assert 'Actual application deadline should be after the shown application deadline' in e['actual_application_deadline']
+        assert 'Shown application deadline should be before the actual application deadline' in e['shown_application_deadline']
