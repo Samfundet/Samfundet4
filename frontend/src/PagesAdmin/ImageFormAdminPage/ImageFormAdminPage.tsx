407cd758089310b618b694f5c0448462a234f9d2
Marion <Marionly@stud.ntnu.no>
2024-01-11 22:02:51 +0100

    Add padding to admin forms & fix translation for closed period form

--- frontend/src/PagesAdmin/ImageFormAdminPage/ImageFormAdminPage.tsx
+++ frontend/src/PagesAdmin/ImageFormAdminPage/ImageFormAdminPage.tsx
@@ -11,6 +11,7 @@ import { STATUS } from '~/http_status_codes';
 import { KEY } from '~/i18n/constants';
 import { ROUTES } from '~/routes';
 import { AdminPageLayout } from '../AdminPageLayout/AdminPageLayout';
+import styles from './ImageFormAdminPage.module.scss';
 
 export function ImageFormAdminPage() {
   const navigate = useCustomNavigate();
@@ -70,10 +71,12 @@ export function ImageFormAdminPage() {
   return (
     <AdminPageLayout title={title} loading={showSpinner}>
       <SamfForm onSubmit={handleOnSubmit} onChange={setImage} submitText={submitText} validateOn="submit">
-        <SamfFormField field="title" type="text" label={`${t(KEY.common_name)}`} />
-        {/* TODO helpText "Merkelapper må være separert med ', ', f.ex 'lapp1, lapp2, lapp3'" */}
-        <SamfFormField field="tag_string" type="text" label={`${t(KEY.common_tags)}`} required={false} />
-        {/* TODO create file picker input type */}
+        <div className={styles.input_row}>
+          <SamfFormField field="title" type="text" label={`${t(KEY.common_name)}`} />
+          {/* TODO helpText "Merkelapper må være separert med ', ', f.ex 'lapp1, lapp2, lapp3'" */}
+          <SamfFormField field="tag_string" type="text" label={`${t(KEY.common_tags)}`} required={false} />
+          {/* TODO create file picker input type */}
+        </div>
         <SamfFormField field="file" type="upload-image" label={`${t(KEY.common_choose)} ${t(KEY.common_image)}`} />
         <p>
           {JSON.stringify(image.file)}
