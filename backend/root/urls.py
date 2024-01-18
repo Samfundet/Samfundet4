2bd8c9e8d4795af5a7df6ea3e12a7fe1b0c6faa5
Tinius Presterud <github@tinius.dev>
2024-01-18 21:09:11 +0100

    Fixed possible sulten url conflict

--- backend/root/urls.py
+++ backend/root/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest_framework/', include('rest_framework.urls')),
     path('notifications/', include(notifications.urls, namespace='notifications')),
-    path('', include('sulten.urls')),
+    path('sulten/', include('sulten.urls')),
     path('', include('samfundet.urls')),  # Put last.
 ]
 
