eb3d83a34bdb30b8e4d937a21ef481910d6cfbb9
Emil Telstad <22004178+emilte@users.noreply.github.com>
2024-02-05 02:19:17 +0100

    Fix B007.

--- backend/root/management/commands/seed_scripts/recruitment_admissions.py
+++ backend/root/management/commands/seed_scripts/recruitment_admissions.py
@@ -21,7 +21,7 @@ def seed():
     created_count = 0
 
     for position_index, position in enumerate(positions):
-        for i in range(randint(0, 5)):  # Create between 0 and 5 instances for each position
+        for _ in range(randint(0, 5)):  # Create between 0 and 5 instances for each position
             admission_data = ADMISSION_DATA.copy()
             admission_data.update(
                 {
