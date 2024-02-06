544202d5cad9f3421ada35af8d2faadb401e7086
Snorre Sæther <snosaet@hotmail.com>
2024-02-06 21:58:51 +0100

    implements datatime in file path generation

--- backend/samfundet/models/utils/filename.py
+++ backend/samfundet/models/utils/filename.py
@@ -5,6 +5,12 @@ from typing import Any
 def upload_file_recruitment_path(instance: Any, filename: str) -> str:
     username = instance.user.username
     recruitment_position_id = instance.recruitment_position.id
-    unique_filename = f"{uuid.uuid4()}-{filename}"
+
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")  # Formats the current time as YYYYMMDDHHMMSS
+    unique_filename = f"{uuid.uuid4()}-{timestamp}-{filename}"
+
+    # Define the folder name using the username and recruitment_position_id
     folder_name = f"uploads/recruitment_admission_files/{username}_files/admission_id_{recruitment_position_id}"
+
+    # Return the full path to the file
     return f"{folder_name}/{unique_filename}"
