e3b710694b3058a5ac870da0e716d6a20c6e98d9
marionlys <144371589+marionlys@users.noreply.github.com>
2024-02-08 21:17:25 +0100

    519 Translate Aboutpage (#941)
    * Translate Aboutpage
    
    * Update backend/root/management/commands/seed_scripts/textitems.py
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>
    
    * Update backend/root/management/commands/seed_scripts/textitems.py
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>
    
    * Update backend/root/management/commands/seed_scripts/textitems.py
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>
    
    * Update backend/root/management/commands/seed_scripts/textitems.py
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>
    
    * Update backend/root/management/commands/seed_scripts/textitems.py
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>
    
    * Update backend/root/management/commands/seed_scripts/textitems.py
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>
    
    * fix typo
    
    * ruff
    
    * prettier :(
    
    ---------
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>

--- backend/root/management/commands/seed_scripts/textitems.py
+++ backend/root/management/commands/seed_scripts/textitems.py
@@ -38,6 +38,26 @@ def seed():
             'text_en': 'Do you have any questions or want to get in touch with us? Don"t hesitate to contact us!',
         },
         {
+            'key': 'festivals',
+            'text_nb': 'Annenhvert år arrangeres Norges største kulturfestival UKA og verdens største internasjonale tematiske studentfestival under Studentersamfundets paraply.',
+            'text_en': "Every other year, Norway's largest cultural festival UKA and the world's largest international thematic student festival are organized under Studentersamfundet's umbrella.",
+        },
+        {
+            'key': 'volunteering',
+            'text_nb': 'Det meste av arbeid på Studentersamfundet i Trondhjem gjøres gjennom dugnad av studenter. Arbeidet er organisert i enheter som kalles gjenger. Potensielle nye medlemmer må søke den aktuelle gjengen om opptak, og gjengen vurderer hver enkelt søker. Med sine omtrent 1700 frivillige utgjør det indre miljøet i Studentersamfundet en betydelig del av det organiserte fritidstilbudet til studenter i Trondheim.',
+            'text_en': 'Most of the work at Samfundet is done on a voluntary basis by students. The work is organized into units called groups. Prospective new members must apply to the appropriate group for admission and the group reviews each applicant. With around 1700 volunteers, the internal environment of Samfundet constitutes a significant part of organized leisure activities for students in Trondheim.',
+        },
+        {
+            'key': 'the_society_meeting',
+            'text_nb': 'Samfundsmøtet er Studentersamfundets høyeste organ. Her velges leder samt medlemmer til Finansstyret og Rådet. Lederen velger ut sitt eget styre, som utformer den politiske profilen og representer medlemmene og foreningen Samfundet. Finansstyret administrerer forretningsdriften. Rådet kontrollerer at all virksomhet i Samfundet foregår i henhold til norske og interne lover. Samfundet har en daglig leder, økonomiansvarlig, husøkonom, vaktmester og renholdspersonell som er ansatte.',
+            'text_en': "The Society Meeting is our highest organ. We elect the leader here, as well as members of the Finance Board and the Council. The leader, elected politically, chooses his or her own Board, which forms the House's political profile and represents all of Samfundet's members. The Finance Board manages the business. The Council ensures that all activities at Samfundet take place according to Norwegian and internal laws. Samfundet has a general manager, financial manager, house-economist, caretaker and cleaning staff who are employees.",
+        },
+        {
+            'key': 'about_samfundet',
+            'text_nb': 'Studentersamfundet i Trondhjem er en organisasjon for studenter i Trondheim som eies og drives av sine rundt 16 100 medlemmer. Formålsparagrafen vår sier at ”Studentersamfundet skal være det naturlige samlingsstedet for studenter i Trondhjem”. Vårt røde runde huser konserter, ulike kulturarrangementer, utallige barer, en kafé og en restaurant. Mest sagnomsust er Samfundsmøtene, viet til debatt om politikk og aktuelle spørsmål, eller til underholdning og moro. Samfundet har også tre av Trondheims beste konsertscener.',
+            'text_en': "Samfundet is an organization for students in Trondheim that is owned and run by its approximately 16100 members. Our mission statement is Samfundet will be the natural meeting place for students in Trondheim. In our red, round building we regularly host concerts and various cultural events, and have countless bars, a café and a restaurant. The Society Meetings are perhaps our most famous events. These meetings serve as a place for debating politics and current events, and but also for entertainment and fun. Samfundet also has three of Trondheim's best music venues.",
+        },
+        {
             'key': 'no_recruitment_text',
             'text_nb': 'Det er for tiden ingen opptak på Samfundet',
             'text_en': 'There are currently no recruitments at Samfundet',
--- frontend/src/constants/TextItems.ts
+++ frontend/src/constants/TextItems.ts
@@ -20,6 +20,12 @@ export const TextItem = {
   sulten_lyche_goal_text: 'sulten_lyche_goal_text',
   sulten_lyche_about_menu_text: 'sulten_lyche_about_menu_text',
   sulten_contact_page_text: 'sulten_contact_page_text',
+
+  //About page
+  festivals: 'festivals',
+  volunteering: 'volunteering',
+  the_society_meeting: 'the_society_meeting',
+  about_samfundet: 'about_samfundet',
   sulten_contact_page_title: 'sulten_contact_page_title',
 
   //Recruitment
