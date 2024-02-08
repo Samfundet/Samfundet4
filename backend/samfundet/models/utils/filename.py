61af70eecda3785e5d6b0cb845b54739c98f48d4
Snorre Sæther <snosaet@hotmail.com>
2024-02-08 17:43:01 +0100

    #688 formated datetime for upload path

--- backend/samfundet/models/utils/filename.py
+++ backend/samfundet/models/utils/filename.py
@@ -1,3 +1,4 @@
+import datetime
 import uuid
 from typing import Any
 
@@ -6,11 +7,13 @@ def upload_file_recruitment_path(instance: Any, filename: str) -> str:
     username = instance.user.username
     recruitment_position_id = instance.recruitment_position.id
 
-    timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")  # Formats the current time as YYYYMMDDHHMMSS
+    timestamp = datetime.datetime.now().strftime(
+        "%Y-%m-%d-%H-%M-%S"
+    )  # Formats the current time, example: 2024-02-08-17-37-59
     unique_filename = f"{uuid.uuid4()}-{timestamp}-{filename}"
 
     # Define the folder name using the username and recruitment_position_id
-    folder_name = f"uploads/recruitment_admission_files/{username}_files/admission_id_{recruitment_position_id}"
+    folder_name = f"uploads/recruitment_admission_files/{username}_files/admission_ID_{recruitment_position_id}"
 
     # Return the full path to the file
     return f"{folder_name}/{unique_filename}"
