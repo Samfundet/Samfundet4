8def7e2b3bca359b7d221cc39c9eefd62cfbfbdd
hei98 <heidi-hei@live.no>
2024-02-09 12:50:18 +0100

    Add putRecruitmentAdmissionInterview function

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -11,6 +11,7 @@ import {
   ImageDto,
   ImagePostDto,
   InformationPageDto,
+  InterviewDto,
   KeyValueDto,
   MenuDto,
   MenuItemDto,
@@ -703,5 +704,19 @@ export async function putRecruitmentAdmission(admission: Partial<RecruitmentAdmi
   };
   const response = await axios.put(url, data, { withCredentials: true });
 
+  return response;
+}
+
+export async function putRecruitmentAdmissionInterview(
+  interviewId: string,
+  interview: Partial<InterviewDto>,
+): Promise<AxiosResponse> {
+  const url =
+    BACKEND_DOMAIN +
+    reverse({
+      pattern: ROUTES.backend.samfundet__interview_detail,
+      urlParams: { pk: interviewId },
+    });
+  const response = await axios.put<InterviewDto>(url, interview, { withCredentials: true });
   return response;
 }
