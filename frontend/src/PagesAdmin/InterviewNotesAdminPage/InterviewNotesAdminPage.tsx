608c3801192f10f5fa9496273642eb4424298160
hei98 <heidi-hei@live.no>
2024-02-12 20:12:46 +0100

    Edit interview notes

--- frontend/src/PagesAdmin/InterviewNotesAdminPage/InterviewNotesAdminPage.tsx
+++ frontend/src/PagesAdmin/InterviewNotesAdminPage/InterviewNotesAdminPage.tsx
@@ -1,35 +1,78 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { Button } from '~/Components';
 import { TextAreaField } from '~/Components/TextAreaField/TextAreaField';
+import { getRecruitmentAdmissionsForGang, putRecruitmentAdmissionInterview } from '~/api';
+import { InterviewDto, RecruitmentAdmissionDto } from '~/dto';
 import { KEY } from '~/i18n/constants';
 import { AdminPageLayout } from '../AdminPageLayout/AdminPageLayout';
 import styles from './InterviewNotesAdminPage.module.scss';
 
 export function InterviewNotesPage() {
-  //TODO: interview notes from backend
+  const recruitmentId = useParams().recruitmentId;
+  const gangId = useParams().gangId;
+  const positionId = useParams().positionId;
+  const interviewId = useParams().interviewId;
   const [editingMode, setEditingMode] = useState(false);
-  const [text, setText] = useState('Notater fra intervjuet her...'); //TODO: place the text from the backend here.
-  const posId = 1; //TODO: get the posId from the backend.
+  const [recruitmentAdmission, setRecruitmentAdmission] = useState<RecruitmentAdmissionDto[]>([]);
+  const [interview, setInterview] = useState<InterviewDto | null>(null);
+  const [disabled, setdisabled] = useState<boolean>(true);
+  const [nameUser, setNameUser] = useState<string>('');
   const { t } = useTranslation();
 
-  function handleEditSave() {
-    if (editingMode) {
-      //TODO: save the text in the textbox and send it to the backend
+  useEffect(() => {
+    if (positionId && recruitmentId && gangId && interviewId) {
+      getRecruitmentAdmissionsForGang(gangId, recruitmentId).then((response) => {
+        const recruitmentAdmissions = response.data;
+        const admission = recruitmentAdmissions.filter(
+          (admission) =>
+            admission.recruitment_position &&
+            admission.recruitment_position.toString() === positionId &&
+            admission.interview.id.toString() === interviewId &&
+            admission.interview.interview_time !== null,
+        );
+        if (admission.length !== 0) {
+          setdisabled(false);
+          setRecruitmentAdmission(admission);
+          setInterview(admission[0].interview);
+          setNameUser(
+            admission[0].user.first_name ? admission[0].user.first_name + ' ' + admission[0].user.last_name : '',
+          );
+        }
+      });
+    }
+  }, [recruitmentId, positionId, gangId, interviewId, t]);
+
+  async function handleEditSave() {
+    if (editingMode && interview) {
+      try {
+        await putRecruitmentAdmissionInterview(interview.id.toString(), interview);
+        toast.success(t(KEY.common_save_successful));
+      } catch (error) {
+        toast.error(t(KEY.common_something_went_wrong));
+      }
     }
     setEditingMode(!editingMode);
   }
-
-  //TODO: make handleSave function to save the text in the textbox and send it to the backend
 
   return (
     <AdminPageLayout title={t(KEY.recruitment_interview_notes)}>
       <div className={styles.container}>
         <label htmlFor="INotes">
-          {t(KEY.recruitment_applicant)} {posId}
+          {t(KEY.recruitment_applicant)}: {nameUser}
         </label>
-        <TextAreaField value={text} onChange={setText} disabled={!editingMode}></TextAreaField>
-        <Button theme="samf" rounded={true} className={styles.button} onClick={handleEditSave}>
+        <TextAreaField
+          value={interview ? interview.notes : ' '}
+          onChange={(value: string) => {
+            const updatedNotes = value;
+            const updatedInterview: InterviewDto = { ...recruitmentAdmission[0].interview, notes: updatedNotes };
+            setInterview(updatedInterview);
+          }}
+          disabled={!editingMode}
+        ></TextAreaField>
+        <Button theme="samf" rounded={true} className={styles.button} onClick={handleEditSave} disabled={disabled}>
           {editingMode ? t(KEY.common_save) : t(KEY.common_edit)}
         </Button>
       </div>
