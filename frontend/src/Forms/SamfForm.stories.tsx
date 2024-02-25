4012ba27a9922de56b3bbfdb6486de42659bb8a6
Erik <eahoff@gmail.com>
2024-02-25 16:00:28 +0100

    fix SamfForm stories

--- frontend/src/Forms/SamfForm.stories.tsx
+++ frontend/src/Forms/SamfForm.stories.tsx
@@ -20,7 +20,7 @@ const Template: ComponentStory<typeof SamfForm> = function (args) {
 };
 
 function validateShrimp(str: string) {
-  if (str.toLowerCase().includes('reke')) return true;
+  if (str && str.toLowerCase().includes('reke')) return true;
   return "Feltet må inneholde 'reke'";
 }
 
@@ -41,15 +41,27 @@ Basic.args = {
   children: (
     <>
       <div style={{ display: 'flex', gap: '.5em' }}>
-        <SamfFormField field="title_nb" type="text" label="Tekst som er nødvendig" />
-        <SamfFormField field="title_en" type="text" label="Tekst med avansert krav" validator={validateShrimp} />
+        <SamfFormField field="required_text" type="text" label="Tekst som er nødvendig" required={true} />
+        <SamfFormField
+          field="spesific_text"
+          type="text"
+          label="Tekst med avansert krav"
+          validator={validateShrimp}
+          required={false}
+        />
       </div>
       <div style={{ display: 'flex', gap: '.5em' }}>
-        <SamfFormField field="duration" type="number" label="Tall-input med krav" validator={validate69} />
-        <SamfFormField field="category" type="options" label="Dropdown input" options={options} />
+        <SamfFormField
+          field="spesific_number"
+          type="number"
+          label="Tall-input med krav"
+          validator={validate69}
+          required={false}
+        />
+        <SamfFormField field="category" type="options" label="Dropdown input" options={options} required={false} />
       </div>
-      <SamfFormField field="description_long_nb" type="text-long" label="Lang tekst input" props={{ rows: 5 }} />
-      <SamfFormField field="phonenumber" type="phonenumber" label="Phonenumber" />
+      <SamfFormField field="text_long" type="text-long" label="Lang tekst input" props={{ rows: 5 }} required={false} />
+      <SamfFormField field="phonenumber" type="phonenumber" label="Phonenumber" required={false} />
     </>
   ),
 };
