86abab1a9693bbe4c2cfc7613e710cfd5ee8d4ff
Erik <eahoff@gmail.com>
2024-03-13 11:46:23 +0100

    Add SamfForm utils with phonenumber and email validator

---
+++ frontend/src/Forms/util.ts
@@ -0,0 +1,24 @@
+import { SamfError } from './SamfForm';
+
+// ---------------------------------- //
+//             Validators             //
+// -----------------------------------//
+export const validPhonenumber = (value: string): SamfError => {
+  if (value.length === 0) {
+    return 'Please enter a phone number';
+  }
+  if (!/^\d+$/.test(value)) {
+    return 'Please enter a valid phone number';
+  }
+  return true;
+};
+
+export const validEmail = (value: string): SamfError => {
+  if (value.length === 0) {
+    return 'Please enter an email';
+  }
+  if (!/^.+@.+\..+$/.test(value)) {
+    return 'Please enter a valid email';
+  }
+  return true;
+};
