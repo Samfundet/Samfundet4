a3be517b045f1e217dd61376d83da00d824e6d4d
emsoraffa <emil.rafael.solberg@gmail.com>
2024-04-16 22:01:29 +0200

    fix: alternatives

--- frontend/src/constants/PurchaseFeedbackForm.ts
+++ frontend/src/constants/PurchaseFeedbackForm.ts
@@ -1,4 +1,4 @@
 export const TITLE = 'Hvor hørte du om dette arrangementet?';
 
-export const ALTERNATIVES = ['samf', 'nbb', 'gufsejuvet'];
+export const ALTERNATIVES = ['samfundet.no', 'venner', 'familie'];
 export const QUESTIONS = ['hvem?', 'hva?', 'hvor?'];
