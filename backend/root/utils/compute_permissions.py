22b5917652d1af9009ab0d0359c3de04ae30e49e
Erik <eahoff@gmail.com>
2024-04-19 12:02:47 +0200

    Add DFS to compute_permission utility

--- backend/root/utils/compute_permissions.py
+++ backend/root/utils/compute_permissions.py
@@ -1,5 +1,53 @@
-from collections import OrderedDict
-# TODO:use collections? improve performance?
+from typing import Generic, TypeVar, Dict, Set
+
+
+T = TypeVar('T')
+
+
+class Node(Generic[T]):
+    def __init__(self, data: T) -> None:
+        self.data: T = data
+        self.children: Set[Node[T]] = set()
+
+    def __str__(self) -> str:
+        return self.data
+
+
+def dfs(graf: Set[Node]) -> Dict[Node, Set[Node]]:
+    # Initialize
+    reachable_nodes = {node: set() for node in graf}
+    for node in graf:
+        reachable_nodes[node].add(node)
+    # Do search
+    for node in graf:
+        stack = [node]
+        while stack:
+            current_node = stack.pop()
+            for child in current_node.children:
+                if len(reachable_nodes[child]) > 1:
+                    # Avoid recomputing reachable nodes for child if it has already been computed
+                    for reachable_node in reachable_nodes[child]:
+                        reachable_nodes[node].add(reachable_node)
+                if child not in reachable_nodes[node]:
+                    reachable_nodes[node].add(child)
+                    stack.append(child)
+
+    return reachable_nodes
+
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+
+node1.children.add(node2)
+node2.children.add(node3)
+
+graf: set[Node[int]] = set()
+graf.add(node1)
+graf.add(node2)
+graf.add(node3)
+
+print(dfs(graf))
 
 
 class Permissions:
@@ -25,7 +73,7 @@ class Permissions:
 
     def clean(self):
         for key, value in self.dict.items():
-            self.dict[key] = "".join(sorted(set(value)))
+            self.dict[key] = ''.join(sorted(set(value)))
 
 
 class Graph:
@@ -33,13 +81,13 @@ class Graph:
         self.V = vertices
 
     def printAdjMatrix(self, reach):
-        print("Following matrix transitive closure of the given graph ")
+        print('Following matrix transitive closure of the given graph ')
         for i in range(self.V):
             for j in range(self.V):
                 if i == j:
-                    print("%7d\t" % (1), end=" ")
+                    print('%7d\t' % (1), end=' ')
                 else:
-                    print("%7d\t" % (reach[i][j]), end=" ")
+                    print('%7d\t' % (reach[i][j]), end=' ')
             print()
 
     def compute_permissions(self, graph, permissions):
@@ -57,7 +105,7 @@ class Graph:
                         )
 
         self.printAdjMatrix(reach)
-        print("")
+        print('')
 
         permissions.clean()
         print(permissions)
@@ -77,11 +125,11 @@ graph = [
 
 p = Permissions(
     {
-        "Ansatt": "B",
-        "Hest": "A",
-        "Mellomleder": "C",
-        "Sjef": "D",
-        "Baltazar": "E",
+        'Ansatt': 'B',
+        'Hest': 'A',
+        'Mellomleder': 'C',
+        'Sjef': 'D',
+        'Baltazar': 'E',
     }
 )
-g.compute_permissions(graph, p)
+# g.compute_permissions(graph, p)
