08624bcec5e9251c3591351be8edec1088ef88bc
robines <espi.robin@gmail.com>
2024-05-02 03:49:32 +0200

    Add (temporary) crumbs for recruitment pages

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -58,6 +58,7 @@ import { ROUTES } from './routes';
 import { App } from '~/App';
 import { t } from 'i18next';
 import { KEY } from '~/i18n/constants';
+import { reverse } from '~/named-urls';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
@@ -229,47 +230,117 @@ export const router = createBrowserRouter(
         />
         {/* Recruitment */}
         <Route
-          path={ROUTES.frontend.admin_recruitment}
-          element={<ProtectedRoute perms={[PERM.SAMFUNDET_VIEW_RECRUITMENT]} Page={RecruitmentAdminPage} />}
-        />
-        <Route
-          path={ROUTES.frontend.admin_recruitment_create}
-          element={<ProtectedRoute perms={[PERM.SAMFUNDET_ADD_RECRUITMENT]} Page={RecruitmentFormAdminPage} />}
-        />
-        <Route
-          path={ROUTES.frontend.admin_recruitment_edit}
-          element={<ProtectedRoute perms={[PERM.SAMFUNDET_CHANGE_RECRUITMENT]} Page={RecruitmentFormAdminPage} />}
-        />
-        <Route
-          path={ROUTES.frontend.admin_recruitment_users_without_interview}
-          element={<RecruitmentUsersWithoutInterview />}
-        />
-        <Route
-          path={ROUTES.frontend.admin_recruitment_gang_position_applicants_interview_notes}
-          element={<InterviewNotesPage />}
-        />
-        {/* TODO ADD PERMISSIONS */}
-        <Route
-          path={ROUTES.frontend.admin_recruitment_gang_overview}
-          element={<ProtectedRoute perms={[]} Page={RecruitmentGangOverviewPage} />}
-        />
-        <Route
-          path={ROUTES.frontend.admin_recruitment_gang_position_overview}
-          element={<ProtectedRoute perms={[]} Page={RecruitmentGangAdminPage} />}
-        />
+          element={<Outlet />}
+          handle={{ crumb: () => <Link url={ROUTES.frontend.admin_recruitment}>{t(KEY.common_recruitment)}</Link> }}
+        >
+          <Route
+            path={ROUTES.frontend.admin_recruitment}
+            element={<ProtectedRoute perms={[PERM.SAMFUNDET_VIEW_RECRUITMENT]} Page={RecruitmentAdminPage} />}
+          />
+          <Route
+            path={ROUTES.frontend.admin_recruitment_create}
+            element={<ProtectedRoute perms={[PERM.SAMFUNDET_ADD_RECRUITMENT]} Page={RecruitmentFormAdminPage} />}
+          />
+          <Route
+            path={ROUTES.frontend.admin_recruitment_edit}
+            element={<ProtectedRoute perms={[PERM.SAMFUNDET_CHANGE_RECRUITMENT]} Page={RecruitmentFormAdminPage} />}
+          />
+          <Route
+            path={ROUTES.frontend.admin_recruitment_users_without_interview}
+            element={<RecruitmentUsersWithoutInterview />}
+          />
+          <Route
+            path={ROUTES.frontend.admin_recruitment_gang_position_applicants_interview_notes}
+            element={<InterviewNotesPage />}
+          />
+          {/* TODO ADD PERMISSIONS */}
+          <Route
+            element={<Outlet />}
+            loader={({ params }) => {
+              // TODO: Fetch recruitment to get name, also pass it to Page (may need to use useRouteLoaderData hook?)
+              return { recruitmentId: params.recruitmentId };
+            }}
+            handle={{
+              crumb: ({ recruitmentId }: { recruitmentId: string }) => (
+                <Link
+                  url={reverse({
+                    pattern: ROUTES.frontend.admin_recruitment_gang_overview,
+                    urlParams: { recruitmentId },
+                  })}
+                >
+                  {recruitmentId}
+                </Link>
+              ),
+            }}
+          >
+            <Route
+              path={ROUTES.frontend.admin_recruitment_gang_overview}
+              element={<ProtectedRoute perms={[]} Page={RecruitmentGangOverviewPage} />}
+            />
 
-        <Route
-          path={ROUTES.frontend.admin_recruitment_gang_position_create}
-          element={<ProtectedRoute perms={[]} Page={RecruitmentPositionFormAdminPage} />}
-        />
-        <Route
-          path={ROUTES.frontend.admin_recruitment_gang_position_applicants_overview}
-          element={<ProtectedRoute perms={[]} Page={RecruitmentPositionOverviewPage} />}
-        />
-        <Route
-          path={ROUTES.frontend.admin_recruitment_gang_position_edit}
-          element={<ProtectedRoute perms={[]} Page={RecruitmentPositionFormAdminPage} />}
-        />
+            <Route
+              element={<Outlet />}
+              loader={({ params }) => {
+                // TODO: Fetch gang to get name, also pass it to Page (may need to use useRouteLoaderData hook?)
+                return { recruitmentId: params.recruitmentId, gangId: params.gangId };
+              }}
+              handle={{
+                crumb: ({ recruitmentId, gangId }: { recruitmentId: string, gangId: string }) => (
+                  <Link
+                    url={reverse({
+                      pattern: ROUTES.frontend.admin_recruitment_gang_position_overview,
+                      urlParams: { recruitmentId, gangId },
+                    })}
+                  >
+                    {gangId}
+                  </Link>
+                ),
+              }}
+            >
+              <Route
+                path={ROUTES.frontend.admin_recruitment_gang_position_overview}
+                element={<ProtectedRoute perms={[]} Page={RecruitmentGangAdminPage} />}
+              />
+              <Route
+                path={ROUTES.frontend.admin_recruitment_gang_position_create}
+                element={<ProtectedRoute perms={[]} Page={RecruitmentPositionFormAdminPage} />}
+              />
+              <Route
+                path={ROUTES.frontend.admin_recruitment_gang_position_edit}
+                element={<ProtectedRoute perms={[]} Page={RecruitmentPositionFormAdminPage} />}
+              />
+              <Route
+                path={ROUTES.frontend.admin_recruitment_gang_position_applicants_overview}
+                loader={({ params }) => {
+                  const { recruitmentId, gangId, positionId } = params;
+                  // TODO: Fetch position to get name, also pass it to Page (may need to use useRouteLoaderData hook?)
+                  return { recruitmentId, gangId, positionId };
+                }}
+                handle={{
+                  crumb: ({
+                    recruitmentId,
+                    gangId,
+                    positionId,
+                  }: {
+                    recruitmentId: string,
+                    gangId: string,
+                    positionId: string,
+                  }) => (
+                    <Link
+                      url={reverse({
+                        pattern: ROUTES.frontend.admin_recruitment_gang_position_applicants_overview,
+                        urlParams: { recruitmentId, gangId, positionId },
+                      })}
+                    >
+                      {positionId}
+                    </Link>
+                  ),
+                }}
+                element={<ProtectedRoute perms={[]} Page={RecruitmentPositionOverviewPage} />}
+              />
+            </Route>
+          </Route>
+        </Route>
         {/*
         Info pages
         Custom layout for edit/create
