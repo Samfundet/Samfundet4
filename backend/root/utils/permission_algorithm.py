1b012d96fb205af3463a24e9b2cdaf1b2eb3d641
emsoraffa <emil.rafael.solberg@gmail.com>
2024-05-14 20:28:24 +0200

    feat: added tests

--- backend/root/utils/permission_algorithm.py
+++ backend/root/utils/permission_algorithm.py
@@ -1,88 +1,52 @@
-from collections import OrderedDict
-# TODO:use collections? improve performance?
-
+from __future__ import annotations
 
 class Permissions:
-    # FIX: permissions added multipletimes.
-    def __init__(self, dictionary):
-        self.dict = dictionary
+    def __init__(self, dictionary: dict):
+        self.dict = {k: set(v) for k, v in dictionary.items()}
         self.index_array = list(dictionary.keys())
 
-    def __getitem__(self, key):
-        return self.dict[key]
+    def __getitem__(self, key: int) -> str:
+        return ''.join(sorted(self.dict[self.index_array[key]]))
 
-    def __str__(self):
-        return str(self.dict)
+    def __str__(self) -> str:
+        return str({k: ''.join(sorted(v)) for k, v in self.dict.items()})
 
-    def get(self, index):
+    def get(self, index: int) -> set:
         return self.dict[self.index_array[index]]
 
-    def set(self, key, value):
-        self.dict[key] = value
+    def set(self, key: str, value: str) -> None:
+        if key in self.dict:
+            self.dict[key].update(value)
+        else:
+            raise KeyError("Key not found in permissions dictionary")
 
-    def get_index_array(self):
+    def get_index_array(self) -> list:
         return self.index_array
-
-    def clean(self):
-        for key, value in self.dict.items():
-            self.dict[key] = ''.join(sorted(set(value)))
 
 
 class Graph:
-    # TODO: implement using set.
-    def __init__(self, vertices):
+    def __init__(self, vertices: int, graph: list):
         self.V = vertices
+        self.graph = graph
 
-    def printAdjMatrix(self, reach):
-        print('Following matrix transitive closure of the given graph ')
-        for i in range(self.V):
-            for j in range(self.V):
-                if i == j:
-                    print('%7d\t' % (1), end=' ')
-                else:
-                    print('%7d\t' % (reach[i][j]), end=' ')
-            print()
+    def __str__(self) -> str:
+        matrix_str = ''
+        for row in self.graph:
+            for value in row:
+                matrix_str += '%7d\t' % value
+            matrix_str += '\n'
+        return matrix_str
 
-    def compute_permissions(self, graph, permissions):
-        reach = [i[:] for i in graph]
+    def compute_permissions(self, permissions: Permissions) -> Permissions:
+        reach = [row[:] for row in self.graph]
         index_array = permissions.get_index_array()
 
         for k in range(self.V):
             for i in range(self.V):
                 for j in range(self.V):
-                    reach[i][j] = reach[i][j] or (reach[i][k] and reach[k][j])
-                    if reach[i][j] == 1:
-                        permissions.set(
-                            index_array[j],
-                            permissions.get(j) + permissions[index_array[i]],
-                        )
-
-        self.printAdjMatrix(reach)
-        print('')
-
-        permissions.clean()
-        print(permissions)
-
+                    if reach[i][j] or (reach[i][k] and reach[k][j]):
+                        reach[i][j] = 1
+                        permissions.set(index_array[j], permissions.get(i))
+        # Update the graph's own matrix to reflect the transitive closure
+        self.graph = reach
         return permissions
-
-
-g = Graph(5)
-
-graph = [
-    [1, 0, 1, 0, 0],
-    [0, 1, 1, 0, 1],
-    [0, 0, 1, 1, 0],
-    [0, 0, 0, 1, 0],
-    [0, 0, 0, 0, 1],
-]
-
-p = Permissions(
-    {
-        'Ansatt': 'B',
-        'Hest': 'A',
-        'Mellomleder': 'C',
-        'Sjef': 'D',
-        'Baltazar': 'E',
-    }
-)
-g.compute_permissions(graph, p)
