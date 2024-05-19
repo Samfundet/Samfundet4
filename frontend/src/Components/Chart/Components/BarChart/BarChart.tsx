ae874d30d1a4eac148cb00116ecdf772ebfbbed8
snorre sæther <snosaet@gmail.com>
2024-05-19 15:09:44 +0200

    makes barchart width relative to container width

--- frontend/src/Components/Chart/Components/BarChart/BarChart.tsx
+++ frontend/src/Components/Chart/Components/BarChart/BarChart.tsx
@@ -1,7 +1,7 @@
 import { ChartData } from '~/Components/Chart/Chart';
 import { Text } from '~/Components/Text/Text';
 import styles from './BarChart.module.scss';
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useEffect, useRef, useState } from 'react';
 import { HoverLabel } from '~/Components/Chart/Components/HoverLabel';
 
 type BarChartProps = {
@@ -9,6 +9,8 @@ type BarChartProps = {
   data: ChartData[];
   labelSpliceStart?: number;
   labelSpliceEnd?: number;
+  maxBarWidth?: number;
+  minBarWidth?: number;
   hAxisLabel?: string;
   vAxisLabel?: string;
 };
@@ -20,20 +22,41 @@ export function BarChart({
   labelSpliceEnd,
   hAxisLabel,
   vAxisLabel,
+  maxBarWidth = 80,
+  minBarWidth = 10,
 }: BarChartProps) {
-  const svgWidth = 1200;
-  const svgHeight = 800;
-  const vAxisSpace = 100;
-  const barSpacing = 10;
+  const [containerWidth, setContainerWidth] = useState<number | null>(null);
+
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const updateDivWidth = () => {
+      if (containerRef.current) {
+        setContainerWidth(containerRef.current.clientWidth);
+      }
+    };
+    updateDivWidth();
+    window.addEventListener('resize', updateDivWidth);
+    return () => {
+      window.removeEventListener('resize', updateDivWidth);
+    };
+  }, []);
+
+  const svgWidth = containerWidth ?? 1200;
+  const svgHeight = 500;
+  const vAxisSpace = 45;
+  const barSpacing = 5;
   const numberOfBars = data.length;
   const totalBarSpacing = (numberOfBars - 1) * barSpacing;
   const totalBarWidthAvailable = svgWidth - totalBarSpacing - vAxisSpace;
   const dynamicBarWidth = totalBarWidthAvailable / numberOfBars;
-  const maxBarWidth = 75;
-  const minBarWidth = 10;
   const barWidth = Math.max(minBarWidth, Math.min(maxBarWidth, dynamicBarWidth)); // Adjust bar width
   const maxValue = Math.max(...data.map((item) => item.value));
   const scale = (svgHeight - 150) / maxValue; // Adjust for axis label space
+  const totalBarWidth = numberOfBars * barWidth;
+  // const totalSpacingWidth = totalBarSpacing;
+  const totalBarsAndSpacingWidth = totalBarWidth + totalBarSpacing;
+  const xAxisLabelPosition = totalBarsAndSpacingWidth / 2 + vAxisSpace;
 
   const [hoverInfo, setHoverInfo] = useState({ value: '', x: 0, y: 0, visible: false });
 
@@ -64,9 +87,9 @@ export function BarChart({
   const numLabels = 5;
   const axisLabels = Array.from({ length: numLabels }, (_, i) => {
     const labelValue = Math.round((maxValue / (numLabels - 1)) * i); // Even distribution of label values
-    const yPosition = svgHeight - 100 - labelValue * scale; // Aligning the labels with bars
+    const yPosition = svgHeight - 50 - labelValue * scale; // Aligning the labels with bars
     return (
-      <text key={i} x="90" y={yPosition} fontSize="12px" fill="black" textAnchor="end">
+      <text key={i} x="40" y={yPosition} style={{ fontSize: '14px' }} fill="currentColor" textAnchor="end">
         {labelValue}
       </text>
     );
@@ -75,7 +98,7 @@ export function BarChart({
   const bars = data.map((item, index) => {
     const barHeight = item.value * scale;
     const xPosition = index * (barWidth + barSpacing) + vAxisSpace; // Adjust to start after axis labels
-    const yPosition = svgHeight - barHeight - 100; // Leave space for labels
+    const yPosition = svgHeight - barHeight - 50; // Leave space for labels
 
     return (
       <g key={index}>
@@ -84,12 +107,18 @@ export function BarChart({
           y={yPosition}
           width={barWidth}
           height={barHeight}
-          fill={'blue'}
+          fill={'#FA8072'}
           onMouseEnter={(event) => handleMouseEnter(event, item.label + ': ' + item.value.toString())}
           onMouseMove={handleMouseMove}
           onMouseLeave={handleMouseLeave}
         />
-        <text x={xPosition + barWidth / 2} y={svgHeight - 80} fill="black" fontSize="12px" textAnchor="middle">
+        <text
+          x={xPosition + barWidth / 2}
+          y={svgHeight - 30}
+          fill="currentColor"
+          style={{ fontSize: '14px' }}
+          textAnchor="middle"
+        >
           {item.label.slice(labelSpliceStart, labelSpliceEnd)}
         </text>
       </g>
@@ -97,7 +126,7 @@ export function BarChart({
   });
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={containerRef}>
       <Text as={'strong'} size={'l'}>
         {chartTitle}
       </Text>
@@ -105,23 +134,28 @@ export function BarChart({
         width={svgWidth}
         height={svgHeight}
         viewBox={`0 0 ${svgWidth} ${svgHeight}`}
-        className={styles.chart}
         xmlns="http://www.w3.org/2000/svg"
       >
         {axisLabels}
         {bars}
         {/* Horizontal Axis Label */}
-        <text x={svgWidth / 2} y={svgHeight - 20} fontSize="16px" fill="black" textAnchor="middle">
+        <text
+          x={xAxisLabelPosition}
+          y={svgHeight - 5}
+          style={{ fontSize: '18px' }}
+          textAnchor="middle"
+          fill="currentColor"
+        >
           {hAxisLabel}
         </text>
         {/* Vertical Axis Label */}
         <text
           x={svgHeight / 8}
           y={svgHeight / 2}
-          fontSize="12px"
-          fill="black"
+          style={{ fontSize: '18px' }}
           textAnchor="end"
-          transform={`rotate(-90 12,${svgHeight / 2})`}
+          transform={`rotate(-90 14,${svgHeight / 2})`}
+          fill="currentColor"
         >
           {vAxisLabel}
         </text>
