b4e12c3e7295c4d98d9ad0dd35cbddab217587a5
magsyg <magsyg@github.com>
2024-05-30 13:19:12 +0200

    add component for backbutton

---
+++ frontend/src/Components/BackButton/BackButton.tsx
@@ -0,0 +1,23 @@
+import { Button } from '../Button';
+import { useTranslation } from 'react-i18next';
+import { useCustomNavigate } from '~/hooks';
+import { KEY } from '~/i18n/constants';
+
+type BackButtonProps = {
+  url?: string;
+};
+
+export function BackButton({ url }: BackButtonProps) {
+  const { t } = useTranslation();
+  const navigate = useCustomNavigate();
+
+  const goBack = () => {
+    navigate({ url: url ? url : -1 });
+  };
+
+  return (
+    <Button theme="success" rounded={true} onClick={goBack}>
+      {t(KEY.common_go_back)}
+    </Button>
+  );
+}
