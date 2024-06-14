c0a98ba3a9f524e3e51410d05033ee188ed2dac5
snorre sæther <snosaet@gmail.com>
2024-06-14 06:00:04 +0200

    removes redundant serializer test + formating

--- backend/samfundet/models/utils/genrate_random_color.py
+++ backend/samfundet/models/utils/genrate_random_color.py
@@ -5,7 +5,7 @@ import random
 
 def generate_random_hex_color() -> str:
     # Generate a random integer between 0 and 16777215 (0xFFFFFF)
-    random_number = random.randint(0, 0xFFFFFF)
+    random_number = random.randint(0, 0xFFFFFF)  # noqa: S311 --> weird crypto rule
     # Convert the integer to a hexadecimal string and pad with leading zeros if necessary
     hex_color = f'#{random_number:06X}'
     return hex_color
