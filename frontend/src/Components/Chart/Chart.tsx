8e18794dd9f1b5fb40ba145aa6a5099b68264762
snorre sæther <snosaet@gmail.com>
2024-06-17 21:43:20 +0200

    removes barfy emojis 🤢

--- frontend/src/Components/Chart/Chart.tsx
+++ frontend/src/Components/Chart/Chart.tsx
@@ -18,22 +18,22 @@ type ChartProps =
     } & CircularChartProps);
 
 /**
- * @param type 👉 chart type.
- * @param data 👉 displayed in the chart.
- * @param chartTitle 👉 title of the chart.
- * @param size 👉 the visual (style) size of the chart.
- * @param xAxisLegend 👉 cartesian charts only. X-axis description, should contain a word description and unit
+ * @param type chart type.
+ * @param data displayed in the chart.
+ * @param chartTitle title of the chart.
+ * @param size the visual (style) size of the chart.
+ * @param xAxisLegend cartesian charts only. X-axis description, should contain a word description and unit
  * (+ unit magnitude if relevant).
- * @param yAxisLegend 👉 cartesian only. Y-axis description, should contain a word description and unit.
+ * @param yAxisLegend cartesian only. Y-axis description, should contain a word description and unit.
  * (+ unit magnitude if relevant).
- * @param splitYLabel 👉 cartesian only. Allows for splitting the y-axis label. Usually a number value, but can split.
+ * @param splitYLabel cartesian only. Allows for splitting the y-axis label. Usually a number value, but can split.
  * Nice if large number. E.g. '1000000', can be split to '1'.
- * @param spliceXLabel 👉 cartesian only. Allows for splitting the x-axis label.
+ * @param spliceXLabel cartesian only. Allows for splitting the x-axis label.
  * Used if the label is long e.g. '24. december', could be split to '24. dec'
- * @param yLabelCount 👉 cartesian only. Controls amount of labels on the y-axis.
- * @param hasXDirLines 👉 cartesian only. Controls weather the chart has horizontal lines.
- * @param hasYDirLines 👉 cartesian only. Controls weather the chart has vertical lines.
- * @param legend 👉 circle only. description of chart data.
+ * @param yLabelCount cartesian only. Controls amount of labels on the y-axis.
+ * @param hasXDirLines cartesian only. Controls weather the chart has horizontal lines.
+ * @param hasYDirLines cartesian only. Controls weather the chart has vertical lines.
+ * @param legend circle only. description of chart data.
  * */
 export function Chart({
   type,
