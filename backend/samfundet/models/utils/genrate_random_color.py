2a95d38e408d019d62394508a8b357328318273e
snorre sæther <snosaet@gmail.com>
2024-06-20 23:04:11 +0200

    uwuwuuwuw RuFF !

--- backend/samfundet/models/utils/genrate_random_color.py
+++ backend/samfundet/models/utils/genrate_random_color.py
@@ -1,11 +1,11 @@
 from __future__ import annotations
 
-import random
+import secrets
 
 
 def generate_random_hex_color() -> str:
     # Generate a random integer between 0 and 16777215 (0xFFFFFF)
-    random_number = random.randint(0, 0xFFFFFF)  # noqa: S311 --> weird crypto rule
+    random_number = secrets.randbelow(0xFFFFFF + 1)  # the safest random color you have ever seen, ruff wants it
     # Convert the integer to a hexadecimal string and pad with leading zeros if necessary
     hex_color = f'#{random_number:06X}'
     return hex_color
