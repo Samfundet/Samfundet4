7d8f7317cbefb52463c255c2ace1e201d9b0e358
robines <espi.robin@gmail.com>
2024-06-24 00:43:48 +0200

    Fix OccupiedFormModal.tsx

--- frontend/src/Components/OccupiedForm/OccupiedFormModal.tsx
+++ frontend/src/Components/OccupiedForm/OccupiedFormModal.tsx
@@ -23,7 +23,7 @@ export function OccupiedFormModal({ recruitmentId = 1 }: OccupiedFormModalProps)
 
       <Modal isOpen={open} className={styles.occupied_modal}>
         <>
-          <button className={styles.close_btn} title="Close" onClick={() => setOpen(false)}>
+          <button type="button" className={styles.close_btn} title="Close" onClick={() => setOpen(false)}>
             <Icon icon="octicon:x-24" width={24} />
           </button>
           <OccupiedForm recruitmentId={recruitmentId} onCancel={() => setOpen(false)} />
