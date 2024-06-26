71a2634ed5608edc0b22a6c247c687d1700240bb
Robin <16273164+robines@users.noreply.github.com>
2024-06-26 21:48:58 +0200

    Fix duplicate route (#1233)

--- backend/samfundet/urls.py
+++ backend/samfundet/urls.py
@@ -43,9 +43,7 @@ router.register('recruitment-position', views.RecruitmentPositionView, 'recruitm
 router.register('recruitment-admisisons-for-applicant', views.RecruitmentAdmissionForApplicantView, 'recruitment_admissions_for_applicant')
 router.register('recruitment-admisisons-for-group', views.RecruitmentAdmissionForGangView, 'recruitment_admissions_for_group')
 router.register('recruitment-admisisons-for-gang', views.RecruitmentAdmissionForGangView, 'recruitment_admissions_for_gang')
-router.register('recruitment-admissions-for-applicant', views.RecruitmentAdmissionForApplicantView, 'recruitment_admissions_for_applicant')
 router.register('recruitment-admissions-for-position', views.RecruitmentAdmissionForRecruitmentPositionView, 'recruitment_admissions_for_position')
-router.register('recruitment-admissions-for-gang', views.RecruitmentAdmissionForGangView, 'recruitment_admissions_for_gang')
 router.register('interview', views.InterviewView, 'interview')
 
 app_name = 'samfundet'
