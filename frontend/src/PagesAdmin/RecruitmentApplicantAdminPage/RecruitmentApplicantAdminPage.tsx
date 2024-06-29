ca93a72bd8a37d0066969bd11fe0081e3f45efe3
robines <espi.robin@gmail.com>
2024-06-29 05:30:32 +0200

    Fix routes merge

--- frontend/src/PagesAdmin/RecruitmentApplicantAdminPage/RecruitmentApplicantAdminPage.tsx
+++ frontend/src/PagesAdmin/RecruitmentApplicantAdminPage/RecruitmentApplicantAdminPage.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { BackButton, Button, Link, Page, SamfundetLogoSpinner } from '~/Components';
-import { getRecruitmentApplicationsForRecruiter, withdrawRecruitmentApplicationRecruiter  } from '~/api';
+import { getRecruitmentApplicationsForRecruiter, withdrawRecruitmentApplicationRecruiter } from '~/api';
 import { RecruitmentApplicationDto, RecruitmentUserDto } from '~/dto';
 import { KEY } from '~/i18n/constants';
 import { reverse } from '~/named-urls';
