20cd59381324aceb4e31eaf911be5d2360295ac5
robines <espi.robin@gmail.com>
2024-07-31 00:43:04 +0200

    Remove accidental import

--- frontend/src/PagesAdmin/RecruitmentGangOverviewPage/RecruitmentGangOverviewPage.tsx
+++ frontend/src/PagesAdmin/RecruitmentGangOverviewPage/RecruitmentGangOverviewPage.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next';
 import { useParams, useRouteLoaderData } from 'react-router-dom';
 import { Button, Link } from '~/Components';
 import { Table } from '~/Components/Table';
-import { getRecruitmentGangs, withdrawRecruitmentApplicationRecruiter } from '~/api';
+import { getRecruitmentGangs } from '~/api';
 import { type RecruitmentGangDto } from '~/dto';
 import { useTitle } from '~/hooks';
 import { KEY } from '~/i18n/constants';
