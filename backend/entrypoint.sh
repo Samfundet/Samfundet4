c59dbd7c6a3e7217b3c981cab7cee9a08ba39c48
Mathias Aas <mathias.aas123@gmail.com>
2024-08-22 21:41:09 +0200

    Change to poetry

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pipenv run python manage.py collectstatic --noinput
-pipenv run python manage.py makemigrations
-pipenv run python manage.py migrate
-pipenv run python manage.py seed_if_db_empty
+poetry run python manage.py collectstatic --noinput
+poetry run python manage.py makemigrations
+poetry run python manage.py migrate
+poetry run python manage.py seed_if_db_empty
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
