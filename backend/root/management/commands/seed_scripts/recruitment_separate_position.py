5aca7222f0298afd18b6c54d5bdf4a3e09c04aff
magsyg <magsyg@github.com>
2024-08-22 19:07:06 +0200

    fix: ruff

--- backend/root/management/commands/seed_scripts/recruitment_separate_position.py
+++ backend/root/management/commands/seed_scripts/recruitment_separate_position.py
@@ -16,7 +16,12 @@ def seed():
     for recruitment in recruitments:
         for i in range(5):
             RecruitmentSeparatePosition.objects.create(
-                recruitment=recruitment, name_nb=f'{words(2)} ({i})', name_en=f'{words(2)} ({i})', description_en=f'{words(5)}', description_nb=f'{words(5)}', url='https://open.spotify.com/track/6rbLnlNqSDSlvw5Z5beetG'
+                recruitment=recruitment,
+                name_nb=f'{words(2)} ({i})',
+                name_en=f'{words(2)} ({i})',
+                description_en=f'{words(5)}',
+                description_nb=f'{words(5)}',
+                url='https://open.spotify.com/track/6rbLnlNqSDSlvw5Z5beetG',
             )
             created_count += 1
     yield 100, f'Created {created_count} recruitment_separate_positions'
