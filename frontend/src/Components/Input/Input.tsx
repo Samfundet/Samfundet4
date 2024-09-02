c6a3b81ff54e92ae6022c56703182894ef8cefe7
robines <espi.robin@gmail.com>
2024-09-03 01:49:40 +0200

    Automatically convert to Number for number inputs on onChange

--- frontend/src/Components/Input/Input.tsx
+++ frontend/src/Components/Input/Input.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 import styles from './Input.module.scss';
 import classNames from 'classnames';
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  onChange: (...event: unknown[]) => void;
+}
 
-/* eslint-disable react/prop-types */
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, onChange, ...props }, ref) => {
   return (
     <input
       type={type}
       className={classNames(styles.input_field, type === 'number' && styles.number_input, className)}
+      onChange={(event) => (type === 'number' ? onChange?.(+event.target.value) : onChange?.(event.target.value))}
       ref={ref}
       {...props}
     />
