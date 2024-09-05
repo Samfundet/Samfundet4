fa42582a2b670592a59b8741e877b9d5fb822089
robines <espi.robin@gmail.com>
2024-09-05 21:39:38 +0200

    Fix import merge

--- backend/samfundet/admin.py
+++ backend/samfundet/admin.py
@@ -23,7 +23,7 @@ from root.custom_classes.admin_classes import (
 
 from .models.role import Role, UserOrgRole, UserGangRole, UserGangSectionRole
 
-from .models.event import Event, EventGroup, EventRegistration
+from .models.event import Event, EventGroup, EventRegistration, PurchaseFeedbackModel
 from .models.general import (
     Tag,
     Gang,
