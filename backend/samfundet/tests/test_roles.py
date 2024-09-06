b98493805839a79583b16ccbfeab288f12ad3405
robines <espi.robin@gmail.com>
2024-09-06 03:40:36 +0200

    Update tests, and add hierarchy testing

--- backend/samfundet/tests/test_roles.py
+++ backend/samfundet/tests/test_roles.py
@@ -8,65 +8,232 @@ from samfundet.models.role import Role, UserOrgRole, UserGangRole, UserGangSecti
 
 
 def test_has_perm_superuser(
-    fixture_role_auth_backend: RoleAuthBackend, fixture_superuser: User, fixture_organization: Organization, fixture_org_permission: Permission
+    fixture_role_auth_backend: RoleAuthBackend, fixture_superuser: User, fixture_organization: Organization,
+    fixture_org_permission: Permission
 ):
+    """Test that superusers have permissions to all resources even without any roles."""
     assert fixture_role_auth_backend.has_perm(fixture_superuser, fixture_org_permission.codename, fixture_organization)
 
 
-def test_has_perm_user_with_orgrole(
-    fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_role: Role, fixture_organization: Organization, fixture_org_permission: Permission
+def test_has_perm_inactive_user(
+    fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_organization: Organization,
+    fixture_org_permission: Permission,
 ):
-    fixture_role.permissions.add(fixture_org_permission)
-
     assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
 
-    org_role = UserOrgRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_organization)
-    org_role.save()
+
+def test_has_perm_inactive_user_with_role(
+    fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_organization: Organization,
+    fixture_org_permission: Permission, fixture_role: Role
+):
+    """Test that inactive users who would otherwise have access to a resource, don't."""
+    fixture_role.permissions.add(fixture_org_permission)
+    UserOrgRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_organization)
+    fixture_user.is_active = False
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
+
+
+def test_has_perm_no_obj(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                         fixture_org_permission: Permission):
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, None)
+
+
+def test_has_perm_superuser_no_obj(fixture_role_auth_backend: RoleAuthBackend, fixture_superuser: User,
+                                   fixture_org_permission: Permission):
+    assert not fixture_role_auth_backend.has_perm(fixture_superuser, fixture_org_permission.codename, None)
+
+
+def test_has_perm_user_with_no_roles(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_role: Role,
+                                     fixture_organization: Organization, fixture_gang: Gang,
+                                     fixture_gang_section: GangSection, fixture_org_permission: Permission,
+                                     fixture_gang_permission: Permission, fixture_gang_section_permission: Permission):
+    """Sanity check. Within the scope of our auth backend, a user with no roles should have no permissions,
+    on any hierarchical level."""
+
+    # Create a role with permission to our example resources (org/gang/section), but don't attach it to user.
+    fixture_role.permissions.add(fixture_org_permission)
+    fixture_role.permissions.add(fixture_gang_permission)
+    fixture_role.permissions.add(fixture_gang_section_permission)
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                                  fixture_gang_section)
+
+
+def test_has_perm_user_with_org_role(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_role: Role,
+                                     fixture_organization: Organization, fixture_org_permission: Permission):
+    """Test that giving user an OrgRole with permission to access a resource, actually gives them access."""
+    fixture_role.permissions.add(fixture_org_permission)
+    UserOrgRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_organization)
 
     assert fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
 
 
-def test_has_perm_user_with_gangrole(
-    fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_role: Role, fixture_gang: Gang, fixture_gang_permission: Permission
-):
+def test_has_perm_user_with_gang_role(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                                      fixture_role: Role, fixture_gang: Gang, fixture_gang_permission: Permission):
+    """Test that giving user a GangRole with permission to access a resource, actually gives them access."""
     fixture_role.permissions.add(fixture_gang_permission)
-
-    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
-
-    gang_role = UserGangRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang)
-    gang_role.save()
+    UserGangRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang)
 
     assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
 
 
-def test_has_perm_user_with_sectionrole(
-    fixture_role_auth_backend: RoleAuthBackend,
-    fixture_user: User,
-    fixture_role: Role,
-    fixture_gang_section: GangSection,
-    fixture_gang_section_permission: Permission,
-):
+def test_has_perm_user_with_section_role(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                                         fixture_role: Role, fixture_gang_section: GangSection,
+                                         fixture_gang_section_permission: Permission):
+    """Test that giving user a GangSectionRole with permission to access a resource, actually gives them access."""
     fixture_role.permissions.add(fixture_gang_section_permission)
+    UserGangSectionRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang_section)
 
-    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename, fixture_gang_section)
-
-    section_role = UserGangSectionRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang_section)
-    section_role.save()
-
-    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename, fixture_gang_section)
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                              fixture_gang_section)
 
 
-def test_has_perm_inactive_user(
-    fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_organization: Organization, fixture_org_permission: Permission
-):
-    fixture_user.is_active = False
-    fixture_user.save()
+def test_has_perm_different_orgs(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                                 fixture_organization: Organization, fixture_organization2: Organization,
+                                 fixture_org_permission: Permission, fixture_role: Role):
+    """Test that giving user a role to a specific org, does not give it to other orgs"""
+    fixture_role.permissions.add(fixture_org_permission)
+
     assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
 
+    UserOrgRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_organization)
 
-def test_has_perm_no_obj(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_org_permission: Permission):
-    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, None)
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization2)
 
 
-def test_has_perm_superuser_no_obj(fixture_role_auth_backend: RoleAuthBackend, fixture_superuser: User, fixture_org_permission: Permission):
-    assert not fixture_role_auth_backend.has_perm(fixture_superuser, fixture_org_permission.codename, None)
+def test_has_perm_different_gangs(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                                  fixture_gang: Gang, fixture_gang2: Gang,
+                                  fixture_gang_permission: Permission, fixture_role: Role):
+    """Test that giving user a role to a specific gang, does not give it to other gangs"""
+    fixture_role.permissions.add(fixture_gang_permission)
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+
+    UserGangRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang)
+
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang2)
+
+
+def test_has_perm_different_gang_sections(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                                          fixture_gang_section: GangSection, fixture_gang_section2: Gang,
+                                          fixture_gang_section_permission: Permission, fixture_role: Role):
+    """Test that giving user a role to a specific gang section, does not give it to other gang sections"""
+    fixture_role.permissions.add(fixture_gang_section_permission)
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                                  fixture_gang_section)
+
+    UserGangSectionRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang_section)
+
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                              fixture_gang_section)
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                                  fixture_gang_section2)
+
+
+def test_has_perm_different_users(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User, fixture_user2: User,
+                                  fixture_organization: Organization, fixture_gang: Gang,
+                                  fixture_gang_section: GangSection, fixture_org_permission: Permission,
+                                  fixture_gang_permission: Permission, fixture_gang_section_permission: Permission,
+                                  fixture_role: Role):
+    """Test that giving user a role, does not give it to other users"""
+    fixture_role.permissions.add(fixture_org_permission)
+    fixture_role.permissions.add(fixture_gang_permission)
+    fixture_role.permissions.add(fixture_gang_section_permission)
+
+    UserOrgRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_organization)
+    UserGangRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang)
+    UserGangSectionRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang_section)
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user2, fixture_org_permission.codename, fixture_organization)
+    assert not fixture_role_auth_backend.has_perm(fixture_user2, fixture_gang_permission.codename, fixture_gang)
+    assert not fixture_role_auth_backend.has_perm(fixture_user2, fixture_gang_section_permission.codename,
+                                                  fixture_gang_section)
+
+
+def test_has_perm_org_downward(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                               fixture_organization: Organization, fixture_organization2: Organization,
+                               fixture_gang: Gang, fixture_gang2: Gang, fixture_gang_section: GangSection,
+                               fixture_role: Role, fixture_org_permission: Permission,
+                               fixture_gang_permission: Permission, fixture_gang_section_permission: Permission):
+    """Test that giving permission on org/gang level, also gives it downwards (gang/section)."""
+    fixture_role.permissions.add(fixture_gang_section_permission)
+    fixture_role.permissions.add(fixture_gang_permission)
+
+    fixture_gang.organization = fixture_organization
+    fixture_gang_section.gang = fixture_gang
+
+    # Giving a user an Org role should give the same permissions on Gang and Section levels
+
+    org_role = UserOrgRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_organization)
+
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                              fixture_gang_section)
+
+    org_role.delete()
+
+    # Permissions should be gone after deleting org role
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                                  fixture_gang_section)
+
+    # Giving a user a Gang role should give the same permissions on Section level
+
+    gang_role = UserGangRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang)
+
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                              fixture_gang_section)
+
+    gang_role.delete()
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                                  fixture_gang_section)
+
+    # Give the user the Org role again, and ensure that after we detach Gang and Section from the Organization,
+    # we no longer have permissions.
+    UserOrgRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_organization)
+
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                              fixture_gang_section)
+
+    fixture_gang_section.gang = fixture_gang2
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_section_permission.codename,
+                                                  fixture_gang_section)
+
+    assert fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+
+    fixture_gang.organization = fixture_organization2
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+
+
+def test_has_perm_section_upward(fixture_role_auth_backend: RoleAuthBackend, fixture_user: User,
+                                 fixture_organization: Organization, fixture_gang: Gang,
+                                 fixture_gang_section: GangSection, fixture_role: Role,
+                                 fixture_org_permission: Permission, fixture_gang_permission: Permission,
+                                 fixture_gang_section_permission: Permission):
+    """Test that giving permission on section/gang level, does not give it upwards (gang/org)."""
+    fixture_role.permissions.add(fixture_org_permission)
+    fixture_role.permissions.add(fixture_gang_permission)
+    fixture_role.permissions.add(fixture_gang_section_permission)
+
+    fixture_gang.organization = fixture_organization
+    fixture_gang_section.gang = fixture_gang
+
+    section_role = UserGangSectionRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang_section)
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_gang_permission.codename, fixture_gang)
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
+
+    section_role.delete()
+
+    UserGangRole.objects.create(user=fixture_user, role=fixture_role, obj=fixture_gang)
+
+    assert not fixture_role_auth_backend.has_perm(fixture_user, fixture_org_permission.codename, fixture_organization)
