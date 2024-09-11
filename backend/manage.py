3760befd5cd604d92d1a0439867146f28770b16c
Emil Telstad <22004178+emilte@users.noreply.github.com>
2024-09-11 15:47:21 +0200

    Set default DJANGO_SETTINGS_MODULE #1328.

--- backend/manage.py
+++ backend/manage.py
@@ -3,12 +3,15 @@
 
 from __future__ import annotations
 
+import os
 import sys
 
 
 def main() -> None:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'root.settings')
     try:
         from django.core.management import execute_from_command_line
+
     except ImportError as exc:
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
