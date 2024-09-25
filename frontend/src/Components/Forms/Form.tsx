246ad741673fed7eeccda88a6433d47b913dac31
Robin <16273164+robines@users.noreply.github.com>
2024-09-25 09:45:31 +0200

    Add React Hook Form and Zod (#1306)
    * Add packages, set up zod with translations
    
    * Update translation files
    
    * Create form components (from shadcn/ui)
    
    * Add styling for FormLabel
    
    * Create ExampleForm and schema for username/pass
    
    * Add width: 100% to input styling
    
    * Add dropdown to example, render validated data
    
    * Wrap Dropdown in forwardRef for proper control by react-hook-form
    
    * Automatically convert to Number for number inputs on onChange
    
    * Add number field to example form
    
    * Add missing number_input css class
    
    * Fix Checkbox component
    
    * Add Checkbox to example form
    
    * Simplify Checkbox component
    
    * Fix SamfFormFieldTypes after Checkbox changes
    
    * Begin docs
    
    * Biome
    
    * Move schema definition outside ExampleForm
    
    * Update docs
    
    * Update index title

---
+++ frontend/src/Components/Forms/Form.tsx
@@ -0,0 +1,134 @@
+import { Slot } from '@radix-ui/react-slot';
+import * as React from 'react';
+import {
+  Controller,
+  type ControllerProps,
+  type FieldPath,
+  type FieldValues,
+  FormProvider,
+  useFormContext,
+} from 'react-hook-form';
+
+import classNames from 'classnames';
+import styles from './Forms.module.scss';
+
+export const Form = FormProvider;
+
+type FormFieldContextValue<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  name: TName;
+};
+
+const FormFieldContext = React.createContext<FormFieldContextValue>({} as FormFieldContextValue);
+
+export const FormField = <
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>({
+  ...props
+}: ControllerProps<TFieldValues, TName>) => {
+  return (
+    <FormFieldContext.Provider value={{ name: props.name }}>
+      <Controller {...props} />
+    </FormFieldContext.Provider>
+  );
+};
+
+export const useFormField = () => {
+  const fieldContext = React.useContext(FormFieldContext);
+  const itemContext = React.useContext(FormItemContext);
+  const { getFieldState, formState } = useFormContext();
+
+  const fieldState = getFieldState(fieldContext.name, formState);
+
+  if (!fieldContext) {
+    throw new Error('useFormField should be used within <FormField>');
+  }
+
+  const { id } = itemContext;
+
+  return {
+    id,
+    name: fieldContext.name,
+    formItemId: `${id}-form-item`,
+    formDescriptionId: `${id}-form-item-description`,
+    formMessageId: `${id}-form-item-message`,
+    ...fieldState,
+  };
+};
+
+type FormItemContextValue = {
+  id: string;
+};
+
+const FormItemContext = React.createContext<FormItemContextValue>({} as FormItemContextValue);
+
+export const FormItem = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(({ ...props }, ref) => {
+  const id = React.useId();
+
+  return (
+    <FormItemContext.Provider value={{ id }}>
+      <div ref={ref} {...props} />
+    </FormItemContext.Provider>
+  );
+});
+FormItem.displayName = 'FormItem';
+
+interface FormLabelProps extends React.HTMLAttributes<HTMLLabelElement> {
+  className?: string;
+}
+
+export const FormLabel = React.forwardRef<
+  React.ElementRef<'label'>,
+  React.ComponentPropsWithoutRef<'label'> & FormLabelProps
+>(({ className, ...props }, ref) => {
+  const { error, formItemId } = useFormField();
+
+  return (
+    <label
+      ref={ref}
+      className={classNames(styles.label, error && styles.error, className)}
+      htmlFor={formItemId}
+      {...props}
+    />
+  );
+});
+FormLabel.displayName = 'FormLabel';
+
+export const FormControl = React.forwardRef<React.ElementRef<typeof Slot>, React.ComponentPropsWithoutRef<typeof Slot>>(
+  ({ ...props }, ref) => {
+    const { error, formItemId, formDescriptionId, formMessageId } = useFormField();
+
+    return (
+      <Slot
+        ref={ref}
+        id={formItemId}
+        aria-describedby={!error ? `${formDescriptionId}` : `${formDescriptionId} ${formMessageId}`}
+        aria-invalid={!!error}
+        {...props}
+      />
+    );
+  },
+);
+FormControl.displayName = 'FormControl';
+
+export const FormMessage = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>(
+  // eslint-disable-next-line react/prop-types
+  ({ className, children, ...props }, ref) => {
+    const { error, formMessageId } = useFormField();
+    const body = error ? String(error?.message) : children;
+
+    if (!body) {
+      return null;
+    }
+
+    return (
+      <p ref={ref} id={formMessageId} className={classNames(styles.error, className)} {...props}>
+        {body}
+      </p>
+    );
+  },
+);
+FormMessage.displayName = 'FormMessage';
---
+++ frontend/src/Components/Input/Input.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames';
+import * as React from 'react';
+import styles from './Input.module.scss';
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  onChange: (...event: unknown[]) => void;
+}
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, onChange, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={classNames(styles.input_field, type === 'number' && styles.number_input, className)}
+      onChange={(event) => (type === 'number' ? onChange?.(+event.target.value) : onChange?.(event.target.value))}
+      ref={ref}
+      {...props}
+    />
+  );
+});
+Input.displayName = 'Input';
