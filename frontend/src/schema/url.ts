c66d6f81da942b7fb21e73abf30b8b39251a4457
robines <espi.robin@gmail.com>
2024-10-02 20:30:14 +0200

    Remove ftp|smtp from regex

--- frontend/src/schema/url.ts
+++ frontend/src/schema/url.ts
@@ -2,4 +2,4 @@ import { z } from 'zod';
 
 export const WEBSITE_URL = z
   .string()
-  .regex(/^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;%=]+\/?)*$/);
+  .regex(/^(https?:\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;%=]+\/?)*$/);
