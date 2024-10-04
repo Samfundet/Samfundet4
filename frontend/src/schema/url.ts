6e4e733b6d061acf3270e9a490266e6941caf178
robines <espi.robin@gmail.com>
2024-10-04 03:33:34 +0200

    Add comment explaining why we don't use .url()

--- frontend/src/schema/url.ts
+++ frontend/src/schema/url.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 
+// The reason we don't use zod's .url() is because it allows too much. Eg. it considers this valid: https:....google.com
 export const WEBSITE_URL = z
   .string()
   .regex(/^(https?:\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[A-Za-z0-9-._~:/?#\[\]@!$&'()*+,;%=]+\/?)*\/?$/);
