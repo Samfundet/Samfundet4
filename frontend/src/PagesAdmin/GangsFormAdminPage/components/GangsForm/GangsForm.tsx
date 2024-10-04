65013dea6cd458cf7aa45415b1193b1925be379f
robines <espi.robin@gmail.com>
2024-10-04 03:34:36 +0200

    Add info page and gang type to form

--- frontend/src/PagesAdmin/GangsFormAdminPage/components/GangsForm/GangsForm.tsx
+++ frontend/src/PagesAdmin/GangsFormAdminPage/components/GangsForm/GangsForm.tsx
@@ -1,23 +1,25 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 import { z } from 'zod';
-import { Button, Form, FormControl, FormField, FormItem, FormLabel, FormMessage, Input } from '~/Components';
-import { postGang, putGang } from '~/api';
-import type { GangDto } from '~/dto';
+import { Button, Dropdown, Form, FormControl, FormField, FormItem, FormLabel, FormMessage, Input } from '~/Components';
+import { getGangList, getInformationPages, postGang, putGang } from '~/api';
+import type { GangDto, GangTypeDto, InformationPageDto } from '~/dto';
 import { KEY } from '~/i18n/constants';
-import { ABBREVIATION, NAME } from '~/schema/gang';
+import { ABBREVIATION, GANG_INFO_PAGE, GANG_TYPE, NAME } from '~/schema/gang';
 import { WEBSITE_URL } from '~/schema/url';
-import { lowerCapitalize } from '~/utils';
+import { dbT, lowerCapitalize } from '~/utils';
 import styles from './GangsForm.module.scss';
 
 const schema = z.object({
   name_nb: NAME.min(1),
   name_en: NAME.min(1),
-  abbreviation: ABBREVIATION.optional().or(z.literal('')),
-  website: WEBSITE_URL,
+  abbreviation: ABBREVIATION.nullish().or(z.literal('')),
+  website: WEBSITE_URL.or(z.literal('')),
+  info_page: GANG_INFO_PAGE.nullish(),
+  gang_type: GANG_TYPE.nullish(),
 });
 
 type Props = {
@@ -28,7 +30,35 @@ type Props = {
 
 export function GangsForm({ gang, onSuccess, onError }: Props) {
   const { t } = useTranslation();
+  const [loadingInfoPages, setLoadingInfoPages] = useState(true);
+  const [infoPages, setInfoPages] = useState<InformationPageDto[]>();
+  const [loadingGangTypes, setLoadingGangTypes] = useState(true);
+  const [gangTypes, setGangTypes] = useState<GangTypeDto[]>();
+
   const [submitting, setSubmitting] = useState(false);
+
+  useEffect(() => {
+    setLoadingInfoPages(true);
+    setLoadingGangTypes(true);
+    getInformationPages()
+      .then(setInfoPages)
+      .finally(() => setLoadingInfoPages(false));
+    getGangList()
+      .then(setGangTypes)
+      .finally(() => setLoadingGangTypes(false));
+  }, []);
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: we want labels to be updated when language changes
+  const infoPageOptions = useMemo(
+    () => infoPages?.map((p) => ({ value: p.slug_field, label: dbT(p, 'title') as string })),
+    [infoPages, t],
+  );
+
+  // biome-ignore lint/correctness/useExhaustiveDependencies: we want labels to be updated when language changes
+  const gangTypeOptions = useMemo(
+    () => gangTypes?.map((gt) => ({ value: gt.id, label: dbT(gt, 'title') as string })),
+    [gangTypes, t],
+  );
 
   const form = useForm<z.infer<typeof schema>>({
     resolver: zodResolver(schema),
@@ -37,6 +67,7 @@ export function GangsForm({ gang, onSuccess, onError }: Props) {
       name_en: gang?.name_en ?? '',
       abbreviation: gang?.abbreviation ?? '',
       website: gang?.webpage ?? '',
+      info_page: gang?.info_page,
     },
   });
 
@@ -114,6 +145,50 @@ export function GangsForm({ gang, onSuccess, onError }: Props) {
                 <FormLabel>{lowerCapitalize(t(KEY.admin_gangsadminpage_webpage))}</FormLabel>
                 <FormControl>
                   <Input {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className={styles.row}>
+          <FormField
+            control={form.control}
+            name="info_page"
+            render={({ field }) => (
+              <FormItem className={styles.form_item}>
+                <FormLabel>{lowerCapitalize(t(KEY.information_page))}</FormLabel>
+                <FormControl>
+                  {loadingInfoPages ? (
+                    <>{t(KEY.common_loading)}...</>
+                  ) : (
+                    <Dropdown
+                      defaultValue={infoPageOptions?.find((o) => o.value === gang?.info_page)}
+                      options={infoPageOptions}
+                      {...field}
+                    />
+                  )}
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="gang_type"
+            render={({ field }) => (
+              <FormItem className={styles.form_item}>
+                <FormLabel>{lowerCapitalize(t(KEY.common_gang_type))}</FormLabel>
+                <FormControl>
+                  {loadingGangTypes ? (
+                    <>{t(KEY.common_loading)}...</>
+                  ) : (
+                    <Dropdown
+                      defaultValue={gangTypeOptions?.find((o) => o.value === gang?.gang_type)}
+                      options={gangTypeOptions}
+                      {...field}
+                    />
+                  )}
                 </FormControl>
                 <FormMessage />
               </FormItem>
--- frontend/src/schema/gang.ts
+++ frontend/src/schema/gang.ts
@@ -1,5 +1,10 @@
 import { z } from 'zod';
+import { INFO_PAGE_SLUG } from '~/schema/infopage';
 
 export const NAME = z.string();
 
 export const ABBREVIATION = z.string();
+
+export const GANG_INFO_PAGE = INFO_PAGE_SLUG;
+
+export const GANG_TYPE = z.number();
