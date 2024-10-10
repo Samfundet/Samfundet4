02988a2a1a8076d46ed58cbc0c800b7ea932c470
magsyg <magsyg@github.com>
2024-10-10 21:45:30 +0200

    lol fix

--- frontend/src/PagesAdmin/RecruitmentGangOverviewPage/RecruitmentGangOverviewPage.tsx
+++ frontend/src/PagesAdmin/RecruitmentGangOverviewPage/RecruitmentGangOverviewPage.tsx
@@ -11,7 +11,7 @@ import { reverse } from '~/named-urls';
 import { ROUTES } from '~/routes';
 import { dbT, lowerCapitalize } from '~/utils';
 import { AdminPageLayout } from '../AdminPageLayout/AdminPageLayout';
-import { AppletContainer, RecruitmentInterviewGroupsList } from './components';
+import { AppletContainer, RecruitmentInterviewGroupsList } from './gitsucks';
 
 export function RecruitmentGangOverviewPage() {
   const { recruitmentId } = useParams();
