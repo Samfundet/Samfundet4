8a0e7dd19aaf8f0adc39b7b65476a9cd660a0684
robines <espi.robin@gmail.com>
2024-10-25 01:58:50 +0200

    Remove Input component hacks

--- frontend/src/Components/Input/Input.tsx
+++ frontend/src/Components/Input/Input.tsx
@@ -2,23 +2,16 @@ import classNames from 'classnames';
 import * as React from 'react';
 import styles from './Input.module.scss';
 
-export interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
-  onChange: (value: string | number | readonly string[] | null) => void;
-  value: string | number | readonly string[] | null | undefined;
-}
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, onChange, value, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={classNames(styles.input_field, type === 'number' && styles.number_input, className)}
-        onChange={(event) => (type === 'number' ? onChange?.(+event.target.value) : onChange?.(event.target.value))}
-        ref={ref}
-        value={value === null ? '' : value}
-        {...props}
-      />
-    );
-  },
-);
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={classNames(styles.input_field, type === 'number' && styles.number_input, className)}
+      ref={ref}
+      {...props}
+    />
+  );
+});
 Input.displayName = 'Input';
