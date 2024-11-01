4c40b86619b7ff548cb42b4bbb80d315deb3925a
Snorre Jr <snosaet@gmail.com>
2024-11-02 00:23:08 +0100

    setup django rest framework pagination

---
+++ backend/samfundet/pagination.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from rest_framework.pagination import PageNumberPagination
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
