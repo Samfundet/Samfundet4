ff1da3ba1f41903267a2232eaf69853fa0def595
Snorre Jr <snosaet@gmail.com>
2024-11-02 03:24:19 +0100

    removes the use of pagination globaly, now it must be specified in the view

--- backend/root/settings/base.py
+++ backend/root/settings/base.py
@@ -180,8 +180,6 @@ REST_FRAMEWORK = {
         # 'root.custom_classes.permission_classes.CustomDjangoObjectPermissions',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
 }
 ### End: DRF ###
 
