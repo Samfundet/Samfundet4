98bb7c5f830fc84e71b79be20e6a6be3d0aebcc0
Snorre Jr <snosaet@gmail.com>
2024-11-06 15:11:55 +0100

    added classname to ellipsis

--- frontend/src/Components/Pagination/components/PaginationEllipsis/PaginationEllipsis.tsx
+++ frontend/src/Components/Pagination/components/PaginationEllipsis/PaginationEllipsis.tsx
@@ -1,8 +1,9 @@
+import classNames from 'classnames';
 import React from 'react';
 import styles from './PaginationEllipsis.module.scss';
 export const PaginationEllipsis = React.forwardRef<HTMLSpanElement, React.ComponentProps<'span'>>(
   ({ className, ...props }, ref) => (
-    <span ref={ref} className={`${styles.ellipsis} ${className}`} {...props}>
+    <span ref={ref} className={classNames(styles.ellipsis, className)} {...props}>
       &hellip;
     </span>
   ),
