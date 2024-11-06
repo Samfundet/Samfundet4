b873652c23e2c03b5fb017ca41fd4108a32ea67a
Snorre Jr <snosaet@gmail.com>
2024-11-06 17:10:06 +0100

    newline between pagination imte comp and imports

--- frontend/src/Components/Pagination/components/PaginationItem/PaginationItem.tsx
+++ frontend/src/Components/Pagination/components/PaginationItem/PaginationItem.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
 import styles from './PaginationItem.module.scss';
+
 export const PaginationItem = React.forwardRef<HTMLLIElement, React.ComponentProps<'li'>>(
   ({ className, ...props }, ref) => <li ref={ref} className={classNames(styles.item, className)} {...props} />,
 );
