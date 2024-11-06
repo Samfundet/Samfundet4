253db6f46dc1c94129f9658953b2647dd4c1d968
Snorre Jr <snosaet@gmail.com>
2024-11-06 16:44:50 +0100

    adds whitespace in PaginationContent

--- frontend/src/Components/Pagination/components/PaginationContent/PaginationContent.tsx
+++ frontend/src/Components/Pagination/components/PaginationContent/PaginationContent.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
 import React from 'react';
 import styles from './PaginationContent.module.scss';
+
 export const PaginationContent = React.forwardRef<HTMLUListElement, React.ComponentProps<'ul'>>(
   ({ className, ...props }, ref) => <ul ref={ref} className={classNames(styles.list, className)} {...props} />,
 );
