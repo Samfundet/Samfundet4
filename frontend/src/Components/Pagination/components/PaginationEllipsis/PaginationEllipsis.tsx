7fd57785500a6ab48287a70f55a9b28e4eb9d235
Snorre Jr <snosaet@gmail.com>
2024-11-06 17:09:23 +0100

    newline between import and comp

--- frontend/src/Components/Pagination/components/PaginationEllipsis/PaginationEllipsis.tsx
+++ frontend/src/Components/Pagination/components/PaginationEllipsis/PaginationEllipsis.tsx
@@ -2,6 +2,7 @@ import { Icon } from '@iconify/react';
 import classNames from 'classnames';
 import React from 'react';
 import styles from './PaginationEllipsis.module.scss';
+
 export const PaginationEllipsis = React.forwardRef<HTMLSpanElement, React.ComponentProps<'span'>>(
   ({ className, ...props }, ref) => (
     <span ref={ref} className={classNames(styles.ellipsis, className)} {...props}>
