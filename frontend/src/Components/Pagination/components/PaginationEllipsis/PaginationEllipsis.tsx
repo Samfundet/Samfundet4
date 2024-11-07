850c0c2eac782d0c9e17c24ccf6d80e7c8558a69
Snorre Sæther <112980079+Snorre98@users.noreply.github.com>
2024-11-07 20:12:00 +0100

    Adds pagination component (#1586)
    * adds all, very inspired by shadcn
    
    * removes unused code
    
    * adds Pagination to outer barrel file
    
    * removed more unused code and comments
    
    * improved / simplified story
    
    * adds supress unique key
    
    * refactors styling
    
    * adds margin to component
    
    * Update frontend/src/Components/Pagination/components/PaginationEllipsis/PaginationEllipsis.tsx
    
    &hellip;
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>
    
    * add classnames
    
    * added classname to ellipsis
    
    * added ellipsis icon, same width as chevrons in pagination control (in DrfPagination)
    
    * uses classNames in PaginationContent
    
    * adds whitespace in PaginationContent
    
    * adds classNames in Pagination
    
    * whitespace in pagination
    
    * style to make button width consistent
    
    * renamed PaginationControl
    
    * renamed controllText to controlSymbol
    
    * removed style modularity from pagination control (removes props)
    
    * PaginationControllProps -> PaginationControlProps
    
    * newline between import and comp
    
    * newline between pagination imte comp and imports
    
    * barrel x 6
    
    * use useMemo
    
    * fix storybook
    
    * sibling count and boundry count
    
    * klink siblings with boundries
    
    * changes to good ol regular function
    
    * better styling
    
    * renamed to paged pagination
    
    * renamed PaginationControll
    
    * more renaming
    
    * more rename
    
    * extend button prop
    
    * rename
    
    ---------
    
    Co-authored-by: Robin <16273164+robines@users.noreply.github.com>

---
+++ frontend/src/Components/Pagination/components/PaginationEllipsis/PaginationEllipsis.tsx
@@ -0,0 +1,13 @@
+import { Icon } from '@iconify/react';
+import classNames from 'classnames';
+import React from 'react';
+import styles from './PaginationEllipsis.module.scss';
+
+export const PaginationEllipsis = React.forwardRef<HTMLSpanElement, React.ComponentProps<'span'>>(
+  ({ className, ...props }, ref) => (
+    <span ref={ref} className={classNames(styles.ellipsis, className)} {...props}>
+      <Icon icon={'lucide:ellipsis'} />
+    </span>
+  ),
+);
+PaginationEllipsis.displayName = 'PaginationEllipsis';
---
+++ frontend/src/Components/Pagination/index.ts
@@ -0,0 +1,1 @@
+export { PagedPagination } from './PagedPagination';
