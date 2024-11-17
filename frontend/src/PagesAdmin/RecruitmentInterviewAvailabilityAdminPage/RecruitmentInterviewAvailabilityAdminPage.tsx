b52f5c7a1150bac2a020a9f632b018b44fc76512
robines <espi.robin@gmail.com>
2024-11-17 15:26:59 +0100

    Set title

--- frontend/src/PagesAdmin/RecruitmentInterviewAvailabilityAdminPage/RecruitmentInterviewAvailabilityAdminPage.tsx
+++ frontend/src/PagesAdmin/RecruitmentInterviewAvailabilityAdminPage/RecruitmentInterviewAvailabilityAdminPage.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { AdminPageLayout } from '~/PagesAdmin/AdminPageLayout/AdminPageLayout';
+import { useTitle } from '~/hooks';
 import { KEY } from '~/i18n/constants';
 import { lowerCapitalize } from '~/utils';
 import { RecruitmentInterviewAvailabilityForm } from './RecruitmentInterviewAvailabilityForm';
@@ -7,6 +8,7 @@ import { RecruitmentInterviewAvailabilityForm } from './RecruitmentInterviewAvai
 export function RecruitmentInterviewAvailabilityAdminPage() {
   const { t } = useTranslation();
   const title = lowerCapitalize(`${t(KEY.common_edit)} ${t(KEY.interview_availability)}`);
+  useTitle(title);
 
   return (
     <AdminPageLayout title={title}>
