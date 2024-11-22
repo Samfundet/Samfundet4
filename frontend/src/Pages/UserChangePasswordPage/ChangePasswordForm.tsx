e50eaa297be2f0735006808edeed45969d35026a
robines <espi.robin@gmail.com>
2024-11-22 18:56:25 +0100

    Send change password request to backend

--- frontend/src/Pages/UserChangePasswordPage/ChangePasswordForm.tsx
+++ frontend/src/Pages/UserChangePasswordPage/ChangePasswordForm.tsx
@@ -1,18 +1,27 @@
 import { zodResolver } from '@hookform/resolvers/zod';
+import { useMutation } from '@tanstack/react-query';
+import i18next from 'i18next';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
 import { z } from 'zod';
 import { Button, Form, FormControl, FormField, FormItem, FormLabel, FormMessage, Input } from '~/Components';
+import { changePassword } from '~/api';
 import { KEY } from '~/i18n/constants';
 import { PASSWORD } from '~/schema/user';
-import { lowerCapitalize } from '~/utils';
+import { handleServerFormErrors, lowerCapitalize } from '~/utils';
 import styles from './ChangePasswordForm.module.scss';
 
-const schema = z.object({
-  current_password: PASSWORD,
-  new_password: PASSWORD,
-  repeat_password: PASSWORD,
-});
+const schema = z
+  .object({
+    current_password: PASSWORD,
+    new_password: PASSWORD,
+    repeat_password: PASSWORD,
+  })
+  .refine((data) => data.new_password === data.repeat_password, {
+    message: i18next.t(KEY.loginpage_passwords_must_match),
+    path: ['repeat_password'],
+  });
 
 type SchemaType = z.infer<typeof schema>;
 
@@ -28,8 +37,23 @@ export function ChangePasswordForm() {
     },
   });
 
+  const { mutate, isPending } = useMutation({
+    mutationFn: ({ current_password, new_password }: { current_password: string; new_password: string }) =>
+      changePassword(current_password, new_password),
+    onSuccess: () => {
+      form.reset();
+      toast.success(t(KEY.common_update_successful));
+    },
+    onError: (error) =>
+      handleServerFormErrors(error, form, {
+        'Incorrect current': 'Ugyldig nåværende passord',
+        'too short': 'Passordet er for kort. Det må inneholde minst 8 karakterer',
+        'too common': 'Passordet er for vanlig.',
+      }),
+  });
+
   function onSubmit(values: SchemaType) {
-    console.log('Values:', values);
+    mutate({ current_password: values.current_password, new_password: values.new_password });
   }
 
   return (
@@ -38,6 +62,7 @@ export function ChangePasswordForm() {
         <FormField
           name="current_password"
           control={form.control}
+          disabled={isPending}
           render={({ field }) => (
             <FormItem>
               <FormLabel>{lowerCapitalize(`${t(KEY.common_current)} ${t(KEY.common_password)}`)}</FormLabel>
@@ -51,6 +76,7 @@ export function ChangePasswordForm() {
         <FormField
           name="new_password"
           control={form.control}
+          disabled={isPending}
           render={({ field }) => (
             <FormItem>
               <FormLabel>{lowerCapitalize(t(KEY.new_password))}</FormLabel>
@@ -64,6 +90,7 @@ export function ChangePasswordForm() {
         <FormField
           name="repeat_password"
           control={form.control}
+          disabled={isPending}
           render={({ field }) => (
             <FormItem>
               <FormLabel>{lowerCapitalize(`${t(KEY.common_repeat)} ${t(KEY.new_password)}`)}</FormLabel>
@@ -76,7 +103,7 @@ export function ChangePasswordForm() {
         />
 
         <div className={styles.action_row}>
-          <Button type="submit" theme="green">
+          <Button type="submit" theme="green" disabled={isPending}>
             {t(KEY.common_save)}
           </Button>
         </div>
