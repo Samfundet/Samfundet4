99185be864f9e61d73afc73511ebaa514427e32f
magsyg <magsyg@github.com>
2024-12-10 23:41:26 +0100

    feat: fix infinte loop

--- frontend/src/PagesAdmin/RecruitmentPositionFormAdminPage/RecruitmentPositionFormAdminPage.tsx
+++ frontend/src/PagesAdmin/RecruitmentPositionFormAdminPage/RecruitmentPositionFormAdminPage.tsx
@@ -16,7 +16,7 @@ export function RecruitmentPositionFormAdminPage() {
   const navigate = useNavigate();
   const { recruitmentId, gangId, positionId } = useParams();
   const [position, setPosition] = useState<Partial<RecruitmentPositionDto>>();
-  const [users, setUsers] = useState<Partial<UserDto[]>>();
+  const [users, setUsers] = useState<UserDto[]>();
 
   useEffect(() => {
     getUsers().then((data) => {
