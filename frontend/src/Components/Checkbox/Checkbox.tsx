66020ed284111b2d2d66cf1a2698a42ee47ce354
Simen Seeberg-Rommetveit <simensee@gmail.com>
2025-01-30 22:10:14 +0100

    make components optional un/controllable and init with values

--- frontend/src/Components/Checkbox/Checkbox.tsx
+++ frontend/src/Components/Checkbox/Checkbox.tsx
@@ -1,22 +1,55 @@
-import classNames from 'classnames';
 import React from 'react';
+import classNames from 'classnames';
 import styles from './Checkbox.module.scss';
 
-export type CheckboxProps = {
-  name?: string;
-  disabled?: boolean;
-  checked?: boolean;
+export type PrimitiveCheckboxProps = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  'checked' | 'defaultChecked' | 'type'
+> & {
   className?: string;
-  onChange?: (...event: unknown[]) => void;
-  readOnly?: boolean;
 };
 
-export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(({ className, ...props }, ref) => {
-  return (
-    <label className={styles.checkbox}>
-      <input type="checkbox" ref={ref} className={classNames(styles.checkbox__input, className)} {...props} />
-      <div className={styles.checkbox__box} />
-    </label>
-  );
-});
+/**
+ * Controlled props: requires `checked`, forbids `defaultChecked`.
+ */
+interface ControlledCheckboxProps extends PrimitiveCheckboxProps {
+  checked: boolean;
+  defaultChecked?: never;
+}
+
+/**
+ * Uncontrolled props: requires `defaultChecked`, forbids `checked`.
+ */
+interface UncontrolledCheckboxProps extends PrimitiveCheckboxProps {
+  checked?: never;
+  defaultChecked?: boolean;
+}
+
+/**
+ * Union type for either controlled or uncontrolled usage.
+ */
+export type CheckboxProps = ControlledCheckboxProps | UncontrolledCheckboxProps;
+
+export const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  ({ className, checked, defaultChecked, onChange, disabled, ...props }, ref) => {
+    const isControlled = checked !== undefined;
+
+    return (
+      <label className={styles.checkbox}>
+        <input
+          ref={ref}
+          type="checkbox"
+          className={classNames(styles.checkbox__input, className)}
+          checked={isControlled ? checked : undefined}
+          defaultChecked={!isControlled ? defaultChecked : undefined}
+          onChange={onChange}
+          disabled={disabled}
+          {...props}
+        />
+        <div className={styles.checkbox__box} />
+      </label>
+    );
+  },
+);
+
 Checkbox.displayName = 'Checkbox';
