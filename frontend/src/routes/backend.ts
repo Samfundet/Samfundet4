708cb3f73671be04740e8791f908742811fb118f
snorre sæther <snosaet@gmail.com>
2025-02-14 12:06:37 +0100

    generate routes

--- frontend/src/routes/backend.ts
+++ frontend/src/routes/backend.ts
@@ -565,6 +565,7 @@ export const ROUTES_BACKEND = {
   samfundet__interview_detail: '/api/interview/:pk/',
   samfundet__create_reservation_list: '/api/create-reservation/',
   samfundet__create_reservation_detail: '/api/create-reservation/:pk/',
+  samfundet__api_root: '/api/',
   samfundet__schema: '/schema/',
   samfundet__swagger_ui: '/schema/swagger-ui/',
   samfundet__redoc: '/schema/redoc/',
