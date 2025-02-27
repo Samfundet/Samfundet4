349f44a3d45b7da451f91a79f775b07b2bf40000
Mathias Aas <54811233+Mathias-a@users.noreply.github.com>
2025-02-27 18:43:04 +0100

    Fix/fix event admin page (#1706)
    * change to react hook form and zod
    
    * make draft that somewhat works
    
    * cleanupt
    
    * add keys to fix weird beaviour
    
    * remove unused useeffect
    
    * fix validation
    
    * remove logs
    
    * format with biome
    
    * remove unused style file

---
+++ frontend/src/PagesAdmin/EventCreatorAdminPage/EventCreatorSchema.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+import {
+  EVENT_CATEGORY,
+  EVENT_DESCRIPTION_LONG,
+  EVENT_DESCRIPTION_SHORT,
+  EVENT_DURATION,
+  EVENT_START_DT,
+  EVENT_TITLE,
+} from '~/schema/event';
+import { OPTIONAL_IMAGE } from '~/schema/samfImage';
+
+export const eventSchema = z.object({
+  // text and description
+  title_nb: EVENT_TITLE,
+  title_en: EVENT_TITLE,
+  description_long_nb: EVENT_DESCRIPTION_LONG,
+  description_long_en: EVENT_DESCRIPTION_LONG,
+  description_short_nb: EVENT_DESCRIPTION_SHORT,
+  description_short_en: EVENT_DESCRIPTION_SHORT,
+  // Date and information
+  start_dt: EVENT_START_DT,
+  duration: EVENT_DURATION,
+  category: EVENT_CATEGORY,
+  host: z.string().min(1, { message: 'Arrangør er påkrevd' }),
+  location: z.string().min(1, { message: 'Lokale er påkrevd' }),
+  capacity: z.number().min(1, { message: 'Kapasitet må være større enn 0' }),
+  // Payment/registration
+  age_restriction: z.enum(['none', 'eighteen', 'twenty', 'mixed']),
+  ticket_type: z.enum(['free', 'included', 'billig', 'registration', 'prepaid', 'custom']),
+  // Graphics
+  image: OPTIONAL_IMAGE,
+  // Summary/Publication date
+  publish_dt: z.string().min(1, { message: 'Publikasjonsdato er påkrevd' }),
+});
