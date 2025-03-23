5291c328945b8d2755562ac6c6796f211cec7fc4
Snorre Sæther <snosaet@gmail.com>
2025-03-23 11:05:24 +0100

    improves props

--- frontend/src/Components/BuyButton/BuyButton.tsx
+++ frontend/src/Components/BuyButton/BuyButton.tsx
@@ -3,19 +3,21 @@ import type { BilligTicketGroupDto } from '~/apis/billig/billigDtos';
 import { Button } from '../Button';
 
 type BuyButtonProps = {
-  ticketSalesState: BilligTicketGroupDto;
+  eventId: number;
+  billigId: number;
+  ticketSaleState: Pick<BilligTicketGroupDto, 'is_sold_out' | 'is_almost_sold_out'>;
 };
 
-export function BuyButton({ ticketSalesState }: BuyButtonProps) {
+export function BuyButton({ eventId, billigId, ticketSaleState }: BuyButtonProps) {
   const [buttonText, setButtonText] = useState<string>('Kjøp');
-  if (ticketSalesState.is_sold_out) {
+  if (ticketSaleState.is_sold_out) {
     setButtonText('Utsolgt');
   }
-  if (ticketSalesState.is_almost_sold_out) {
+  if (ticketSaleState.is_almost_sold_out) {
     setButtonText('Nesten utsolgt, kjøp!');
   }
   const openPaymentForm = () => {
-    alert('TODO: PR #1695');
+    alert(`ToDo: Open payment form modal with eventId ${eventId} and billigId ${billigId}`);
   };
   return (
     <Button
