45443e4acd6e34db6005fb3578b6529718e6e13a
Snorre Sæther <snosaet@gmail.com>
2025-03-24 21:46:59 +0100

    adds tabs for different statistics

--- frontend/src/PagesAdmin/RecruitmentStatisticsAdminPage/RecruitmentStatisticsAdminPage.tsx
+++ frontend/src/PagesAdmin/RecruitmentStatisticsAdminPage/RecruitmentStatisticsAdminPage.tsx
@@ -1,29 +1,27 @@
-import { useQuery } from '@tanstack/react-query';
+import { type ReactNode, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useParams } from 'react-router';
+import { type Tab, TabView } from '~/Components';
 import { AdminPageLayout } from '~/PagesAdmin/AdminPageLayout/AdminPageLayout';
-import { getRecruitmentForRecruiter } from '~/api';
-import type { RecruitmentForRecruiterDto } from '~/dto';
 import { useTitle } from '~/hooks';
 import { KEY } from '~/i18n/constants';
-import { dbT } from '~/utils';
 import { RecruitmentStatistics } from './RecruitmentStatistics/RecruitmentStatistics';
 
 export function RecruitmentStatisticsAdminPage() {
   const { t } = useTranslation();
   const { recruitmentId } = useParams();
 
-  const { data, isLoading, error } = useQuery<RecruitmentForRecruiterDto>({
-    queryKey: ['recruitmentStats', recruitmentId],
-    queryFn: () => getRecruitmentForRecruiter(recruitmentId as string),
-    enabled: typeof recruitmentId === 'string',
-  });
-
   useTitle(t(KEY.recruitment_overview));
+  const tabs: Tab<ReactNode>[] = useMemo(() => {
+    return [
+      { key: 1, label: 'TEST', value: <div>Test</div> },
+      { key: 2, label: t(KEY.recruitment_statistics), value: <RecruitmentStatistics recruitmentId={recruitmentId} /> },
+    ];
+  }, [recruitmentId, t]);
 
   return (
-    <AdminPageLayout title={`${t(KEY.recruitment_overview)}: ${dbT(data, 'name')}`}>
-      <RecruitmentStatistics statistics={data?.statistics} />
+    <AdminPageLayout title={`${t(KEY.recruitment_overview)} `}>
+      <TabView tabs={tabs} />
     </AdminPageLayout>
   );
 }
