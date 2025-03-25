cc30adf0333c1bacd2522019b942e9046ea467ed
Snorre Sæther <snosaet@gmail.com>
2025-03-25 19:15:20 +0100

    mathias sabotere meg

--- backend/samfundet/views.py
+++ backend/samfundet/views.py
@@ -17,6 +17,7 @@ from rest_framework.request import Request
 from rest_framework.generics import ListAPIView, ListCreateAPIView
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.permissions import AllowAny, IsAuthenticated, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 
