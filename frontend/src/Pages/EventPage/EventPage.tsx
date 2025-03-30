d8020a4ec602d670953abc62c9fbad5b0f63a1e9
Snorre Sæther <snosaet@gmail.com>
2025-03-30 16:52:48 +0200

    implements react query

--- frontend/src/Pages/EventPage/EventPage.tsx
+++ frontend/src/Pages/EventPage/EventPage.tsx
@@ -1,51 +1,33 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import { useTranslation } from 'react-i18next';
 import { useNavigate, useParams } from 'react-router';
-import { toast } from 'react-toastify';
 import { ExpandableHeader, H1, Image, Page } from '~/Components';
 import { BuyButton } from '~/Components/BuyButton/BuyButton';
 import { SamfMarkdown } from '~/Components/SamfMarkdown';
 import { getEvent } from '~/api';
 import { BACKEND_DOMAIN } from '~/constants';
-import type { EventDto } from '~/dto';
 import { useTitle } from '~/hooks';
-import { STATUS } from '~/http_status_codes';
 import { KEY } from '~/i18n/constants';
-import { ROUTES } from '~/routes';
+import { eventKeys } from '~/queryKeys';
 import { dbT } from '~/utils';
 import styles from './EventPage.module.scss';
 import { EventInformation } from './components/EventInformation/EventInformation';
 import { EventTable } from './components/EventTable';
 
 export function EventPage() {
-  const { id } = useParams();
   const { t } = useTranslation();
-  const [event, setEvent] = useState<EventDto>();
+  const { id } = useParams();
   const navigate = useNavigate();
-  const [showSpinner, setShowSpinner] = useState<boolean>(true);
+
+  const { data: event, isLoading } = useQuery({
+    queryKey: id ? eventKeys.detail(Number(id)) : ['events', 'no-id'],
+    queryFn: () => getEvent(id as string),
+    enabled: !!id,
+  });
 
   useTitle((event && dbT(event, 'title')) || t(KEY.common_event));
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: t and navigate do not need to be in deplist
-  useEffect(() => {
-    if (id) {
-      getEvent(id)
-        .then((data) => {
-          setEvent(data);
-          setShowSpinner(false);
-        })
-        .catch((error) => {
-          if (error.request.status === STATUS.HTTP_404_NOT_FOUND) {
-            navigate(ROUTES.frontend.not_found, { replace: true });
-          }
-          toast.error(t(KEY.common_something_went_wrong));
-          console.error(error);
-        });
-    }
-  }, [id]);
-
   return (
-    <Page className={styles.container} loading={showSpinner}>
+    <Page className={styles.container} loading={isLoading}>
       <div className={styles.image_wrapper}>
         {event && <Image src={BACKEND_DOMAIN + event.image_url} className={styles.event_image} />}
       </div>
