2c8bbfc2980edc43ea25760b2c7e41f4b3da2392
Robin <16273164+robines@users.noreply.github.com>
2025-04-24 19:55:30 +0200

    Interview availability admin page (#1559)
    * Begin interview availability admin page
    
    * Switch to NumberInput
    
    * Switch to DatePicker
    
    * Split description for interval
    
    * Update date preview
    
    * baseDate fall back to today
    
    * Set title
    
    * Preview timeslots
    
    * Improve styling
    
    * Clean up TimeslotContainer and TimeslotButton
    
    * TimeslotContainer -> TimeslotSelector
    
    More descriptive
    
    * Move TimeslotButton to primary Components dir
    
    It theoretically can be used other places than the TimeslotSelector,
    therefore it belongs in the Components dir.
    
    * Update usage
    
    * Fix start/end_time defaultValues
    
    * Set initialSelectedDate in DatePicker
    
    * Improve form styling
    
    * Fix generate_timeslots
    
    Say start_time is 08:00, end_time is 22:00, and interval is 30.
    Previously, the last timeslot would be 22:00, which is incorrect,
    because that means there's a possibility for an interview from
    22:00 to 22:30.
    
    With this fix, the last timeslot would now be 21:30.
    
    * generate_timeslots: support times passing midnight
    
    * Improve disabled styling
    
    * Also return start_time, end_time, timeslot_interval from API
    
    * Implement backend
    
    * Update frontend/src/PagesAdmin/RecruitmentInterviewAvailabilityAdminPage/RecruitmentInterviewAvailabilityForm.tsx
    
    Co-authored-by: Emil Solberg <86266961+emsoraffa@users.noreply.github.com>
    
    * Changes from master
    
    * Update scss imports
    
    * Don't use global selector in interview availability form css
    
    * DatePicker: fix descending specificity
    
    * Regenerate poetry lockfile
    
    * ruff
    
    * Make interval a dropdown instead
    
    * ruff format
    
    * mypy
    
    * ruffffffff
    
    * Add Time input component TODO comments
    
    * Fix dropdown value type
    
    ---------
    
    Co-authored-by: Emil Solberg <86266961+emsoraffa@users.noreply.github.com>

--- frontend/src/dto.ts
+++ frontend/src/dto.ts
@@ -35,9 +35,23 @@ export type CampusDto = {
   abbreviation?: string;
 };
 
+export type RecruitmentInterviewAvailabilityDto = {
+  id: number;
+  start_date: string;
+  end_date: string;
+  start_time: string;
+  end_time: string;
+  timeslot_interval: number;
+  recruitment_id: number;
+  position_id?: number;
+};
+
 export type RecruitmentAvailabilityDto = {
   start_date: string;
   end_date: string;
+  start_time: string;
+  end_time: string;
+  timeslot_interval: number;
   timeslots: string[];
   interval: number;
 };
