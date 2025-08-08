2a6ee1b2d4d76834050e8388f2b3adcde704a0fb
Snorre Sæther <snosaet@gmail.com>
2025-08-09 01:28:02 +0200

    Conditionally loads image based on isDarkmode state; more efficient rendering.
    Uses IconButton component to close modal

--- frontend/src/Components/BuyTicketModal/BuyTicketModal.tsx
+++ frontend/src/Components/BuyTicketModal/BuyTicketModal.tsx
@@ -1,8 +1,8 @@
-import { Icon } from '@iconify/react';
-import { useState } from 'react';
 import { logoBlack, logoWhite } from '~/assets';
 import type { EventDto } from '~/dto';
+import { useIsDarkTheme } from '~/hooks';
 import { dbT } from '~/utils';
+import { IconButton } from '../IconButton';
 import { Modal } from '../Modal';
 import BuyTicketForm from './BuyTicketForm';
 import styles from './BuyTicketModal.module.scss';
@@ -14,17 +14,28 @@ type BuyTicketModalProps = {
 };
 
 export function BuyTicketModal({ event, isOpen, onClose }: BuyTicketModalProps) {
-  const [open, setOpen] = useState(false);
+
+  const isDarkmode = useIsDarkTheme();
 
   return (
     <>
       <Modal isOpen={isOpen} className={styles.modal}>
-        <img src={logoBlack} className={styles.samf_logo_black} alt="LogoBlack" />
-        <img src={logoWhite} className={styles.samf_logo_white} alt="LogoWhite" />
+        <div className={styles.buy_ticket_modal_header}>
+          {
+            isDarkmode ?      
+               <img src={logoWhite} className={styles.samf_logo} alt="LogoWhite" /> : 
+               <img src={logoBlack} className={styles.samf_logo} alt="LogoBlack" />
+          }
+          <IconButton
+            title="close"
+            icon="mdi:close"
+            className={styles.close_btn}
+            color={isDarkmode ? "white" : "black"}
+            avatarColor={isDarkmode ? "black" : "white"}
+            onClick={onClose}
+          />
+        </div>
         <h2 className={styles.modal_title}>{dbT(event, 'title')}</h2>
-        <button type="button" className={styles.close_btn} title="Close" onClick={onClose}>
-          <Icon icon="octicon:x-24" width={24} />
-        </button>
         <BuyTicketForm event={event} />
       </Modal>
     </>
