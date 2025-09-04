15135ac4e1c8d94d55524a7133913bdc7ca8cf8b
eilifhl <eilifhl@stud.ntnu.no>
2025-09-04 21:42:57 +0200

    Implement comment saving logic in comment component

--- frontend/src/Components/ApplicationCommentForm/ApplicationCommentForm.tsx
+++ frontend/src/Components/ApplicationCommentForm/ApplicationCommentForm.tsx
@@ -1,25 +1,44 @@
+import { useMutation } from '@tanstack/react-query';
 import type React from 'react';
 import { useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { toast } from 'react-toastify';
+import { addApplicationComment } from '~/api';
 import { Textarea } from '~/Components';
+import { KEY } from '~/i18n/constants';
 
 type CommentFormProps = {
+  applicationId: number;
   commentText?: string;
-  handlePost: () => void;
+  onSuccess: () => void;
 };
 
-export function ApplicationCommentForm({ commentText, handlePost }: CommentFormProps) {
+export function ApplicationCommentForm({ applicationId, commentText, onSuccess }: CommentFormProps) {
   const [comment, setComment] = useState(commentText || '');
   const textareaRef = useRef<HTMLTextAreaElement>(null);
   const prevCommentRef = useRef(comment);
+  const { t } = useTranslation();
+
+  const { mutate: postComment } = useMutation({
+    mutationFn: (newComment: string) => addApplicationComment(applicationId, newComment),
+    onSuccess: () => {
+      toast.success(t(KEY.common_update_successful));
+      onSuccess();
+    },
+    onError: () => {
+      toast.error(t(KEY.common_error));
+    },
+  });
 
   // Update state if initialData changes
   useEffect(() => {
-    setComment(commentText || '');
+    const newCommentText = commentText || '';
+    setComment(newCommentText);
+    prevCommentRef.current = newCommentText;
   }, [commentText]);
 
   // Handle blur event
   const handleBlur = (event: React.FocusEvent<HTMLTextAreaElement>) => {
-    // TODO: implement actuall comment logic
     const newValue = event.target.value;
 
     // Only update if the value has changed
@@ -30,7 +49,8 @@ export function ApplicationCommentForm({ commentText, handlePost }: CommentFormP
       // Update state
       setComment(newValue);
 
-      handlePost();
+      // Post comment to backend
+      postComment(newValue);
     }
   };
 
