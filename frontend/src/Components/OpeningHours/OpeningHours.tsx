a81cf467615e64568f8bdd706932ecbf88691bc2
specter <sigurd.thornes@gmail.com>
2025-10-16 20:56:45 +0200

    changed text used from description to message

--- frontend/src/Components/OpeningHours/OpeningHours.tsx
+++ frontend/src/Components/OpeningHours/OpeningHours.tsx
@@ -26,7 +26,7 @@ export function OpeningHours({ venues, isLoading, isError }: OpeningHoursProps) 
       for (const period of periods) {
         if (new Date(period.start_dt) < now && now < new Date(period.end_dt)) {
           setIsClosed(true);
-          setClosedText(dbT(period, 'description'));
+          setClosedText(dbT(period, 'message'));
           break;
         }
       }
