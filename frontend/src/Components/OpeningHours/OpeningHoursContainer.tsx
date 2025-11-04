83aeae9bde90539b02624d6d0b68362bfcbddc3e
eilifhl <eilifhl@stud.ntnu.no>
2025-11-04 21:16:19 +0100

    Lol fix compiler errors

--- frontend/src/Components/OpeningHours/OpeningHoursContainer.tsx
+++ frontend/src/Components/OpeningHours/OpeningHoursContainer.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import { getOpenVenues } from '~/api';
 import type { VenueDto } from '~/dto';
+import { venueKeys } from '~/queryKeys';
 import { OpeningHours } from './OpeningHours';
 
 export function OpeningHoursContainer() {
