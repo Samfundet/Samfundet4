6e85b13dd8262024711f20bb48d8308d328ecf43
Tor Madsen <marvel.madsen@gmail.com>
2026-01-29 16:55:49 +0100

    removed unnecessary const declaration

--- frontend/src/routes/samf-three.ts
+++ frontend/src/routes/samf-three.ts
@@ -28,7 +28,7 @@ export const ROUTES_SAMF_THREE = {
     club: `${BASE_URL}/informasjon/klubb`,
   },
   volunteer: `${BASE_URL}/opptak`,
-} as const;
+};
 // Samfundet3 innlogging
 export const SAMF3_LOGIN_URL = {
   login: `${BASE_URL}/logg-inn`,
