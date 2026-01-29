d13830dcfb6e4b31a9f3d8367d8c318a1003225e
Tor Madsen <marvel.madsen@gmail.com>
2026-01-29 18:55:56 +0100

    Fixed samf-three-routes const

--- frontend/src/routes/samf-three.ts
+++ frontend/src/routes/samf-three.ts
@@ -20,31 +20,6 @@ export const ROUTES_SAMF_THREE = {
     openingHours: `${BASE_URL}/informasjon/aapningstider`,
     photos: `${PHOTO}`,
     renting: `${BASE_URL}/informasjon/leie-lokaler`,
-  },
-  venues: {
-    restaurant: `${BASE_URL}/lyche`,
-    bar: `${BASE_URL}/informasjon/bar`,
-    scene: `${BASE_URL}/informasjon/scene`,
-    club: `${BASE_URL}/informasjon/klubb`,
-  },
-  volunteer: `${BASE_URL}/opptak`,
-};
-// Samfundet3 innlogging
-export const SAMF3_LOGIN_URL = {
-  login: `${BASE_URL}/logg-inn`,
-};
-
-export const SAMF3_MEMBER_URL = {
-  medlem: 'https://medlem.samfundet.no/',
-};
-
-export const ROUTES_SAMF_THREE = {
-  information: {
-    general: `${BASE_URL}/informasjon`,
-    membership: `${BASE_URL}/informasjon/medlemskap`,
-    openingHours: `${BASE_URL}/informasjon/aapningstider`,
-    photos: `${PHOTO}`,
-    renting: `${BASE_URL}/informasjon/leie-lokaler`,
     contact: `${BASE_URL}/informasjon/kontaktinfo`,
   },
   venues: {
@@ -55,3 +30,12 @@ export const ROUTES_SAMF_THREE = {
   },
   volunteer: `${BASE_URL}/opptak`,
 } as const;
+
+// Samfundet3 innlogging
+export const SAMF3_LOGIN_URL = {
+  login: `${BASE_URL}/logg-inn`,
+};
+
+export const SAMF3_MEMBER_URL = {
+  medlem: 'https://medlem.samfundet.no/',
+};
