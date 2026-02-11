89e7af907c1c301beda00e48da8caafc6472a120
robines <espi.robin@gmail.com>
2026-02-11 20:33:15 +0100

    Set up fake mdb2 service. Create seed. Create unmanaged MedlemsInfo model

--- backend/root/db_router.py
+++ backend/root/db_router.py
@@ -1,6 +1,6 @@
 """
 Handles routing for databases (which database should be used for which model).
-All models use the default database except billig models.
+All models use the default database except billig and mdb models.
 """
 
 from __future__ import annotations
@@ -14,6 +14,7 @@ from samfundet.models.billig import (
     BilligPriceGroup,
     BilligTicketGroup,
 )
+from samfundet.models.mdb import MedlemsInfo
 
 # List of models routed to billig database.
 BILLIG_MODELS: list[type[models.Model]] = [
@@ -22,16 +23,25 @@ BILLIG_MODELS: list[type[models.Model]] = [
     BilligPriceGroup,
 ]
 
+# List of models routed to mdb database.
+MDB_MODELS: list[type[models.Model]] = [
+    MedlemsInfo,
+]
+
 
 class SamfundetDatabaseRouter:
     def db_for_read(self, model: type[models.Model], **hints: dict[str, Any]) -> str | None:
         if model in BILLIG_MODELS:
             return 'billig'
+        if model in MDB_MODELS:
+            return 'mdb'
         return None
 
     def db_for_write(self, model: type[models.Model], **hints: dict[str, Any]) -> str | None:
         if model in BILLIG_MODELS:
             return 'billig'
+        if model in MDB_MODELS:
+            return 'mdb'
         return None
 
     def allow_migrate(
@@ -41,4 +51,5 @@ class SamfundetDatabaseRouter:
         model_name: str | None = None,
         **hints: dict[str, Any],
     ) -> bool:
-        return model_name not in [m._meta.model_name for m in BILLIG_MODELS]
+        return (model_name not in [m._meta.model_name for m in BILLIG_MODELS] and
+               model_name not in [m._meta.model_name for m in MDB_MODELS])
