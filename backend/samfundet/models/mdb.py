cd4112806a9bdc09450b2f9ebe1f0171e7b7d7f9
Robin <16273164+robines@users.noreply.github.com>
2026-02-14 16:34:13 +0100

    Create mdb database (#2061)
    * Set up fake mdb2 service. Create seed. Create unmanaged MedlemsInfo model
    
    * Rename to mdb_dev
    
    * ruff
    
    * Simplify disallowed models check
    
    * Fix port in github workflow
    
    * ruff
    
    * Update migration

---
+++ backend/samfundet/models/mdb.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+
+from django.db import models
+
+
+class MedlemsInfo(models.Model):
+    medlem_id = models.PositiveIntegerField(null=False, blank=False, primary_key=True)
+    fornavn = models.CharField(null=True, blank=True)
+    etternavn = models.CharField(null=True, blank=True)
+    fodselsdato = models.DateField(null=True, blank=True)
+    telefon = models.CharField(null=True, blank=True)
+    mail = models.CharField(null=True, blank=True)
+    skole = models.CharField(null=True, blank=True)
+    studie = models.CharField(null=True, blank=True)
+    brukernavn = models.CharField(max_length=14, null=False, blank=False)
+
+    class Meta:
+        managed = False
+        verbose_name = 'MedlemsInfo'
+        db_table = 'lim_medlemsinfo'
+
+    def __str__(self) -> str:
+        return self.medlem_id
