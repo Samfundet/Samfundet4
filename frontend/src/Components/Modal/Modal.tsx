ca9262623c9050d44d09c92e001da8624c5135a9
Erik <eahoff@gmail.com>
2026-02-17 17:44:08 +0100

    correct body overflow handling when modal is inactive

--- frontend/src/Components/Modal/Modal.tsx
+++ frontend/src/Components/Modal/Modal.tsx
@@ -15,11 +15,11 @@ export function Modal({ children, className, isOpen, ...props }: ModalProps) {
     if (isOpen) {
       document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
     }
 
     return () => {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
     };
   }, [isOpen]);
 
