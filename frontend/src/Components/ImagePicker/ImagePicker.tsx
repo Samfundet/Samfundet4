942ad42f16ef0bdccacbb061f2548275a362f221
Erik <eahoff@gmail.com>
2026-02-17 23:05:49 +0100

    Add pagination and filtering to ImagePicker component

--- frontend/src/Components/ImagePicker/ImagePicker.tsx
+++ frontend/src/Components/ImagePicker/ImagePicker.tsx
@@ -7,7 +7,11 @@ import { BACKEND_DOMAIN } from '~/constants';
 import type { ImageDto } from '~/dto';
 import { KEY } from '~/i18n/constants';
 import { backgroundImageFromUrl } from '~/utils';
+import { ImageQuery } from '../ImageQuery/ImageQuery';
+import { PagedPagination } from '../Pagination';
 import styles from './ImagePicker.module.scss';
+
+const PAGE_SIZE = 12;
 
 export type ImagePickerProps = {
   onSelected?(image: ImageDto): void;
@@ -18,12 +22,22 @@ export function ImagePicker({ onSelected, selectedImage }: ImagePickerProps) {
   const { t } = useTranslation();
   const [selected, setSelected] = useState<ImageDto | undefined>(selectedImage);
   const [images, setImages] = useState<ImageDto[]>([]);
+  const [allImages, setAllImages] = useState<ImageDto[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   useEffect(() => {
     getImages()
-      .then((imgs) => setImages(imgs))
+      .then((imgs) => {
+        setImages(imgs);
+        setAllImages(imgs);
+      })
       .catch(() => console.error);
   }, []);
+
+  // Reset to page 1 when filtered images change
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [images]);
 
   function select(image: ImageDto) {
     setSelected(image);
@@ -43,6 +57,11 @@ export function ImagePicker({ onSelected, selectedImage }: ImagePickerProps) {
     );
   }
 
+  // Calculate paginated images
+  const startIndex = (currentPage - 1) * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const displayImages = images.slice(startIndex, endIndex);
+
   return (
     <div className={styles.container}>
       <div className={styles.selected_container}>
@@ -55,9 +74,19 @@ export function ImagePicker({ onSelected, selectedImage }: ImagePickerProps) {
             </>
           )}
         </div>
-        {/* TODO tags and other metadata */}
       </div>
-      <div className={styles.image_container}>{images.map((image) => renderImage(image))}</div>
+      <div style={{ display: 'flex', flexDirection: 'column'}}>
+        <ImageQuery allImages={allImages} setImages={setImages} />
+        <div className={styles.image_container}>{displayImages.map((image) => renderImage(image))}</div>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
+          <PagedPagination
+            currentPage={currentPage}
+            totalItems={images.length}
+            pageSize={PAGE_SIZE}
+            onPageChange={setCurrentPage}
+          />
+        </div>
+      </div>
     </div>
   );
 }
