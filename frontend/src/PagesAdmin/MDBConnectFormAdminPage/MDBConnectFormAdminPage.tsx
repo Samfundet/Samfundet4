51c595a72a625df230dbac3f679f24f2eeb23b1b
Bragon <brage.sebastian.brevik@gmail.com>
2026-02-19 21:08:56 +0100

    feat: create MDB connection form

--- frontend/src/PagesAdmin/MDBConnectFormAdminPage/MDBConnectFormAdminPage.tsx
+++ frontend/src/PagesAdmin/MDBConnectFormAdminPage/MDBConnectFormAdminPage.tsx
@@ -1,3 +1,44 @@
+import { SamfForm } from '~/Forms/SamfForm';
+import { SamfFormField } from '~/Forms/SamfFormField';
+import styles from './MDBConnectFormAdminPage.module.scss'
+import { KEY } from '~/i18n/constants';
+import { useTranslation } from 'react-i18next';
+import { useState } from 'react';
+
+type FormProps = {
+  email: string;
+  password: string;
+};
+
 export function MDBConnectFormAdminPage() {
-  return <div>Her ska en vakker form komme</div>;
+  const [submitting, setSubmitting] = useState(false);
+  const {t} = useTranslation();
+
+  function handleSubmit(formData: FormProps) {
+      setSubmitting(true)
+      console.log(formData);
+      //TODO: Koble te backend
+      setSubmitting(false);
+  }
+
+
+  return <div className={styles.wrapper}>
+    <h1>{t(KEY.adminpage_connect_mdb)}</h1>
+    <SamfForm onSubmit={handleSubmit} isDisabled={submitting}>
+      <h2>{t(KEY.common_email)}</h2>
+      <SamfFormField<string, FormProps>
+        required={true}
+        field='email'
+        type='text'
+        
+      />
+      <h2>{t(KEY.common_password)}</h2>
+    <SamfFormField<string, FormProps>
+        required={true}
+        field='password'
+        type='password'
+      />
+
+    </SamfForm>
+  </div>;
 }
