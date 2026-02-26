aeedd306ca0cf77b1cc42c3d27d2f5ea23c98f41
Erik <eahoff@gmail.com>
2026-02-26 22:56:55 +0100

    Fix broken route protection of LoginPicker

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -120,8 +120,10 @@ export const router = createBrowserRouter(
           <Route path={ROUTES.frontend.venues} element={<SiteFeatureGate feature="venues"><VenuePage /></SiteFeatureGate>}/>
           <Route path={ROUTES.frontend.health} element={<HealthPage />} />
           <Route path={ROUTES.frontend.components} element={<ComponentPage />} />
-          <Route element={<ProtectedRoute authState={false} element={<Outlet />} />}>
-            <Route path={ROUTES.frontend.login} element={<LoginPickerPage />} />
+          <Route path={ROUTES.frontend.login} element={<LoginPickerPage />} />
+          <Route
+            element={<ProtectedRoute authState={false} redirectPath={ROUTES.frontend.admin} element={<Outlet />} />}
+          >
             <Route path={SAMF3_LOGIN_URL.login} element={<LoginPage />} />
             <Route handle={{ crumb: () => <Link url={ROUTES.frontend.login}>{t(KEY.common_login)}</Link> }}>
               <Route
