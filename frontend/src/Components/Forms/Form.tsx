2984da0203627d4683ea0699e8397310d6868e76
hei98 <heidi-hei@live.no>
2026-03-12 15:39:19 +0100

    Add asterix to FormLabel

--- frontend/src/Components/Forms/Form.tsx
+++ frontend/src/Components/Forms/Form.tsx
@@ -78,12 +78,13 @@ FormItem.displayName = 'FormItem';
 
 interface FormLabelProps extends React.HTMLAttributes<HTMLLabelElement> {
   className?: string;
+  required?: boolean
 }
 
 export const FormLabel = React.forwardRef<
   React.ElementRef<'label'>,
   React.ComponentPropsWithoutRef<'label'> & FormLabelProps
->(({ className, ...props }, ref) => {
+>(({ className, required = false, children, ...props }, ref) => {
   const { error, formItemId } = useFormField();
 
   return (
@@ -92,7 +93,10 @@ export const FormLabel = React.forwardRef<
       className={classNames(styles.label, error && styles.error, className)}
       htmlFor={formItemId}
       {...props}
-    />
+    >
+      {children}
+      {required ? '*' : ''}
+    </label>
   );
 });
 FormLabel.displayName = 'FormLabel';
