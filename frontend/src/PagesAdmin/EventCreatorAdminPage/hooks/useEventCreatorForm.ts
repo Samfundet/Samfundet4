7b43b69ae7a87428af27853080973d48603a81f9
Erik <eahoff@gmail.com>
2026-04-07 23:22:00 +0200

    remove harcoded string

--- frontend/src/PagesAdmin/EventCreatorAdminPage/hooks/useEventCreatorForm.ts
+++ frontend/src/PagesAdmin/EventCreatorAdminPage/hooks/useEventCreatorForm.ts
@@ -4,7 +4,7 @@ import { useForm } from 'react-hook-form';
 import type { z } from 'zod';
 
 import type { EventDto, EventWriteDto } from '~/dto';
-import type { EventCategoryValue } from '~/types';
+import { type EventCategoryValue, EventStatusChoice } from '~/types';
 import { utcTimestampToLocal } from '~/utils';
 import { eventSchema } from '../EventCreatorSchema';
 
@@ -56,7 +56,7 @@ export function useEventCreatorForm(params: {
       vimeo_link: '',
       general_link: '',
       image: undefined,
-      status: 'public',
+      status: EventStatusChoice.PUBLIC,
       visibility_from_dt: '',
       visibility_to_dt: '',
     },
@@ -96,7 +96,7 @@ export function useEventCreatorForm(params: {
       vimeo_link: event.vimeo_link || '',
       general_link: event.general_link || '',
       image: event.image ?? undefined,
-      status: event.status || 'public',
+      status: event.status || EventStatusChoice.PUBLIC,
       visibility_from_dt: event.visibility_from_dt ? utcTimestampToLocal(event.visibility_from_dt, false) : '',
       visibility_to_dt: event.visibility_to_dt ? utcTimestampToLocal(event.visibility_to_dt, false) : '',
     };
