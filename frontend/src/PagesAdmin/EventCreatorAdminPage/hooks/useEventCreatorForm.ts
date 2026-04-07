a6b013b9ff77dc02eaded736a55bae1917da7d2e
Erik <eahoff@gmail.com>
2026-04-07 20:14:45 +0200

    Setup event status in event schema

--- frontend/src/PagesAdmin/EventCreatorAdminPage/hooks/useEventCreatorForm.ts
+++ frontend/src/PagesAdmin/EventCreatorAdminPage/hooks/useEventCreatorForm.ts
@@ -56,6 +56,7 @@ export function useEventCreatorForm(params: {
       vimeo_link: '',
       general_link: '',
       image: undefined,
+      status: 'public',
       visibility_from_dt: '',
       visibility_to_dt: '',
     },
@@ -95,6 +96,7 @@ export function useEventCreatorForm(params: {
       vimeo_link: event.vimeo_link || '',
       general_link: event.general_link || '',
       image: event.image ?? undefined,
+      status: event.status || 'public',
       visibility_from_dt: event.visibility_from_dt ? utcTimestampToLocal(event.visibility_from_dt, false) : '',
       visibility_to_dt: event.visibility_to_dt ? utcTimestampToLocal(event.visibility_to_dt, false) : '',
     };
