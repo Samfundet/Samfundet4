bb73442805f7646c35c71aa05849de6a2f725c23
Robin <16273164+robines@users.noreply.github.com>
2026-06-22 00:12:46 +0200

    Tidy up homepage (#2197)

--- frontend/src/Components/EventCardContainer/EventCardContainer.tsx
+++ frontend/src/Components/EventCardContainer/EventCardContainer.tsx
@@ -1,16 +1,30 @@
-import { EventCard, H4 } from '~/Components';
+import { EventCard, H4, Skeleton } from '~/Components';
 import type { EventDto } from '~/dto';
 import styles from './EventCardContainer.module.scss';
 
 type Props = {
   events: EventDto[];
   title?: string;
+  skeletonCount?: number;
 };
 
-export function EventCardContainer({ events, title }: Props) {
+export function EventCardContainer({ events, title, skeletonCount }: Props) {
   return (
     <div className={styles.wrapper}>
       {title && <H4>{title}</H4>}
+
+      {!events.length && skeletonCount && (
+        <>
+          <Skeleton width="14rem" height="2rem" marginBottom="0.5rem" />
+          <div className={styles.container}>
+            {Array.from({ length: skeletonCount }).map((_, i) => (
+              // biome-ignore lint/suspicious/noArrayIndexKey: no other unique value we can use here
+              <Skeleton key={i} height="250px" width="330px" className={styles.card_container} />
+            ))}
+          </div>
+        </>
+      )}
+
       <div className={styles.container}>
         {events.map((event) => (
           <EventCard event={event} key={event.id} className={styles.card_container} />
