cce37e818779aed5c1bda1206878367597bbcab8
robines <espi.robin@gmail.com>
2026-06-21 17:17:55 +0200

    Remove chromatic

--- frontend/.storybook/main.ts
+++ frontend/.storybook/main.ts
@@ -5,12 +5,7 @@ const config: StorybookConfig = {
   framework: '@storybook/react-vite',
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   // Optional
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@chromatic-com/storybook',
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
   docs: {},
 };
 
