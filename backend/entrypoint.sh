984f176a174e912d49534b946357033dca3b4406
Robin <16273164+robines@users.noreply.github.com>
2026-07-05 10:30:03 +0200

    Fix backend superuser warnings (#2222)

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,7 @@
 uv run python manage.py collectstatic --noinput
 uv run python manage.py makemigrations
 uv run python manage.py migrate
+uv run python manage.py create_dev_superuser
 uv run python manage.py seed_if_db_empty
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
