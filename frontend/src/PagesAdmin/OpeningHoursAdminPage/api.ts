0c4f09dead583ae7ff3f6df1bb14b35a8d2ee510
Sondre Erichsen <so@erichsen.me>
2026-09-18 12:45:42 +0200

    chore: add explicit return type to patchVenueDaySchedule

--- frontend/src/PagesAdmin/OpeningHoursAdminPage/api.ts
+++ frontend/src/PagesAdmin/OpeningHoursAdminPage/api.ts
@@ -5,7 +5,11 @@ import { ROUTES } from '~/routes';
 import type { Day } from '~/types';
 import type { VenueDaySchedule, VenueDayScheduleDto } from './dto';
 
-export async function patchVenueDaySchedule(slug: string, weekday: Day, schedule: VenueDaySchedule) {
+export async function patchVenueDaySchedule(
+  slug: string,
+  weekday: Day,
+  schedule: VenueDaySchedule,
+): Promise<VenueDayScheduleDto> {
   const url =
     BACKEND_DOMAIN + reverse({ pattern: ROUTES.backend.samfundet__venues_opening_hours, urlParams: { slug, weekday } });
   const response = await axios.patch<VenueDayScheduleDto>(url, schedule, { withCredentials: true });
